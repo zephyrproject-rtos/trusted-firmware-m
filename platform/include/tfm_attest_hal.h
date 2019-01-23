d09c38f9797da78b46341a10127b876f842e2049
Tamas Ban <tamas.ban@arm.com>
2019-01-23 22:04:50 +0000

    Platform: Add getter functions for attestation
    Getter functions were added to attestation HAL to retrive the
    following optional claims for initial attestation service:
     - Verification Service Indicator
     - Profile Definition
    
    Change-Id: I3be78e7713ba4a4c25165d9ae12efbf5f1d4ff75
    Signed-off-by: Tamas Ban <tamas.ban@arm.com>

--- platform/include/tfm_attest_hal.h
+++ platform/include/tfm_attest_hal.h
@@ -8,6 +8,8 @@
 #ifndef __TFM_ATTEST_HAL_H__
 #define __TFM_ATTEST_HAL_H__
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -33,6 +35,38 @@ enum tfm_security_lifecycle_t {
  * \return According to \ref tfm_security_lifecycle_t
  */
 enum tfm_security_lifecycle_t tfm_attest_hal_get_security_lifecycle(void);
+
+/**
+ * \brief Retrieve the verification service indicator for initial attestation.
+ *
+ * It is used by relying party to locate a validation service for the token.
+ * It can be a text string that can be used to locate the service or can be a
+ * URL specifying the address of the service.
+ *
+ * \param[out]  size  Length of the string, without the termination zero byte.
+ *
+ * \return NULL pointer if not available otherwise the address of the
+ *         verification service string in the device memory.
+ */
+const char *
+tfm_attest_hal_get_verification_service(uint32_t *size);
+
+/**
+ * \brief Retrieve the name of the profile definition document for initial
+ *        attestation.
+ *
+ *  This document describes the 'profile' of the initial attestation token,
+ *  being a full description of the claims, their usage, verification and
+ *  token signing.
+ *
+ * \param[out]  size  Length of the document name, without the termination zero
+ *                    byte.
+ *
+ * \return NULL pointer if not available otherwise the address of the document
+ *         name string in the device memory.
+ */
+const char *
+tfm_attest_hal_get_profile_definition(uint32_t *size);
 
 #ifdef __cplusplus
 }
