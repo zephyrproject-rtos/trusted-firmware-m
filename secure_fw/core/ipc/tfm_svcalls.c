karl-zh/tfm_demo

Core: Implement the IPC Secure Client API
Implement the IPC Client APIs in syscalls.c file.

Change-Id: I8e71a9803bc5ae1fc5e779aba0bbe5ab99290702
Signed-off-by: Summer Qin <summer.qin@arm.com>

secure_fw/core/ipc/tfm_svcalls.c
@@ -31,21 +31,225 @@ uint32_t tfm_svcall_psa_framework_version(void)
 
 uint32_t tfm_svcall_psa_version(uint32_t *args, int32_t ns_caller)
 {
-    return PSA_VERSION_NONE;
+    uint32_t sid;
+    struct tfm_spm_service_t *service;
+
+    TFM_ASSERT(args != NULL);
+    sid = (uint32_t)args[0];
+    /*
+     * It should return PSA_VERSION_NONE if the RoT Service is not
+     * implemented.
+     */
+    service = tfm_spm_get_service_by_sid(sid);
+    if (!service) {
+        return PSA_VERSION_NONE;
+    }
+
+    /*
+     * It should return PSA_VERSION_NONE if the caller is not authorized
+     * to access the RoT Service.
+     */
+    if (ns_caller && !service->service_db->non_secure_client) {
+        return PSA_VERSION_NONE;
+    }
+
+    return service->service_db->minor_version;
 }
 
 psa_handle_t tfm_svcall_psa_connect(uint32_t *args, int32_t ns_caller)
 {
+    uint32_t sid;
+    uint32_t minor_version;
+    struct tfm_spm_service_t *service;
+    struct tfm_msg_body_t *msg;
+
+    TFM_ASSERT(args != NULL);
+    sid = (uint32_t)args[0];
+    minor_version = (uint32_t)args[1];
+
+    /* It is a fatal error if the RoT Service does not exist on the platform */
+    service = tfm_spm_get_service_by_sid(sid);
+    if (!service) {
+        tfm_panic();
+    }
+
+    /*
+     * It is a fatal error if the caller is not authorized to access the RoT
+     * Service.
+     */
+    if (ns_caller && !service->service_db->non_secure_client) {
+        tfm_panic();
+    }
+
+    /*
+     * It is a fatal error if the version of the RoT Service requested is not
+     * supported on the platform.
+     */
+    if (tfm_spm_check_client_version(service, minor_version) != IPC_SUCCESS) {
+        tfm_panic();
+    }
+
+    /* No input or output needed for connect message */
+    msg = tfm_spm_create_msg(service, PSA_NULL_HANDLE, PSA_IPC_CONNECT,
+                             NULL, 0, NULL, 0);
+    if (!msg) {
+        return PSA_NULL_HANDLE;
+    }
+
+    /*
+     * Send message and wake up the SP who is wating on message queue,
+     * and scheduler triggered
+     */
+    tfm_spm_send_event(service, msg);
+
     return PSA_NULL_HANDLE;
 }
 
 psa_status_t tfm_svcall_psa_call(uint32_t *args, int32_t ns_caller)
 {
+    psa_handle_t handle;
+    psa_invec *inptr, invecs[PSA_MAX_IOVEC];
+    psa_outvec *outptr, outvecs[PSA_MAX_IOVEC];
+    size_t in_num, out_num;
+    struct tfm_spm_service_t *service;
+    struct tfm_msg_body_t *msg;
+    int i;
+
+    TFM_ASSERT(args != NULL);
+    handle = (psa_handle_t)args[0];
+    if (!ns_caller) {
+        inptr = (psa_invec *)args[1];
+        in_num = (size_t)args[2];
+        outptr = (psa_outvec *)args[3];
+        /*
+         * FixMe: 5th parameter is pushed at stack top before SVC; plus
+         * exception stacked contents, 5th parameter is now at 8th position
+         * in SVC handler. However, if thread mode applies FloatPoint, then
+         * FloatPoint context is pushed into stack and then 5th parameter
+         * will not be args[8].
+         * Will refine it later.
+         */
+         out_num = (size_t)args[8];
+    } else {
+        /*
+         * FixMe: From non-secure caller, vec and len are composed into a new
+         * struct parameter. Need to extract them.
+         */
+        TFM_ASSERT(args[1] != 0);
+        TFM_ASSERT(args[2] != 0);
+        inptr = (psa_invec *)((psa_invec *)args[1])->base;
+        in_num = ((psa_invec *)args[1])->len;
+        outptr = ((psa_outvec *)args[2])->base;
+        out_num = ((psa_outvec *)args[2])->len;
+    }
+
+    /* It is a fatal error if in_len + out_len > PSA_MAX_IOVEC. */
+    if (in_num + out_num > PSA_MAX_IOVEC) {
+        tfm_panic();
+    }
+
+    /* It is a fatal error if an invalid handle was passed. */
+    service = tfm_spm_get_service_by_handle(handle);
+    if (!service) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        tfm_panic();
+    }
+
+    /* It is a fatal error if an invalid memory reference was provide. */
+    if (tfm_memory_check((void *)inptr, in_num * sizeof(psa_invec),
+        ns_caller) != IPC_SUCCESS) {
+        tfm_panic();
+    }
+    if (tfm_memory_check((void *)outptr, out_num * sizeof(psa_outvec),
+        ns_caller) != IPC_SUCCESS) {
+        tfm_panic();
+    }
+
+    tfm_memset(invecs, 0, sizeof(invecs));
+    tfm_memset(outvecs, 0, sizeof(outvecs));
+
+    /* Copy the address out to avoid TOCTOU attacks. */
+    tfm_memcpy(invecs, inptr, in_num * sizeof(psa_invec));
+    tfm_memcpy(outvecs, outptr, out_num * sizeof(psa_outvec));
+
+    /*
+     * It is a fatal error if an invalid payload memory reference
+     * was provided.
+     */
+    for (i = 0; i < in_num; i++) {
+        if (tfm_memory_check((void *)invecs[i].base, invecs[i].len,
+            ns_caller) != IPC_SUCCESS) {
+            tfm_panic();
+        }
+    }
+    for (i = 0; i < out_num; i++) {
+        if (tfm_memory_check(outvecs[i].base, outvecs[i].len,
+            ns_caller) != IPC_SUCCESS) {
+            tfm_panic();
+        }
+    }
+
+    /*
+     * FixMe: Need to check if the message is unrecognized by the RoT
+     * Service or incorrectly formatted.
+     */
+    msg = tfm_spm_create_msg(service, handle, PSA_IPC_CALL, invecs, in_num,
+                             outvecs, out_num);
+    if (!msg) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        tfm_panic();
+    }
+
+    /*
+     * Send message and wake up the SP who is wating on message queue,
+     * and scheduler triggered
+     */
+    if (tfm_spm_send_event(service, msg) != IPC_SUCCESS) {
+        /* FixMe: Need to refine failure process here. */
+        tfm_panic();
+    }
     return PSA_SUCCESS;
 }
 
 void tfm_svcall_psa_close(uint32_t *args, int32_t ns_caller)
 {
+    psa_handle_t handle;
+    struct tfm_spm_service_t *service;
+    struct tfm_msg_body_t *msg;
+
+    TFM_ASSERT(args != NULL);
+    handle = args[0];
+    /* It will have no effect if called with the NULL handle */
+    if (handle == PSA_NULL_HANDLE) {
+        return;
+    }
+
+    /*
+     * It is a fatal error if an invalid handle was provided that is not the
+     * null handle..
+     */
+    service = tfm_spm_get_service_by_handle(handle);
+    if (!service) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        tfm_panic();
+    }
+
+    /* No input or output needed for close message */
+    msg = tfm_spm_create_msg(service, handle, PSA_IPC_DISCONNECT,
+                             NULL, 0, NULL, 0);
+    if (!msg) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        return;
+    }
+
+    /*
+     * Send message and wake up the SP who is wating on message queue,
+     * and scheduler triggered
+     */
+    tfm_spm_send_event(service, msg);
+
+    /* Service handle is not used anymore */
+    tfm_spm_free_service_handle(service, handle);
 }
 
 /*********************** SVC handler for PSA Service APIs ********************/

zephyrproject-rtos/trusted-firmware-m

secure_fw/core/ipc/tfm_svcalls.c
@@ -33,21 +33,232 @@ uint32_t tfm_svcall_psa_framework_version(void)
 
 uint32_t tfm_svcall_psa_version(uint32_t *args, int32_t ns_caller)
 {
-    return PSA_VERSION_NONE;
+    uint32_t sid;
+    struct tfm_spm_service_t *service;
+
+    TFM_ASSERT(args != NULL);
+    sid = (uint32_t)args[0];
+    /*
+     * It should return PSA_VERSION_NONE if the RoT Service is not
+     * implemented.
+     */
+    service = tfm_spm_get_service_by_sid(sid);
+    if (!service) {
+        return PSA_VERSION_NONE;
+    }
+
+    /*
+     * It should return PSA_VERSION_NONE if the caller is not authorized
+     * to access the RoT Service.
+     */
+    if (ns_caller && !service->service_db->non_secure_client) {
+        return PSA_VERSION_NONE;
+    }
+
+    return service->service_db->minor_version;
 }
 
 psa_handle_t tfm_svcall_psa_connect(uint32_t *args, int32_t ns_caller)
 {
+    uint32_t sid;
+    uint32_t minor_version;
+    struct tfm_spm_service_t *service;
+    struct tfm_msg_body_t *msg;
+
+    TFM_ASSERT(args != NULL);
+    sid = (uint32_t)args[0];
+    minor_version = (uint32_t)args[1];
+
+    /* It is a fatal error if the RoT Service does not exist on the platform */
+    service = tfm_spm_get_service_by_sid(sid);
+    if (!service) {
+        tfm_panic();
+    }
+
+    /*
+     * It is a fatal error if the caller is not authorized to access the RoT
+     * Service.
+     */
+    if (ns_caller && !service->service_db->non_secure_client) {
+        tfm_panic();
+    }
+
+    /*
+     * It is a fatal error if the version of the RoT Service requested is not
+     * supported on the platform.
+     */
+    if (tfm_spm_check_client_version(service, minor_version) != IPC_SUCCESS) {
+        tfm_panic();
+    }
+
+    /* No input or output needed for connect message */
+    msg = tfm_spm_create_msg(service, PSA_NULL_HANDLE, PSA_IPC_CONNECT,
+                             ns_caller, NULL, 0, NULL, 0, NULL);
+    if (!msg) {
+        return PSA_NULL_HANDLE;
+    }
+
+    /*
+     * Send message and wake up the SP who is waiting on message queue,
+     * and scheduler triggered
+     */
+    tfm_spm_send_event(service, msg);
+
     return PSA_NULL_HANDLE;
 }
 
 psa_status_t tfm_svcall_psa_call(uint32_t *args, int32_t ns_caller)
 {
+    psa_handle_t handle;
+    psa_invec *inptr, invecs[PSA_MAX_IOVEC];
+    psa_outvec *outptr, outvecs[PSA_MAX_IOVEC];
+    size_t in_num, out_num;
+    struct tfm_spm_service_t *service;
+    struct tfm_msg_body_t *msg;
+    int i;
+
+    TFM_ASSERT(args != NULL);
+    handle = (psa_handle_t)args[0];
+    if (!ns_caller) {
+        inptr = (psa_invec *)args[1];
+        in_num = (size_t)args[2];
+        outptr = (psa_outvec *)args[3];
+        /*
+         * FixMe: 5th parameter is pushed at stack top before SVC; plus
+         * exception stacked contents, 5th parameter is now at 8th position
+         * in SVC handler. However, if thread mode applies FloatPoint, then
+         * FloatPoint context is pushed into stack and then 5th parameter
+         * will not be args[8].
+         * Will refine it later.
+         */
+         out_num = (size_t)args[8];
+    } else {
+        /*
+         * FixMe: From non-secure caller, vec and len are composed into a new
+         * struct parameter. Need to extract them.
+         */
+        if (tfm_memory_check((void *)args[1], sizeof(uint32_t),
+            ns_caller) != IPC_SUCCESS) {
+            tfm_panic();
+        }
+        if (tfm_memory_check((void *)args[2], sizeof(uint32_t),
+            ns_caller) != IPC_SUCCESS) {
+            tfm_panic();
+        }
+
+        inptr = (psa_invec *)((psa_invec *)args[1])->base;
+        in_num = ((psa_invec *)args[1])->len;
+        outptr = (psa_outvec *)((psa_invec *)args[2])->base;
+        out_num = ((psa_invec *)args[2])->len;
+    }
+
+    /* It is a fatal error if in_len + out_len > PSA_MAX_IOVEC. */
+    if (in_num + out_num > PSA_MAX_IOVEC) {
+        tfm_panic();
+    }
+
+    /* It is a fatal error if an invalid handle was passed. */
+    service = tfm_spm_get_service_by_handle(handle);
+    if (!service) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        tfm_panic();
+    }
+
+    /* It is a fatal error if an invalid memory reference was provide. */
+    if (tfm_memory_check((void *)inptr, in_num * sizeof(psa_invec),
+        ns_caller) != IPC_SUCCESS) {
+        tfm_panic();
+    }
+    if (tfm_memory_check((void *)outptr, out_num * sizeof(psa_outvec),
+        ns_caller) != IPC_SUCCESS) {
+        tfm_panic();
+    }
+
+    tfm_memset(invecs, 0, sizeof(invecs));
+    tfm_memset(outvecs, 0, sizeof(outvecs));
+
+    /* Copy the address out to avoid TOCTOU attacks. */
+    tfm_memcpy(invecs, inptr, in_num * sizeof(psa_invec));
+    tfm_memcpy(outvecs, outptr, out_num * sizeof(psa_outvec));
+
+    /*
+     * It is a fatal error if an invalid payload memory reference
+     * was provided.
+     */
+    for (i = 0; i < in_num; i++) {
+        if (tfm_memory_check((void *)invecs[i].base, invecs[i].len,
+            ns_caller) != IPC_SUCCESS) {
+            tfm_panic();
+        }
+    }
+    for (i = 0; i < out_num; i++) {
+        if (tfm_memory_check(outvecs[i].base, outvecs[i].len,
+            ns_caller) != IPC_SUCCESS) {
+            tfm_panic();
+        }
+    }
+
+    /*
+     * FixMe: Need to check if the message is unrecognized by the RoT
+     * Service or incorrectly formatted.
+     */
+    msg = tfm_spm_create_msg(service, handle, PSA_IPC_CALL, ns_caller, invecs,
+                             in_num, outvecs, out_num, outptr);
+    if (!msg) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        tfm_panic();
+    }
+
+    /*
+     * Send message and wake up the SP who is waiting on message queue,
+     * and scheduler triggered
+     */
+    if (tfm_spm_send_event(service, msg) != IPC_SUCCESS) {
+        /* FixMe: Need to refine failure process here. */
+        tfm_panic();
+    }
     return PSA_SUCCESS;
 }
 
 void tfm_svcall_psa_close(uint32_t *args, int32_t ns_caller)
 {
+    psa_handle_t handle;
+    struct tfm_spm_service_t *service;
+    struct tfm_msg_body_t *msg;
+
+    TFM_ASSERT(args != NULL);
+    handle = args[0];
+    /* It will have no effect if called with the NULL handle */
+    if (handle == PSA_NULL_HANDLE) {
+        return;
+    }
+
+    /*
+     * It is a fatal error if an invalid handle was provided that is not the
+     * null handle..
+     */
+    service = tfm_spm_get_service_by_handle(handle);
+    if (!service) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        tfm_panic();
+    }
+
+    /* No input or output needed for close message */
+    msg = tfm_spm_create_msg(service, handle, PSA_IPC_DISCONNECT, ns_caller,
+                             NULL, 0, NULL, 0, NULL);
+    if (!msg) {
+        /* FixMe: Need to implement one mechanism to resolve this failure. */
+        return;
+    }
+
+    /*
+     * Send message and wake up the SP who is waiting on message queue,
+     * and scheduler triggered
+     */
+    tfm_spm_send_event(service, msg);
+
+    /* Service handle is not used anymore */
+    tfm_spm_free_conn_handle(service, handle);
 }
 
 /*********************** SVC handler for PSA Service APIs ********************/