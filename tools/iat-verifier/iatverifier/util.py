998a09aa6778e6c1790d9faef32aea9ae8622e7a
Sergei Trofimov <sergei.trofimov@arm.com>
2019-04-11 09:13:20 +0100

    Tools: add IAT verifier.
    Add a script for verifying the signatures and structure of Initial
    Attestation Tokens.
    
    Change-Id: Ic3649f25c32edd9b08793eb8a77c8b40dd71e8c8
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

---
+++ tools/iat-verifier/iatverifier/util.py
@@ -0,0 +1,139 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+from collections.abc import Iterable
+
+import cbor
+import yaml
+from ecdsa import SigningKey
+from pycose.sign1message import Sign1Message
+
+from iatverifier import const
+
+
+FIELD_NAMES = {v: k for k, v in const.NAMES.items()}
+
+
+def sign_eat(token, key=None):
+    signed_msg = Sign1Message()
+    signed_msg.payload = token
+    if key:
+        signed_msg.key = key
+        signed_msg.signature = signed_msg.compute_signature()
+    return signed_msg.encode()
+
+
+def convert_map_to_token_files(mapfile, keyfile, outfile):
+    token_map = read_token_map(mapfile)
+
+    with open(keyfile) as fh:
+        signing_key = SigningKey.from_pem(fh.read())
+
+    with open(outfile, 'wb') as wfh:
+        convert_map_to_token(token_map, signing_key, wfh)
+
+
+def convert_map_to_token(token_map, signing_key, wfh):
+    token = cbor.dumps(token_map)
+    signed_token = sign_eat(token, signing_key)
+    wfh.write(signed_token)
+
+
+def convert_token_to_map(raw_data):
+    payload = get_cose_payload(raw_data)
+    token_map = cbor.loads(payload)
+    return _relabel_keys(token_map)
+
+
+def read_token_map(f):
+    if hasattr(f, 'read'):
+        raw = yaml.load(fh)
+    else:
+        with open(f) as fh:
+            raw = yaml.load(fh)
+
+    return _parse_raw_token(raw)
+
+
+def extract_iat_from_cose(keyfile, tokenfile, keep_going=False):
+    if keyfile:
+        try:
+            sk = SigningKey.from_pem(open(keyfile, 'rb').read())
+        except Exception as e:
+            msg = 'Bad key file "{}": {}'
+            error(msg.format(keyfile, e), keep_going)
+    else:  # no keyfile
+        sk = None
+
+    try:
+        with open(tokenfile, 'rb') as wfh:
+            return get_cose_payload(wfh.read(), sk)
+    except Exception as e:
+        msg = 'Bad COSE file "{}": {}'
+        raise ValueError(msg.format(tokenfile, e))
+
+
+def get_cose_payload(cose, sk=None):
+    msg = Sign1Message.decode(cose)
+    if sk:
+        msg.key = sk
+        msg.signature = msg.signers
+        try:
+            msg.verify_signature(alg='ES256')
+        except Exception:
+            raise ValueError('Bad signature')
+    return msg.payload
+
+
+def _parse_raw_token(raw):
+    result = {}
+    for raw_key, raw_value in raw.items():
+        if isinstance(raw_key, int):
+            key = raw_key
+        else:
+            field_name = raw_key.upper()
+            try:
+                key = FIELD_NAMES[field_name]
+            except KeyError:
+                mag = 'Unknown field "{}" in token.'.format(field_name)
+                raise ValueError(msg)
+
+        if key == const.SECURITY_LIFECYCLE:
+            value = (const.SL_NAMES.index(raw_value.upper()) + 1) << const.SL_SHIFT
+        elif hasattr(raw_value, 'items'):
+            value = _parse_raw_token(raw_value)
+        elif (isinstance(raw_value, Iterable) and
+                not isinstance(raw_value, (str, bytes))):
+            # TODO  -- asumes dict elements
+            value = [_parse_raw_token(v) for v in raw_value]
+        else:
+            value = raw_value
+
+        result[key] = value
+
+    return result
+
+
+def _relabel_keys(token_map):
+    result = {}
+    for key, value in token_map.items():
+        if hasattr(value, 'items'):
+            value = _relabel_keys(value)
+        elif (isinstance(value, Iterable) and
+                not isinstance(value, (str, bytes))):
+            # TODO  -- asumes dict elements
+            value = [_relabel_keys(v) for v in value]
+
+        if key == const.SECURITY_LIFECYCLE:
+            value = (const.SL_NAMES[(value >> const.SL_SHIFT) - 1])
+
+        if key in const.NAMES:
+            new_key = const.NAMES[key].lower()
+        else:
+            new_key = key
+        result[new_key] = value
+    return result
