2a68899a63df66f68b63c45c33909dcd764703ff
Tamas Ban <tamas.ban@arm.com>
2019-05-10 10:39:15 +0100

    Crypto: Increase the buffer size for memory allocation
    Increase the size of static buffer to 8KB which is used by
    Mbed Crypto to dynamically allocate memory. It is required
    due to the ECDSA (NIST P-256) signing of initial attestation
    token.
    
    Change-Id: I22d916140b0b65fc590de545cf7a086de0acb5cb
    Signed-off-by: Tamas Ban <tamas.ban@arm.com>

--- secure_fw/services/crypto/crypto_init.c
+++ secure_fw/services/crypto/crypto_init.c
@@ -241,7 +241,7 @@ static void tfm_crypto_ipc_handler(void)
  *        Crypto for its dynamic allocations
  */
 #ifndef TFM_CRYPTO_ENGINE_BUF_SIZE
-#define TFM_CRYPTO_ENGINE_BUF_SIZE (1024)
+#define TFM_CRYPTO_ENGINE_BUF_SIZE (0x2000) /* 8KB for EC signing in attest */
 #endif
 
 /**
