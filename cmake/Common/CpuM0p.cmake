e3f08fd21625bd05a7f9599e3b8ac896c9ee908f
Antonio de Angelis <antonio.deangelis@arm.com>
2019-07-25 17:26:23 +0100

    Build: Add support for GNUARM 8.3.1
    This patch adds support for the arm-none-eabi-gcc toolchain
    version 8.3.1.
    
    Change-Id: I1d8302e6463aa49f5f1e236ea27fdc0f27b0ee44
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>

---
+++ cmake/Common/CpuM0p.cmake
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+#This file gathers Cortex-M0plus specific settings which control the build system.
+set(ARM_CPU_ARCHITECTURE "ARMv6-M")
+
+set(ARM_CPU_TYPE "Cortex-M0p")
---
+++ cmake/Common/CpuM4.cmake
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+#This file gathers Cortex-M4 specific settings which control the build system.
+set(ARM_CPU_ARCHITECTURE "ARMv7-M")
+
+set(ARM_CPU_TYPE "Cortex-M4")
