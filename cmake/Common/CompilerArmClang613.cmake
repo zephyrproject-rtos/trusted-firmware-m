06fa1e4bd1738b0ef55d7e804d8e943046b76618
Ronald Cron <ronald.cron@arm.com>
2019-09-23 10:05:29 +0200

    Build: Add support for Armv8.1 architecture
    Change-Id: I74c59cf816c5da7d90904eaa3027dd10adda8472
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- cmake/Common/CompilerArmClang613.cmake
+++ cmake/Common/CompilerArmClang613.cmake
@@ -15,6 +15,35 @@ check_armclang_input_vars("6.13")
 
 if(NOT DEFINED ARM_CPU_ARCHITECTURE)
 	set(_NO_ARM_CPU_ARCHITECTURE true)
+elseif(${ARM_CPU_ARCHITECTURE} STREQUAL "ARMv8.1-M.MAIN")
+	string_append_unique_item(STRING CMAKE_C_FLAGS_CPU KEY "--target=" VAL "--target=arm-arm-none-eabi")
+	string_append_unique_item(STRING CMAKE_C_FLAGS_CPU KEY "-march=" VAL "-march=armv8.1-m.main+mve")
+	string_append_unique_item(STRING CMAKE_CXX_FLAGS_CPU KEY "--target=" VAL "--target=arm-arm-none-eabi")
+	string_append_unique_item(STRING CMAKE_CXX_FLAGS_CPU KEY "-march=" VAL "-march=armv8.1-m.main+mve")
+	string_append_unique_item(STRING CMAKE_ASM_FLAGS_CPU KEY "--cpu=" VAL "--cpu=8.1-M.Main.dsp")
+	string_append_unique_item(STRING CMAKE_LINK_FLAGS_CPU KEY "--cpu=" VAL "--cpu=8.1-M.Main.dsp")
+
+# FIXME
+# When compiling for a given architecture, compilers define a flag identifying
+# the architecture like __ARM_ARCH_7M__ or __ARM_ARCH_8M_MAIN__ or
+# __ARM_ARCH_8_1M_MAIN__. Those flags are used in CMSIS headers like
+# cmsis_armclang.h or cmsis_gcc.h to restrict the definition of some symbols
+# to particular architectures. For the new Armv8.1-M.Main architecture, some
+# CMSIS headers like cmsis_armclang.h or cmsis_gcc.h have not been updated yet
+# to use __ARM_ARCH_8_1M_MAIN__. To mitigate this, core_armv81mml.h header
+# defines __ARM_ARCH_8M_MAIN__: #define __ARM_ARCH_8M_MAIN__ 1. Concerning
+# cmsis_armclang.h or cmsis_gcc.h includes, for this solution to work, wherever
+# cmsis_compiler.h (which includes cmsis_armclang.h or cmsis_gcc.h) is included
+# core_armv81mml.h has to be included as well. This is not the case in several
+# places in TF-M code leading to compilation failures for some C files for
+# Armv8.1-M architecture. When the CMSIS headers are updated to fully support
+# the __ARM_ARCH_8_1M_MAIN__ flag, the compilation issues should disappear thus
+# we don't want to change the header inclusions in TF-M code. Thus the below
+# definition as a compile parameter of _ARM_ARCH_8M_MAIN__ that must be removed
+# in the future.
+
+	add_definitions(-D__ARM_ARCH_8M_MAIN__=1)
+
 elseif (${ARM_CPU_ARCHITECTURE} STREQUAL "ARMv8-M.BASE")
 	string_append_unique_item(STRING CMAKE_C_FLAGS_CPU KEY "--target=" VAL "--target=arm-arm-none-eabi")
 	string_append_unique_item(STRING CMAKE_C_FLAGS_CPU KEY "-march=" VAL "-march=armv8-m.base")
