edde1de9ace60db8c679d38f335bd7f7f6c86efd
Kevin Peng <kevin.peng@arm.com>
2019-10-25 17:12:45 +0800

    Build: set SST_TEST_NV_COUNTERS for SST regression
    The SST_TEST_NV_COUNTERS should be set to ON only when SST regression
    is ON for all level of isolation.
    This patch first moves out the test configurations out of
    test/CMakeLists.txt to a seperate file for CommonConfig.cmake to
    include and then set SST_TEST_NV_COUNTERS to on when SST regression
    is on.
    
    Change-Id: Icc7c7086023d73f2570ef27233abc2cdc0c480e0
    Signed-off-by: Kevin Peng <kevin.peng@arm.com>

---
+++ test/TestConfig.cmake
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+option(ENABLE_SECURE_STORAGE_SERVICE_TESTS "Option for secure storage service tests" TRUE)
+option(ENABLE_INTERNAL_TRUSTED_STORAGE_SERVICE_TESTS "Option for internal trusted storage services tests" TRUE)
+option(ENABLE_AUDIT_LOGGING_SERVICE_TESTS "Option for audit logging service tests" TRUE)
+option(ENABLE_CRYPTO_SERVICE_TESTS "Option for crypto service tests" TRUE)
+option(ENABLE_ATTESTATION_SERVICE_TESTS "Option for attestation service tests" TRUE)
+option(ENABLE_PLATFORM_SERVICE_TESTS "Option for platform service tests" TRUE)
+option(ENABLE_QCBOR_TESTS "Option for qcbor tests" TRUE)
