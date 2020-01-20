7224d32f77a2338f77c74c314605ac178049a9ee
Chris Brand <chris.brand@cypress.com>
2020-01-20 13:39:08 -0800

    Platform: PSoC64: Tweak SMPU configuration
    Remove protection from the secondary secure image to support
    upgrading it.
    Re-purpose SMPU1 to cover just the ITS part of flash.
    Re-purpose SMPU2 to cover just the NV Counters in flash.
    Re-purpose SMPU3 to cover just the SST part of flash.
    This also removes protection from the unused space in flash.
    
    Change-Id: Id9517b85db426b760069d7133651bd362d21eba9
    Signed-off-by: Chris Brand <chris.brand@cypress.com>

--- platform/ext/target/cypress/psoc64/smpu_config.h
+++ platform/ext/target/cypress/psoc64/smpu_config.h
@@ -69,10 +69,9 @@
 #error "Flash layout has changed - FLASH_S_PARTITION_SIZE isn't 5/8 of SMPU0_REGIONSIZE"
 #endif
 
-/* SMPU1 - start of secondary images in Flash */
-#define SMPU1_BASE         S_ROM_ALIAS(NON_SECURE_IMAGE_OFFSET + \
-                                       NON_SECURE_IMAGE_MAX_SIZE)
-#define SMPU1_REGIONSIZE   PROT_SIZE_64KB_BIT_SHIFT
+/* SMPU1 - Internal Trusted Storage in Flash */
+#define SMPU1_BASE         S_ROM_ALIAS(FLASH_ITS_AREA_OFFSET)
+#define SMPU1_REGIONSIZE   PROT_SIZE_16KB_BIT_SHIFT
 #define SMPU1_SLAVE_CONFIG {\
     .address = (void *)SMPU1_BASE, \
     .regionSize = SMPU1_REGIONSIZE, \
@@ -90,12 +89,14 @@
 #error "Flash layout has changed - SMPU1 needs updating"
 #endif
 
-/* SMPU2 - remainder of secondary images, plus secure storage, NV counters,
- * and unused space in Flash
- */
-/* SMPU2 should start immediately after SMPU1 */
-#define SMPU2_BASE         (SMPU1_BASE + REGIONSIZE_TO_BYTES(SMPU1_REGIONSIZE))
-#define SMPU2_REGIONSIZE   PROT_SIZE_512KB_BIT_SHIFT
+/* Should exactly cover the ITS region */
+#if FLASH_ITS_AREA_SIZE != REGIONSIZE_TO_BYTES(SMPU1_REGIONSIZE)
+#error "Flash layout has changed - SMPU1_REGIONSIZE isn't FLASH_ITS_AREA_SIZE"
+#endif
+
+/* SMPU2 - NV counters in Flash */
+#define SMPU2_BASE         S_ROM_ALIAS(FLASH_NV_COUNTERS_AREA_OFFSET)
+#define SMPU2_REGIONSIZE   PROT_SIZE_512B_BIT_SHIFT
 #define SMPU2_SLAVE_CONFIG {\
     .address = (void *)SMPU2_BASE, \
     .regionSize = SMPU2_REGIONSIZE, \
@@ -111,6 +112,48 @@
 /* SMPU requires base address aligned to size */
 #if SMPU2_BASE % REGIONSIZE_TO_BYTES(SMPU2_REGIONSIZE)
 #error "Flash layout has changed - SMPU2 needs updating"
+#endif
+
+/* Should exactly cover the NV Counters region */
+#if FLASH_NV_COUNTERS_AREA_SIZE != REGIONSIZE_TO_BYTES(SMPU2_REGIONSIZE)
+#error "Flash layout has changed - SMPU2_REGIONSIZE isn't FLASH_NV_COUNTERS_AREA_SIZE"
+#endif
+
+/* SMPU3 - SST in Flash */
+#define SMPU3_BASE         S_ROM_ALIAS(0x1c8000)
+#define SMPU3_REGIONSIZE   PROT_SIZE_32KB_BIT_SHIFT
+#define SMPU3_SUBREGION_DIS (CY_PROT_SUBREGION_DIS0 | \
+                             CY_PROT_SUBREGION_DIS1 | \
+                             CY_PROT_SUBREGION_DIS2)
+#define SMPU3_SLAVE_CONFIG {\
+    .address = (void *)SMPU3_BASE, \
+    .regionSize = SMPU3_REGIONSIZE, \
+    .subregions = SMPU3_SUBREGION_DIS, \
+    .userPermission = CY_PROT_PERM_RW, \
+    .privPermission = CY_PROT_PERM_RW, \
+    .secure = false, \
+    .pcMatch = false, \
+    .pcMask = SECURE_PCS_MASK, \
+}
+#define SMPU3_MASTER_CONFIG COMMON_SMPU_MASTER_CONFIG
+
+/* SMPU requires base address aligned to size */
+#if SMPU3_BASE % REGIONSIZE_TO_BYTES(SMPU3_REGIONSIZE)
+#error "Flash layout has changed - SMPU3 needs updating"
+#endif
+
+/*
+ * SST_FLASH_AREA_ADDR must equal the base address of subregion 3 of
+ * SMPU3
+ */
+#if S_ROM_ALIAS(SST_FLASH_AREA_ADDR) != (SMPU3_BASE + \
+                          (3 * REGIONSIZE_TO_BYTES(SMPU3_REGIONSIZE) / 8))
+#error "Flash layout has changed - S_DATA_PRIV_START isn't subregion 3 of SMPU3"
+#endif
+
+/* Should exactly cover the SST region */
+#if FLASH_SST_AREA_SIZE != (5 * REGIONSIZE_TO_BYTES(SMPU3_REGIONSIZE) / 8)
+#error "Flash layout has changed - SMPU3_REGIONSIZE isn't FLASH_SST_AREA_SIZE"
 #endif
 
 /* SMPU6 - 64KB of unprivileged secure data in SRAM */
