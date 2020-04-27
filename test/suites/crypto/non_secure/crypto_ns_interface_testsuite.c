0d4047702fd67a6be09a8ae98999b80f6ab55faa
David Hu <david.hu@arm.com>
2020-04-27 13:18:10 +0800

    Test: Add config flags to control build of Crypto test cases
    Add several configuration flags to control the build of Crypto
    test cases.
    TF-M Profiles can skip Crypto test cases in which the algorithms
    are not supported.
    Common Crypto tests will be optimized in further patches.
    
    Change-Id: I48f7609858f65d321767af9a95a8894929f32e9f
    Signed-off-by: David Hu <david.hu@arm.com>

--- test/suites/crypto/non_secure/crypto_ns_interface_testsuite.c
+++ test/suites/crypto/non_secure/crypto_ns_interface_testsuite.c
@@ -11,73 +11,117 @@
 
 /* List of tests */
 static void tfm_crypto_test_6001(struct test_result_t *ret);
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
 static void tfm_crypto_test_6002(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
+#ifdef TFM_CRYPTO_TEST_ALG_CFB
 static void tfm_crypto_test_6003(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CFB */
+#ifdef TFM_CRYPTO_TEST_ALG_CTR
 static void tfm_crypto_test_6005(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CTR */
+#ifdef TFM_CRYPTO_TEST_ALG_GCM
 static void tfm_crypto_test_6007(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_GCM */
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
 static void tfm_crypto_test_6008(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
+#ifdef TFM_CRYPTO_TEST_ALG_CFB
 static void tfm_crypto_test_6009(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CFB */
 static void tfm_crypto_test_6010(struct test_result_t *ret);
 static void tfm_crypto_test_6011(struct test_result_t *ret);
 static void tfm_crypto_test_6012(struct test_result_t *ret);
+#ifdef TFM_CRYPTO_TEST_ALG_SHA_512
 static void tfm_crypto_test_6013(struct test_result_t *ret);
 static void tfm_crypto_test_6014(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_SHA_512 */
 static void tfm_crypto_test_6019(struct test_result_t *ret);
 static void tfm_crypto_test_6020(struct test_result_t *ret);
+#ifdef TFM_CRYPTO_TEST_ALG_SHA_512
 static void tfm_crypto_test_6021(struct test_result_t *ret);
 static void tfm_crypto_test_6022(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_SHA_512 */
 static void tfm_crypto_test_6024(struct test_result_t *ret);
+#ifdef TFM_CRYPTO_TEST_ALG_CCM
 static void tfm_crypto_test_6030(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CCM */
+#ifdef TFM_CRYPTO_TEST_ALG_GCM
 static void tfm_crypto_test_6031(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_GCM */
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
 static void tfm_crypto_test_6032(struct test_result_t *ret);
 static void tfm_crypto_test_6033(struct test_result_t *ret);
 static void tfm_crypto_test_6034(struct test_result_t *ret);
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
 
 static struct test_t crypto_tests[] = {
     {&tfm_crypto_test_6001, "TFM_CRYPTO_TEST_6001",
      "Non Secure Key management interface", {TEST_PASSED} },
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
     {&tfm_crypto_test_6002, "TFM_CRYPTO_TEST_6002",
      "Non Secure Symmetric encryption (AES-128-CBC) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
+#ifdef TFM_CRYPTO_TEST_ALG_CFB
     {&tfm_crypto_test_6003, "TFM_CRYPTO_TEST_6003",
      "Non Secure Symmetric encryption (AES-128-CFB) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CFB */
+#ifdef TFM_CRYPTO_TEST_ALG_CTR
     {&tfm_crypto_test_6005, "TFM_CRYPTO_TEST_6005",
      "Non Secure Symmetric encryption (AES-128-CTR) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CTR */
+#ifdef TFM_CRYPTO_TEST_ALG_GCM
     {&tfm_crypto_test_6007, "TFM_CRYPTO_TEST_6007",
      "Non Secure Symmetric encryption invalid cipher (AES-128-GCM)", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_GCM */
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
     {&tfm_crypto_test_6008, "TFM_CRYPTO_TEST_6008",
      "Non Secure Symmetric encryption invalid cipher (AES-152-CBC)", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
+#ifdef TFM_CRYPTO_TEST_ALG_CFB
     {&tfm_crypto_test_6009, "TFM_CRYPTO_TEST_6009",
      "Non Secure Symmetric encryption invalid cipher (HMAC-128-CFB)", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CFB */
     {&tfm_crypto_test_6010, "TFM_CRYPTO_TEST_6010",
      "Non Secure Unsupported Hash (SHA-1) interface", {TEST_PASSED} },
     {&tfm_crypto_test_6011, "TFM_CRYPTO_TEST_6011",
      "Non Secure Hash (SHA-224) interface", {TEST_PASSED} },
     {&tfm_crypto_test_6012, "TFM_CRYPTO_TEST_6012",
      "Non Secure Hash (SHA-256) interface", {TEST_PASSED} },
+#ifdef TFM_CRYPTO_TEST_ALG_SHA_512
     {&tfm_crypto_test_6013, "TFM_CRYPTO_TEST_6013",
      "Non Secure Hash (SHA-384) interface", {TEST_PASSED} },
     {&tfm_crypto_test_6014, "TFM_CRYPTO_TEST_6014",
      "Non Secure Hash (SHA-512) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_SHA_512 */
     {&tfm_crypto_test_6019, "TFM_CRYPTO_TEST_6019",
      "Non Secure Unsupported HMAC (SHA-1) interface", {TEST_PASSED} },
     {&tfm_crypto_test_6020, "TFM_CRYPTO_TEST_6020",
      "Non Secure HMAC (SHA-256) interface", {TEST_PASSED} },
+#ifdef TFM_CRYPTO_TEST_ALG_SHA_512
     {&tfm_crypto_test_6021, "TFM_CRYPTO_TEST_6021",
      "Non Secure HMAC (SHA-384) interface", {TEST_PASSED} },
     {&tfm_crypto_test_6022, "TFM_CRYPTO_TEST_6022",
      "Non Secure HMAC (SHA-512) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_SHA_512 */
     {&tfm_crypto_test_6024, "TFM_CRYPTO_TEST_6024",
      "Non Secure HMAC with long key (SHA-224) interface", {TEST_PASSED} },
+#ifdef TFM_CRYPTO_TEST_ALG_CCM
     {&tfm_crypto_test_6030, "TFM_CRYPTO_TEST_6030",
      "Non Secure AEAD (AES-128-CCM) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CCM */
+#ifdef TFM_CRYPTO_TEST_ALG_GCM
     {&tfm_crypto_test_6031, "TFM_CRYPTO_TEST_6031",
      "Non Secure AEAD (AES-128-GCM) interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_GCM */
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
     {&tfm_crypto_test_6032, "TFM_CRYPTO_TEST_6032",
      "Non Secure key policy interface", {TEST_PASSED} },
     {&tfm_crypto_test_6033, "TFM_CRYPTO_TEST_6033",
      "Non Secure key policy check permissions", {TEST_PASSED} },
     {&tfm_crypto_test_6034, "TFM_CRYPTO_TEST_6034",
      "Non Secure persistent key interface", {TEST_PASSED} },
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
 };
 
 void register_testsuite_ns_crypto_interface(struct test_suite_t *p_test_suite)
@@ -102,37 +146,49 @@ static void tfm_crypto_test_6001(struct test_result_t *ret)
     psa_key_interface_test(PSA_KEY_TYPE_AES, ret);
 }
 
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
 static void tfm_crypto_test_6002(struct test_result_t *ret)
 {
     psa_cipher_test(PSA_KEY_TYPE_AES, PSA_ALG_CBC_NO_PADDING, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
 
+#ifdef TFM_CRYPTO_TEST_ALG_CFB
 static void tfm_crypto_test_6003(struct test_result_t *ret)
 {
     psa_cipher_test(PSA_KEY_TYPE_AES, PSA_ALG_CFB, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CFB */
 
+#ifdef TFM_CRYPTO_TEST_ALG_CTR
 static void tfm_crypto_test_6005(struct test_result_t *ret)
 {
     psa_cipher_test(PSA_KEY_TYPE_AES, PSA_ALG_CTR, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CTR */
 
+#ifdef TFM_CRYPTO_TEST_ALG_GCM
 static void tfm_crypto_test_6007(struct test_result_t *ret)
 {
     /* GCM is an AEAD mode */
     psa_invalid_cipher_test(PSA_KEY_TYPE_AES, PSA_ALG_GCM, 16, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_GCM */
 
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
 static void tfm_crypto_test_6008(struct test_result_t *ret)
 {
     psa_invalid_key_length_test(ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
 
+#ifdef TFM_CRYPTO_TEST_ALG_CFB
 static void tfm_crypto_test_6009(struct test_result_t *ret)
 {
     /* HMAC is not a block cipher */
     psa_invalid_cipher_test(PSA_KEY_TYPE_HMAC, PSA_ALG_CFB, 16, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CFB */
 
 static void tfm_crypto_test_6010(struct test_result_t *ret)
 {
@@ -149,6 +205,7 @@ static void tfm_crypto_test_6012(struct test_result_t *ret)
     psa_hash_test(PSA_ALG_SHA_256, ret);
 }
 
+#ifdef TFM_CRYPTO_TEST_ALG_SHA_512
 static void tfm_crypto_test_6013(struct test_result_t *ret)
 {
     psa_hash_test(PSA_ALG_SHA_384, ret);
@@ -158,6 +215,7 @@ static void tfm_crypto_test_6014(struct test_result_t *ret)
 {
     psa_hash_test(PSA_ALG_SHA_512, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_SHA_512 */
 
 static void tfm_crypto_test_6019(struct test_result_t *ret)
 {
@@ -170,6 +228,7 @@ static void tfm_crypto_test_6020(struct test_result_t *ret)
     psa_mac_test(PSA_ALG_HMAC(PSA_ALG_SHA_256), 0, ret);
 }
 
+#ifdef TFM_CRYPTO_TEST_ALG_SHA_512
 static void tfm_crypto_test_6021(struct test_result_t *ret)
 {
     psa_mac_test(PSA_ALG_HMAC(PSA_ALG_SHA_384), 0, ret);
@@ -179,21 +238,28 @@ static void tfm_crypto_test_6022(struct test_result_t *ret)
 {
     psa_mac_test(PSA_ALG_HMAC(PSA_ALG_SHA_512), 0, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_SHA_512 */
+
 static void tfm_crypto_test_6024(struct test_result_t *ret)
 {
     psa_mac_test(PSA_ALG_HMAC(PSA_ALG_SHA_224), 1, ret);
 }
 
+#ifdef TFM_CRYPTO_TEST_ALG_CCM
 static void tfm_crypto_test_6030(struct test_result_t *ret)
 {
     psa_aead_test(PSA_KEY_TYPE_AES, PSA_ALG_CCM, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CCM */
 
+#ifdef TFM_CRYPTO_TEST_ALG_GCM
 static void tfm_crypto_test_6031(struct test_result_t *ret)
 {
     psa_aead_test(PSA_KEY_TYPE_AES, PSA_ALG_GCM, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_GCM */
 
+#ifdef TFM_CRYPTO_TEST_ALG_CBC
 static void tfm_crypto_test_6032(struct test_result_t *ret)
 {
     psa_policy_key_interface_test(ret);
@@ -208,3 +274,4 @@ static void tfm_crypto_test_6034(struct test_result_t *ret)
 {
     psa_persistent_key_test(1, ret);
 }
+#endif /* TFM_CRYPTO_TEST_ALG_CBC */
