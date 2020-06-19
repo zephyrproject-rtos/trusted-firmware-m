8f7ab36035c4b0f18fc916e10bff244ce76a4d20
Summer Qin <summer.qin@arm.com>
2020-06-19 16:25:31 +0800

    SPM: Move library specific functions into model_func
    Move 'tfm_core_sfn_request' and 'priv_irq_handler_main' into
    model_func folder.
    
    Change-Id: Iee3d7c672cd1bfb169761c6251f6796e29f15e27
    Signed-off-by: Summer Qin <summer.qin@arm.com>

--- secure_fw/spm/arch/tfm_arch_v8m_main.c
+++ secure_fw/spm/arch/tfm_arch_v8m_main.c
@@ -88,61 +88,6 @@ void tfm_arch_init_actx(struct tfm_arch_ctx_t *p_actx,
     p_actx->sp_limit = sp_limit;
     p_actx->lr = EXC_RETURN_THREAD_S_PSP;
 }
-#else
-__attribute__((section("SFN"), naked))
-int32_t tfm_core_sfn_request(const struct tfm_sfn_req_s *desc_ptr)
-{
-    __ASM volatile(
-        "PUSH   {r4-r12, lr}                \n"
-        "SVC    %[SVC_REQ]                  \n"
-        "MOV    r4,  #0                     \n"
-        "MOV    r5,  r4                     \n"
-        "MOV    r6,  r4                     \n"
-        "MOV    r7,  r4                     \n"
-        "MOV    r8,  r4                     \n"
-        "MOV    r9,  r4                     \n"
-        "MOV    r10, r4                     \n"
-        "MOV    r11, r4                     \n"
-        "BLX    lr                          \n"
-        "SVC    %[SVC_RET]                  \n"
-        "POP    {r4-r12, pc}                \n"
-        : : [SVC_REQ] "I" (TFM_SVC_SFN_REQUEST),
-            [SVC_RET] "I" (TFM_SVC_SFN_RETURN)
-        );
-}
-
-__attribute__((section("SFN"), naked))
-void priv_irq_handler_main(uint32_t partition_id,
-                                                  uint32_t unpriv_handler,
-                                                  uint32_t irq_signal,
-                                                  uint32_t irq_line)
-{
-    __ASM(
-          /* Save the callee saved registers*/
-          "PUSH   {r4-r12, lr}              \n"
-          /* Request SVC to configure environment for the unpriv IRQ handler */
-          "SVC    %[SVC_REQ]                \n"
-          /* clear the callee saved registers to prevent information leak */
-          "MOV    r4,  #0                   \n"
-          "MOV    r5,  r4                   \n"
-          "MOV    r6,  r4                   \n"
-          "MOV    r7,  r4                   \n"
-          "MOV    r8,  r4                   \n"
-          "MOV    r9,  r4                   \n"
-          "MOV    r10, r4                   \n"
-          "MOV    r11, r4                   \n"
-          /* Branch to the unprivileged handler */
-          "BLX    lr                        \n"
-          /* Request SVC to reconfigure the environment of the interrupted
-           * partition
-           */
-          "SVC    %[SVC_RET]                \n"
-            /* restore callee saved registers and return */
-          "POP    {r4-r12, pc}              \n"
-          : : [SVC_REQ] "I" (TFM_SVC_DEPRIV_REQ)
-            , [SVC_RET] "I" (TFM_SVC_DEPRIV_RET)
-          );
-}
 #endif
 
 /**
