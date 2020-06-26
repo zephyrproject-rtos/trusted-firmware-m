24ed5d6f6a0ca2087c7c4f687bbab3c1f88e192d
Kumar Gala <kumar.gala@linaro.org>
2020-06-25 22:14:14 -0500

    init-git.sh: Update for TFM v1.1-RC1
    * Remove CMSIS_5 (this is only need for tf-m-tests and is pulled into
      that repo
    * Added tfm-test and mcuboot as they are needeed for TFM v1.1-RC1 to
      build
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- init-git.sh
+++ init-git.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-TFM_GIT_SHA=TF-Mv1.0
-CMSIS_5_GIT_SHA=5.5.0
+TFM_GIT_SHA=TF-Mv1.1-RC1
+TFM_TESTS_GIT_SHA=TF-Mv1.1-RC1
+MCUBOOT_GIT_SHA=v1.6.0
 MBED_CRYPTO_GIT_SHA=mbedcrypto-3.0.1
 PSA_ARCH_TESTS_GIT_SHA=v20.03_API1.0
 
@@ -13,19 +14,25 @@ if [ ! -d trusted-firmware-m/.git ]; then
    cd ..
 fi
 
-if [ ! -d CMSIS_5/.git ]; then
-   git clone https://github.com/ARM-software/CMSIS_5.git -b ${CMSIS_5_GIT_SHA} --bare CMSIS_5/.git
-   cd CMSIS_5
-   git init
-   git checkout -f ${CMSIS_5_GIT_SHA} CMSIS/RTOS2
-   git lfs fetch
-   git lfs checkout CMSIS/RTOS2
-   cd ..
-fi
-
 if [ ! -d mbed-crypto/.git ]; then
    git clone https://github.com/ARMmbed/mbed-crypto.git -b ${MBED_CRYPTO_GIT_SHA} --bare mbed-crypto/.git
    cd mbed-crypto
+   git init
+   git checkout -f
+   cd ..
+fi
+
+if [ ! -d mcuboot/.git ]; then
+   git clone https://github.com/JuulLabs-OSS/mcuboot.git -b v1.4.0 -b ${MCUBOOT_GIT_SHA} --bare mcuboot/.git
+   cd mcuboot
+   git init
+   git checkout -f
+   cd ..
+fi
+
+if [ ! -d tf-m-tests/.git ]; then
+   git clone https://git.trustedfirmware.org/TF-M/tf-m-tests.git -b ${TFM_TESTS_GIT_SHA} --bare tf-m-tests/.git
+   cd tf-m-tests
    git init
    git checkout -f
    cd ..
