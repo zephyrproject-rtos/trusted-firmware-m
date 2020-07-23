15d2e4b937549ac8fd009cc2b32d16eed29ab54c
Michel Jaouen <michel.jaouen@st.com>
2020-07-23 13:10:08 +0200

    Platform: cmsis: Fix _cmsis_start for gcc C startup support
    __copy_table and __zero_table provides a length in bytes not in word
    
    Change-Id: I1f33aeeea94499aeeb78fbfc404ad8608a6fe5ea
    Signed-off-by: Michel Jaouen <michel.jaouen@st.com>

--- platform/ext/cmsis/cmsis_gcc.h
+++ platform/ext/cmsis/cmsis_gcc.h
@@ -149,13 +149,13 @@ __STATIC_FORCEINLINE __NO_RETURN void __cmsis_start(void)
   extern const __zero_table_t __zero_table_end__;
 
   for (__copy_table_t const* pTable = &__copy_table_start__; pTable < &__copy_table_end__; ++pTable) {
-    for(uint32_t i=0u; i<pTable->wlen; ++i) {
+    for(uint32_t i=0u; i<(pTable->wlen/sizeof(pTable->dest)); ++i) {
       pTable->dest[i] = pTable->src[i];
     }
   }
  
   for (__zero_table_t const* pTable = &__zero_table_start__; pTable < &__zero_table_end__; ++pTable) {
-    for(uint32_t i=0u; i<pTable->wlen; ++i) {
+    for(uint32_t i=0u; i<(pTable->wlen/sizeof(pTable->dest)); ++i) {
       pTable->dest[i] = 0u;
     }
   }
