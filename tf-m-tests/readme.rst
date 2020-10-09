5697c95a31a4402d21a68208629b4dae53d9623c
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2020-10-09 23:22:16 +0200

    trusted-firmware-m: tf-m-tests: synchronize with upstream
    Synchronize the tf-m-test directory with the
    corresponding upstream repository up to commit
    652bb8a1502c95bfd1ee8e44ccd1126ce74ae41f
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- tf-m-tests/readme.rst
+++ tf-m-tests/readme.rst
@@ -2,11 +2,24 @@
 Trusted Firmware-M Tests
 ########################
 
-The Trusted Firmware-M Tests repo is meant to hold various tests for the
+The Trusted Firmware-M(TF-M) Tests repo is meant to hold various tests for the
 `Trusted Firmware-M`_.
-Currently it only contains the CMSIS RTX libraries, which is its the dependency.
-And the tests are within `test` folder in `Trusted Firmware-M`_ which will be
-moved to this repo gradually.
+The TF-M tests mainly focus on functionalities of various TF-M componentes such
+as the TF-M core and various secure partitions.
+
+There is also the `psa-arch-tests`_ suite which mainly focuses on the
+implementation compliance of the Platform Security Architecture(PSA).
+
+****************
+Folder Structure
+****************
+
+- app - The application code that executes the testing, including both the TF-M
+  tests and the `psa-arch-tests`_.
+- CMSIS - The libraries of RTX which is the Non-secure RTOS of the tests
+- test - The TF-M test codes including test framework, test suites and test
+  services
+
 
 ####################
 Feedback and support
@@ -15,5 +28,6 @@ Feedback can be submitted via email to
 `TF-M mailing list <tf-m@lists.trustedfirmware.org>`__.
 
 .. _Trusted Firmware-M: https://git.trustedfirmware.org/TF-M/trusted-firmware-m.git/
+.. _psa-arch-tests: https://github.com/ARM-software/psa-arch-tests
 
 *Copyright (c) 2020, Arm Limited. All rights reserved.*
---
+++ tf-m-tests/test/framework/secure_suites.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2017-2020, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "test_framework_integ_test.h"
+#include "test_framework_integ_test_helper.h"
+#include "test_framework.h"
+
+/* Service specific includes */
+#ifdef TFM_PARTITION_PROTECTED_STORAGE
+#include "ps_tests.h"
+#endif
+#ifdef TFM_PARTITION_INTERNAL_TRUSTED_STORAGE
+#include "its_s_tests.h"
+#endif
+#ifdef TFM_PARTITION_INITIAL_ATTESTATION
+#include "attest_s_tests.h"
+#endif
+#ifdef TFM_PARTITION_CRYPTO
+#include "crypto_s_tests.h"
+#endif
+#ifdef TFM_PARTITION_PLATFORM
+#include "platform_s_tests.h"
+#endif
+#ifdef TFM_PSA_API
+#include "ipc_s_tests.h"
+#else
+#ifdef TFM_PARTITION_AUDIT_LOG
+#include "audit_s_tests.h"
+#endif
+#endif /* TFM_PSA_API */
+
+static struct test_suite_t test_suites[] = {
+
+#ifdef TFM_PARTITION_PROTECTED_STORAGE
+    {&register_testsuite_s_psa_ps_interface, 0, 0, 0},
+    {&register_testsuite_s_psa_ps_reliability, 0, 0, 0},
+
+#ifdef PS_TEST_NV_COUNTERS
+    {&register_testsuite_s_rollback_protection, 0, 0, 0},
+#endif
+#endif
+
+#ifdef TFM_PARTITION_INTERNAL_TRUSTED_STORAGE
+    /* Secure ITS test cases */
+    {&register_testsuite_s_psa_its_interface, 0, 0, 0},
+    {&register_testsuite_s_psa_its_reliability, 0, 0, 0},
+#endif
+
+#ifdef TFM_PARTITION_CRYPTO
+    /* Crypto test cases */
+    {&register_testsuite_s_crypto_interface, 0, 0, 0},
+#endif
+
+#ifdef TFM_PARTITION_INITIAL_ATTESTATION
+    /* Secure initial attestation service test cases */
+    {&register_testsuite_s_attestation_interface, 0, 0, 0},
+#endif
+
+#ifdef TFM_PARTITION_PLATFORM
+    /* Secure platform service test cases */
+    {&register_testsuite_s_platform_interface, 0, 0, 0},
+#endif
+
+#ifdef TFM_PARTITION_AUDIT_LOG
+    /* Secure Audit Logging test cases */
+    {&register_testsuite_s_audit_interface, 0, 0, 0},
+#endif
+
+#ifdef ENABLE_IPC_TEST
+    /* Secure IPC test cases */
+    {&register_testsuite_s_ipc_interface, 0, 0, 0},
+#endif
+    /* End of test suites */
+    {0, 0, 0, 0}
+};
+
+static void setup_integ_test(void)
+{
+    /* Left empty intentionally, currently implemented
+     * test suites require no setup
+     */
+}
+
+static void tear_down_integ_test(void)
+{
+    /* Left empty intentionally, currently implemented
+     * test suites require no tear down
+     */
+}
+
+enum test_suite_err_t start_integ_test(void)
+{
+    enum test_suite_err_t retval;
+
+    setup_integ_test();
+    retval = integ_test("Secure", test_suites);
+    tear_down_integ_test();
+    return retval;
+}
