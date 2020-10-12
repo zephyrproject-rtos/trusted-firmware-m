0399e7c45b34e45ed019fa1bff6ba544202ff2b1
Raef Coles <raef.coles@arm.com>
2020-10-12 16:52:14 +0100

    Build: Fix issues with CC312 OTP configuration
    Change-Id: I99f0ae8ec2c539483c76b640c9f5cc5268b47a59
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/musca_s1/config.cmake
+++ platform/ext/target/musca_s1/config.cmake
@@ -7,3 +7,7 @@
 
 set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms" FORCE)
 set(TFM_CRYPTO_TEST_ALG_CFB             OFF         CACHE BOOL      "Test CFB cryptography mode" FORCE)
+
+if(CRYPTO_HW_ACCELERATOR_OTP_STATE STREQUAL "ENABLED")
+    set(PLATFORM_DUMMY_CRYPTO_KEYS      FALSE       CACHE BOOL      "Use dummy crypto keys. Should not be used in production." FORCE)
+endif()
