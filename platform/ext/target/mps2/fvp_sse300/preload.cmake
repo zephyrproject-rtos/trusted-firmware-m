600a463bb79c5e3c10da3cfaaae6935422956583
Raef Coles <raef.coles@arm.com>
2020-10-15 16:42:35 +0100

    Build: Fix cpu and arch for sse_300
    Also bump cmsis_armclang.h to V5.3.1 to allow armv8.1m-main support
    
    Change-Id: I8bc7cb98100f8aaaf83d33e933b5cceac387d603
    Signed-off-by: Raef Coles <raef.coles@arm.com>
    Signed-off-by: Marton Berke <marton.berke@arm.com>

--- platform/ext/target/mps2/fvp_sse300/preload.cmake
+++ platform/ext/target/mps2/fvp_sse300/preload.cmake
@@ -11,8 +11,8 @@
 # variables related to hardware.
 
 # Set architecture and CPU
-set(TFM_SYSTEM_PROCESSOR cortex-m33)
-set(TFM_SYSTEM_ARCHITECTURE armv8-m.main)
+set(TFM_SYSTEM_PROCESSOR cortex-m55)
+set(TFM_SYSTEM_ARCHITECTURE armv8.1-m.main)
 set(TFM_SYSTEM_DSP OFF)
 
 # Reload compiler to generate options from the CPU and architecture
