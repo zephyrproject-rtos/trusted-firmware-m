931622b6492bedd6df4670c443f1cab20bd61432
Gabor Abonyi <gabor.abonyi@arm.com>
2020-10-19 15:08:40 +0200

    Platform: Make UART stdout implementation replaceable
    Change-Id: Ibc808027c8f4623ac2f3ae2c35f87d2f413837be
    Signed-off-by: Gabor Abonyi <gabor.abonyi@arm.com>

--- config/config_default.cmake
+++ config/config_default.cmake
@@ -73,6 +73,7 @@ set(PLATFORM_DUMMY_NV_COUNTERS          TRUE        CACHE BOOL      "Use dummy n
 set(PLATFORM_DUMMY_CRYPTO_KEYS          TRUE        CACHE BOOL      "Use dummy crypto keys. Should not be used in production.")
 set(PLATFORM_DUMMY_ROTPK                TRUE        CACHE BOOL      "Use dummy root of trust public key. Dummy key is the public key for the default keys in bl2. Should not be used in production.")
 set(PLATFORM_DUMMY_IAK                  TRUE        CACHE BOOL      "Use dummy initial attestation_key. Should not be used in production.")
+set(PLATFORM_DEFAULT_UART_STDOUT        TRUE        CACHE BOOL      "Use default uart stdout implementation.")
 
 ############################ Partitions ########################################
 
