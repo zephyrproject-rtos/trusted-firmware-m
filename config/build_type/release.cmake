3509afd7de86c645640b301912f314f370afda84
Raef Coles <raef.coles@arm.com>
2020-11-05 14:13:47 +0000

    Build: Set MBEDCRYPTO_BUILD_TYPE to minsizerel
    In all cases except for debug builds, where relwithdebinfo is used.
    
    Change-Id: I132c3e276bfb70f8881aaae0c1f7900a95ede76a
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- config/build_type/release.cmake
+++ config/build_type/release.cmake
@@ -5,4 +5,5 @@
 #
 #-------------------------------------------------------------------------------
 
+set(MBEDCRYPTO_BUILD_TYPE               minsizerel CACHE STRING    "Build type of Mbed Crypto library")
 set(MCUBOOT_LOG_LEVEL                   "OFF"      CACHE STRING    "Level of logging to use for MCUboot [OFF, ERROR, WARNING, INFO, DEBUG]")
