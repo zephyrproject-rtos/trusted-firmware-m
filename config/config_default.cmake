6d47fb7a5208e9985f9632e1705e8c71e0c64633
Raef Coles <raef.coles@arm.com>
2020-12-16 09:26:48 +0000

    Build: Use upstream bootutil cmake
    To allow easier integration of changes to bootutil structure. Also,
    bump MCUboot version to one where the bootutil cmake is present.
    
    Change-Id: I97547480267f69711494b53aee500a4925eb43b7
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- config/config_default.cmake
+++ config/config_default.cmake
@@ -151,7 +151,7 @@ set(TFM_TEST_REPO_VERSION               "master"    CACHE STRING    "The version
 set(CMSIS_5_PATH                        "DOWNLOAD"  CACHE PATH      "Path to CMSIS_5 (or DOWNLOAD to fetch automatically")
 
 set(MCUBOOT_PATH                        "DOWNLOAD"  CACHE PATH      "Path to MCUboot (or DOWNLOAD to fetch automatically")
-set(MCUBOOT_VERSION                     "v1.7.0"    CACHE STRING    "The version of MCUboot to use")
+set(MCUBOOT_VERSION                     "81d19f0"   CACHE STRING    "The version of MCUboot to use")
 
 set(PSA_ARCH_TESTS_PATH                 "DOWNLOAD"  CACHE PATH      "Path to PSA arch tests (or DOWNLOAD to fetch automatically")
 set(PSA_ARCH_TESTS_VERSION              "90c8e680"  CACHE STRING    "The version of PSA arch tests to use")
