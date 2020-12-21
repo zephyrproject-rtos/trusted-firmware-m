9b6a1fe32d6020152cd0fd8f6dc0523808c33c0d
David Hu <david.hu@arm.com>
2020-12-21 15:16:09 +0800

    Build: Set TFM_PARTITION_AUDIT_LOG to OFF in IPC model
    Since audit logging is not supported in IPC model yet, create a IPC
    dedicated default config to explicitly set TFM_PARTITION_AUDIT_LOG
    to OFF in IPC model.
    It can skip installing audit logging files in IPC model.
    
    Change-Id: Ie2c948aedb2fe40a88399a855e22f4490d631179
    Signed-off-by: David Hu <david.hu@arm.com>

---
+++ config/tfm_ipc_config_default.cmake
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2020, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+############################ Partitions ########################################
+
+set(TFM_PARTITION_AUDIT_LOG             OFF          CACHE BOOL      "Enable Audit Log partition")
