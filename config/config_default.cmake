6d3f3dfd13bea958666f422dcf689f17c12faaf5
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2021-02-10 18:25:56 +0100

    Build: Add Cmake argument MBEDCRYPTO_GIT_REMOTE
    To be able to redirect the fetching of mbedtls. This makes it possible
    to have a local clone that TFM fetches from, and the correct version
    and patches are always used, as opposed to using MBEDCRYPTO_PATH where
    you must always manually keep the clone in sync with the current version
    and patches.
    
    Change-Id: I8c316ecdb13b0deff80605f4b3f0acca8f5d2ed5
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- config/config_default.cmake
+++ config/config_default.cmake
@@ -150,6 +150,7 @@ set(TFM_CRYPTO_TEST_HKDF                ON          CACHE BOOL      "Test SHA-51
 
 set(MBEDCRYPTO_PATH                     "DOWNLOAD"  CACHE PATH      "Path to Mbed Crypto (or DOWNLOAD to fetch automatically")
 set(MBEDCRYPTO_VERSION                  "mbedtls-2.24.0" CACHE STRING "The version of Mbed Crypto to use")
+set(MBEDCRYPTO_GIT_REMOTE               "https://github.com/ARMmbed/mbedtls.git" CACHE STRING "The URL (or path) to retrieve MbedTLS from.")
 set(MBEDCRYPTO_BUILD_TYPE               "${CMAKE_BUILD_TYPE}" CACHE STRING "Build type of Mbed Crypto library")
 set(TFM_MBEDCRYPTO_CONFIG_PATH          "${CMAKE_SOURCE_DIR}/lib/ext/mbedcrypto/mbedcrypto_config/tfm_mbedcrypto_config_default.h" CACHE PATH "Config to use for Mbed Crypto")
 set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH "" CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to cnfigure feature support")
