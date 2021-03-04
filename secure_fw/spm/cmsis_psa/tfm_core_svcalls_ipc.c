e212ea1637a66255b44d0e7c19ebe9786ab56ccb
Kevin Peng <kevin.peng@arm.com>
2021-03-04 14:13:03 +0800

    SPM: Prevent IPC SVC call from Handler Mode
    As IPC SVC is expecting client call from Thread mode.
    
    Change-Id: Iab1e587d15c25e7685d19c7f35696d6731a014a7
    Signed-off-by: Kevin Peng <kevin.peng@arm.com>

--- secure_fw/spm/cmsis_psa/tfm_core_svcalls_ipc.c
+++ secure_fw/spm/cmsis_psa/tfm_core_svcalls_ipc.c
@@ -129,6 +129,11 @@ uint32_t tfm_core_svc_handler(uint32_t *msp, uint32_t *psp, uint32_t exc_return)
     tfm_svc_number_t svc_number = TFM_SVC_PSA_FRAMEWORK_VERSION;
     uint32_t *svc_args = msp;
 
+    if (!(exc_return & EXC_RETURN_MODE)) {
+        /* Calling SVC from Handler Mode is not supported */
+        tfm_core_panic();
+    }
+
     if ((exc_return & EXC_RETURN_MODE) && (exc_return & EXC_RETURN_SPSEL)) {
         /* Use PSP when both EXC_RETURN.MODE and EXC_RETURN.SPSEL are set */
         svc_args = psp;
