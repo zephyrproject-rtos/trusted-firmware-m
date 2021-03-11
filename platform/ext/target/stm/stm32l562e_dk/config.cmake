e23dc2283f9e50834e55f48e9b3a37fa9aca0aa3
Michel Jaouen <michel.jaouen@st.com>
2021-03-11 15:10:59 +0100

    platform: stm32l562e_dk: add level 3 isolation support
    Change-Id: I65a121536829e2cc80a392f85fbe18e2304189d6
    Signed-off-by: Michel Jaouen <michel.jaouen@st.com>

--- platform/ext/target/stm/stm32l562e_dk/config.cmake
+++ platform/ext/target/stm/stm32l562e_dk/config.cmake
@@ -13,3 +13,4 @@ set(MCUBOOT_IMAGE_NUMBER                2           CACHE STRING    "Whether to 
 ################################## Dependencies ################################
 set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms")
 set(MBEDCRYPTO_BUILD_TYPE               minsizerel  CACHE STRING "Build type of Mbed Crypto library")
+set(TFM_EXTRA_GENERATED_FILE_LIST_PATH  ${CMAKE_CURRENT_SOURCE_DIR}/platform/ext/target/stm/common/generated_file_list.yaml  CACHE PATH "Path to extra generated file list. Appended to stardard TFM generated file list." FORCE)
