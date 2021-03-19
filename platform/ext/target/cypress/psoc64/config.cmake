4c03b8203c07d81ccda43acc911e6b187bcdd93d
Raef Coles <raef.coles@arm.com>
2021-03-19 10:09:54 +0000

    Platform: Remove duplicated files from psoc64
    Remove duplicated default implementations from the psoc64 platform, and
    instead the ones in platform/common.
    
    Change-Id: I6ef9a94f595eefdea155f4e15e322c9f1231606a
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/cypress/psoc64/config.cmake
+++ platform/ext/target/cypress/psoc64/config.cmake
@@ -16,9 +16,7 @@ set(TFM_NS_CLIENT_IDENTIFICATION        OFF         CACHE BOOL      "Enable NS c
 set(TFM_MULTI_CORE_TOPOLOGY             ON          CACHE BOOL      "Whether to build for a dual-cpu architecture")
 set(NUM_MAILBOX_QUEUE_SLOT              4           CACHE BOOL      "Number of mailbox queue slots")
 
-set(PLATFORM_DUMMY_ATTEST_HAL           FALSE       CACHE BOOL      "Use dummy attest hal implementation. Should not be used in production.")
 set(PLATFORM_DUMMY_NV_COUNTERS          FALSE       CACHE BOOL      "Use dummy nv counter implementation. Should not be used in production.")
-set(PLATFORM_DUMMY_CRYPTO_KEYS          FALSE       CACHE BOOL      "Use dummy crypto keys. Should not be used in production.")
 
 # Disable default PDL IPC configuration. Use Cy_Platform_Init() in
 # in psoc6_system_init_cm4.c and psoc6_system_init_cm0p.c instead.
