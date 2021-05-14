13b27cf6a74062d76a5fce384ff8c77e9e2de9d2
Maulik  Patel <Maulik.Patel@arm.com>
2021-05-14 11:44:53 +0100

    Crypto: Upgrade MbedTLS to 2.26
    Sets the MBEDCRYPTO_VERSION to 2.26.0.
    
    Updates headers in include/psa folder.
    
    Adds a patch for psa arch test. This patch updates crypto api test
    cases for psa_mac_sign_setup (226) and psa_mac_verify_setup (229)
    against unknown MAC algorithm such that key passed is valid but the
    algorithm is unknown. Also, as per PSA Crypto Spec 1.0.0, Fix the
    expected return value to PSA_ERROR_INVALID_ARGUMENT for
    incompatible key to MAC algorithm.
    
    Signed-off-by: Maulik Patel <maulik.patel@arm.com>
    Change-Id: I8f42736a9e5bd7fbf604146b43ef28180e741fc3

--- config/config_default.cmake
+++ config/config_default.cmake
@@ -163,7 +163,7 @@ set(TFM_FWU_TEST_QUERY_WITH_NULL        OFF         CACHE BOOL      "Test psa_fw
 ################################## Dependencies ################################
 
 set(MBEDCRYPTO_PATH                     "DOWNLOAD"  CACHE PATH      "Path to Mbed Crypto (or DOWNLOAD to fetch automatically")
-set(MBEDCRYPTO_VERSION                  "mbedtls-2.25.0" CACHE STRING "The version of Mbed Crypto to use")
+set(MBEDCRYPTO_VERSION                  "mbedtls-2.26.0" CACHE STRING "The version of Mbed Crypto to use")
 set(MBEDCRYPTO_GIT_REMOTE               "https://github.com/ARMmbed/mbedtls.git" CACHE STRING "The URL (or path) to retrieve MbedTLS from.")
 set(MBEDCRYPTO_BUILD_TYPE               "${CMAKE_BUILD_TYPE}" CACHE STRING "Build type of Mbed Crypto library")
 set(TFM_MBEDCRYPTO_CONFIG_PATH          "${CMAKE_SOURCE_DIR}/lib/ext/mbedcrypto/mbedcrypto_config/tfm_mbedcrypto_config_default.h" CACHE PATH "Config to use for Mbed Crypto")
