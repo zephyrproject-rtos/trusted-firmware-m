7533d3c2aea5fe82615628386f5c02890f2e3ef6
MartinaHanusovaNXP <martina.hanusova@nxp.com>
2021-06-04 17:32:28 +0200

    Platform: LPCxpresso55s69: Added TFM_PLATFORM_NXP_HAL_FILE_PATH variable
    Added TFM_PLATFORM_NXP_HAL_FILE_PATH CMake variable for specifying
    the location of NXP SDK hal. Defaultly the variable is set to DOWNLOAD.
    In this case, the pulling drivers from git during build is enabled.
    Otherwise, when specified the path to the checked out repository,
    this funcionality is disabled and the files are linked statically.
    
    Added NXP_SDK_GIT_TAG CMake variable for specifying the version
    of NXP MCUXpresso SDK.
    
    Change-Id: Ie3a6048d76f760591debbf9bee1c7c7c85a4b62d
    Signed-off-by: MartinaHanusovaNXP <martina.hanusova@nxp.com>

--- platform/ext/target/nxp/lpcxpresso55s69/config.cmake
+++ platform/ext/target/nxp/lpcxpresso55s69/config.cmake
@@ -5,6 +5,10 @@
 #
 #-------------------------------------------------------------------------------
 
+################################## Dependencies ################################
+set(TFM_PLATFORM_NXP_HAL_FILE_PATH      "DOWNLOAD"      CACHE STRING    "Path to the NXP SDK hal (or DOWNLOAD to fetch automatically)")
+set(NXP_SDK_GIT_TAG                     "MCUX_2.9.0"    CACHE STRING    "The version of the NXP MCUXpresso SDK")
 
-set(PS_NUM_ASSETS                       "5"         CACHE STRING    "The maximum number of assets to be stored in the Protected Storage area")
-set(PS_MAX_ASSET_SIZE                   "512"       CACHE STRING    "The maximum asset size to be stored in the Protected Storage area")
+############################ Partitions ########################################
+set(PS_NUM_ASSETS                       "5"             CACHE STRING    "The maximum number of assets to be stored in the Protected Storage area")
+set(PS_MAX_ASSET_SIZE                   "512"           CACHE STRING    "The maximum asset size to be stored in the Protected Storage area")
