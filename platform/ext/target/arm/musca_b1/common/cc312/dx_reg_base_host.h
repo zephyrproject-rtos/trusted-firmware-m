0009e9eb325bf89d0525590331ac2a0a3a709c8f
Raef Coles <raef.coles@arm.com>
2021-06-14 09:36:37 +0100

    CC312: Move target config into target dir
    Change-Id: I351e8006e969ceb39dcf3115a895f820fc116042
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/arm/musca_b1/common/cc312/dx_reg_base_host.h
+++ platform/ext/target/arm/musca_b1/common/cc312/dx_reg_base_host.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001-2019, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2001-2021, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -11,9 +11,6 @@
 #define DX_PLAT_MUSCA_B1 1
 
 #define DX_BASE_CC 0x50088000
-#define DX_BASE_CODE 0x50030000 //# not used
-
-#define DX_BASE_ENV_REGS 0x500A0000 //TODO need confirm
 
 #define DX_BASE_HOST_RGF 0x0UL
 #define DX_BASE_CRY_KERNEL     0x0UL
