f5c66be2e9e76b2be193cd01ad611fd8c578de60
Kevin Peng <kevin.peng@arm.com>
2021-07-14 12:12:54 +0800

    SPM: Make boot data table only have enabled Partition
    The access policy table for boot data should only have enabled
    Partition.
    
    Change-Id: I125e980f265df3798e6bcee7be8b0e9aa545b652
    Signed-off-by: Kevin Peng <kevin.peng@arm.com>

--- secure_fw/spm/ffm/tfm_boot_data.c
+++ secure_fw/spm/ffm/tfm_boot_data.c
@@ -69,8 +69,12 @@ struct boot_data_access_policy {
  *        (identified by major_type).
  */
 static const struct boot_data_access_policy access_policy_table[] = {
+#ifdef TFM_PARTITION_INITIAL_ATTESTATION
     {TFM_SP_INITIAL_ATTESTATION, TLV_MAJOR_IAS},
+#endif
+#ifdef TFM_PARTITION_FIRMWARE_UPDATE
     {TFM_SP_FWU, TLV_MAJOR_FWU},
+#endif
 };
 
 /*!
