c19c76627a740ce1ffb9cd7fb06729e434c896a0
Summer Qin <summer.qin@arm.com>
2021-07-21 16:55:10 +0800

    Test: Update the tfm test version
    Update the TFM_TEST_REPO_VERSION which align with mbedtls-3.0.0
    and support crypto single-shot test cases.
    
    Change-Id: Ic61e5a4b31395df0864b21dac28ca02d62f25328
    Signed-off-by: Summer Qin <summer.qin@arm.com>

--- config/config_default.cmake
+++ config/config_default.cmake
@@ -181,7 +181,7 @@ set(TFM_MBEDCRYPTO_CONFIG_PATH          "${CMAKE_SOURCE_DIR}/lib/ext/mbedcrypto/
 set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH "" CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to cnfigure feature support")
 
 set(TFM_TEST_REPO_PATH                  "DOWNLOAD"  CACHE PATH      "Path to TFM-TEST repo (or DOWNLOAD to fetch automatically")
-set(TFM_TEST_REPO_VERSION               "81aebd8"   CACHE STRING    "The version of tf-m-tests to use")
+set(TFM_TEST_REPO_VERSION               "e1a8c9f"   CACHE STRING    "The version of tf-m-tests to use")
 set(CMSIS_5_PATH                        "DOWNLOAD"  CACHE PATH      "Path to CMSIS_5 (or DOWNLOAD to fetch automatically")
 
 set(MCUBOOT_PATH                        "DOWNLOAD"  CACHE PATH      "Path to MCUboot (or DOWNLOAD to fetch automatically")
