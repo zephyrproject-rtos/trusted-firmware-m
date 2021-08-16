798ee00566a2e2f0f7f8510577dc3b9e519a9952
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2021-08-16 23:14:11 +0200

    psa-arch-tests: synchronize with upstream up to commit 51ff2bdf
    Synchronize with the upstream psa-arch-tests repository
    up to commit 51ff2bdfae043f6dd0813b000d928c4bda172660.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- psa-arch-tests/api-tests/dev_apis/crypto/test_c021/test_data.h
+++ psa-arch-tests/api-tests/dev_apis/crypto/test_c021/test_data.h
@@ -1,5 +1,5 @@
 /** @file
- * Copyright (c) 2019-2020, Arm Limited or its affiliates. All rights reserved.
+ * Copyright (c) 2019-2021, Arm Limited or its affiliates. All rights reserved.
  * SPDX-License-Identifier : Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,13 @@
 
 #include "val_crypto.h"
 
+#include "test_crypto_common.h"
+typedef struct {
+    psa_key_derivation_step_t  step;
+    const uint8_t             *data;
+    size_t                     data_length;
+} key_derivation_input_t;
+
 typedef struct {
     char                        test_desc[75];
     psa_key_type_t              key_type;
@@ -32,6 +39,7 @@ typedef struct {
     size_t                      derive_key_bits;
     psa_key_usage_t             derive_usage;
     psa_algorithm_t             derive_key_alg;
+    key_derivation_input_t      info;
     psa_status_t                expected_status;
 } test_data;
 
@@ -44,9 +52,14 @@ static const test_data check1[] = {
  PSA_KEY_DERIVATION_INPUT_SECRET, 32, {0}, 0,
  PSA_KEY_TYPE_AES, BYTES_TO_BITS(AES_32B_KEY_SIZE), PSA_KEY_USAGE_EXPORT,
  PSA_ALG_CTR,
+ {
+   .step        = PSA_KEY_DERIVATION_INPUT_INFO,
+   .data        = input_info,
+   .data_length = INPUT_INFO_LEN
+ },
  PSA_SUCCESS
 },
-
+#if 0
 {"Test psa_key_derivation_output_key - Info\n", PSA_KEY_TYPE_DERIVE,
 {0x49, 0x8E, 0xC7, 0x7D, 0x01, 0x95, 0x0D, 0x94, 0x2C, 0x16, 0xA5, 0x3E, 0x99,
  0x5F, 0xC9, 0x77},
@@ -97,8 +110,8 @@ static const test_data check1[] = {
  AES_16B_KEY_SIZE, PSA_KEY_USAGE_DERIVE,
  PSA_ALG_KEY_AGREEMENT(PSA_ALG_ECDH, PSA_ALG_HKDF(PSA_ALG_SHA_256)),
  PSA_KEY_DERIVATION_INPUT_SECRET, 256, {0}, 0,
- PSA_KEY_TYPE_ECC_PUBLIC_KEY(PSA_ECC_CURVE_SECP256R1), 256, PSA_KEY_USAGE_EXPORT,
- PSA_ALG_CATEGORY_ASYMMETRIC_ENCRYPTION,
+ PSA_KEY_TYPE_ECC_PUBLIC_KEY(PSA_ECC_FAMILY_SECP_R1), 256, PSA_KEY_USAGE_EXPORT,
+ PSA_ALG_ECDSA_ANY,
  PSA_ERROR_NOT_SUPPORTED
 },
 
@@ -108,8 +121,8 @@ static const test_data check1[] = {
  AES_16B_KEY_SIZE, PSA_KEY_USAGE_DERIVE,
  PSA_ALG_KEY_AGREEMENT(PSA_ALG_ECDH, PSA_ALG_HKDF(PSA_ALG_SHA_256)),
  PSA_KEY_DERIVATION_INPUT_SECRET, 224, {0}, 0,
- PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_CURVE_SECP224R1), 224, PSA_KEY_USAGE_EXPORT,
- PSA_ALG_CATEGORY_ASYMMETRIC_ENCRYPTION,
+ PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1), 224, PSA_KEY_USAGE_EXPORT,
+ PSA_ALG_ECDSA_ANY,
  PSA_SUCCESS
 },
 
@@ -130,7 +143,7 @@ static const test_data check1[] = {
  AES_16B_KEY_SIZE, PSA_KEY_USAGE_DERIVE,
  PSA_ALG_KEY_AGREEMENT(PSA_ALG_ECDH, PSA_ALG_HKDF(PSA_ALG_SHA_256)),
  PSA_KEY_DERIVATION_INPUT_SECRET, 256, {0}, 0,
- PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_CURVE_SECP224R1), 2046, PSA_KEY_USAGE_EXPORT,
+ PSA_KEY_TYPE_ECC_KEY_PAIR(PSA_ECC_FAMILY_SECP_R1), 2046, PSA_KEY_USAGE_EXPORT,
  PSA_ALG_RSA_PKCS1V15_SIGN_RAW,
  PSA_SUCCESS
 },
@@ -145,4 +158,5 @@ static const test_data check1[] = {
  PSA_ALG_CTR,
  PSA_ERROR_NOT_SUPPORTED
 },
+#endif
 };
