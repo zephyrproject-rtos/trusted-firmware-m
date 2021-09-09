7d88ae98c4d703ecec3a9bea7519e69e95b82da0
Shawn Shan <Shawn.Shan@arm.com>
2021-09-09 15:32:02 +0800

    Tools: Support for MM_IOVEC
    When mm_iovec attribute is enabled, add SERVICE_FLAG_MM_IOVEC to service
    flags.
    
    Change-Id: If7fb7c27cb68c2e3bb437737e3a437e9efab9e6b
    Signed-off-by: Shawn Shan <Shawn.Shan@arm.com>

--- secure_fw/spm/include/load/service_defs.h
+++ secure_fw/spm/include/load/service_defs.h
@@ -17,6 +17,7 @@
  * bit 8: 1 - NS accessible, 0 - NS not accessible
  * bit 9: 1 - stateless, 0 - connection-based
  * bit 10: 1 - strict version policy, 0 - relaxed version policy
+ * bit 11: 1 - MM-IOVEC enabled, 0 - MM-IOVEC disabled
  */
 #define SERVICE_FLAG_STATELESS_HINDEX_MASK      (0xFF)
 #define SERVICE_FLAG_NS_ACCESSIBLE              (1U << 8)
@@ -25,6 +26,7 @@
 #define SERVICE_FLAG_VERSION_POLICY_BIT         (1U << 10)
 #define SERVICE_VERSION_POLICY_RELAXED          (0U << 10)
 #define SERVICE_VERSION_POLICY_STRICT           (1U << 10)
+#define SERVICE_FLAG_MM_IOVEC                   (1U << 11)
 
 #define SERVICE_GET_STATELESS_HINDEX(flag)      \
     ((flag) & SERVICE_FLAG_STATELESS_HINDEX_MASK)
@@ -34,6 +36,8 @@
     ((flag) & SERVICE_FLAG_STATELESS)
 #define SERVICE_GET_VERSION_POLICY(flag)        \
     ((flag) & SERVICE_FLAG_VERSION_POLICY_BIT)
+#define SERVICE_ENABLED_MM_IOVEC(flag)          \
+    ((flag) & SERVICE_FLAG_MM_IOVEC)
 
 #define STRID_TO_STRING_PTR(strid)              (const char *)(strid)
 #define STRING_PTR_TO_STRID(str)                (uintptr_t)(str)
