86bea3d2141054c13b3ab98ef0c6e4335eb7b41e
Antonio de Angelis <Antonio.deAngelis@arm.com>
2021-10-06 10:47:16 +0100

    CC312: Fix Chacha20 / Poly1305 support for mbedTLS 3.0
    Some deprecated defines were used in CC312 code and got
    removed after migration to mbedTLS 3.0. This does not
    cause a build failure as by default Chacha20 / Poly1305
    are not enabled, but they should be buildable on request.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I48941434ebeecb69669e89c5e79bde7ca42ea5ca

--- lib/ext/cryptocell-312-runtime/codesafe/src/mbedtls_api/poly1305_alt.c
+++ lib/ext/cryptocell-312-runtime/codesafe/src/mbedtls_api/poly1305_alt.c
@@ -11,6 +11,7 @@
 
 #if defined(MBEDTLS_POLY1305_C)
 #include "mbedtls/poly1305.h"
+#include "mbedtls/error.h"
 #include "poly.h"
 #include "mbedtls/platform_util.h"
 #include "chacha_driver.h"
@@ -47,7 +48,7 @@ int mbedtls_poly1305_starts( mbedtls_poly1305_context *ctx,
 {
     CC_UNUSED_PARAM(ctx);
     CC_UNUSED_PARAM(key);
-    return MBEDTLS_ERR_POLY1305_FEATURE_UNAVAILABLE;
+    return MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED;
 }
 
 /* Cryptocell only supports integrated poly1305 operations  */
@@ -58,7 +59,7 @@ int mbedtls_poly1305_update( mbedtls_poly1305_context *ctx,
     CC_UNUSED_PARAM(ctx);
     CC_UNUSED_PARAM(input);
     CC_UNUSED_PARAM(ilen);
-    return MBEDTLS_ERR_POLY1305_FEATURE_UNAVAILABLE;
+    return MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED;
 }
 
 /* Cryptocell only supports integrated poly1305 operations  */
@@ -67,7 +68,7 @@ int mbedtls_poly1305_finish( mbedtls_poly1305_context *ctx,
 {
     CC_UNUSED_PARAM(ctx);
     CC_UNUSED_PARAM(mac);
-    return MBEDTLS_ERR_POLY1305_FEATURE_UNAVAILABLE;
+    return MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED;
 }
 
 int mbedtls_poly1305_mac( const unsigned char key[32],
@@ -91,7 +92,7 @@ int mbedtls_poly1305_mac( const unsigned char key[32],
 
     rc = PolyMacCalc(pKey, NULL, 0, input, ilen, macRes, false);
     if (rc != 0) {
-        return MBEDTLS_ERR_POLY1305_HW_ACCEL_FAILED;
+        return MBEDTLS_ERR_PLATFORM_HW_ACCEL_FAILED;
     }
 
     CC_PalMemCopy(mac, (unsigned char *)macRes, MBEDTLS_POLY_MAC_SIZE_BYTES);
