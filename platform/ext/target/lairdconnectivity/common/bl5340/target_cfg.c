5273dd31848d8cb215f134c0e972c181b33a7a2e
Joakim Andersson <joakim.andersson@nordicsemi.no>
2021-10-20 14:16:04 +0200

    platform: lairdconnectivity: Remove GPIOTE1 configured as non-secure
    Remove GPIOTE1 configured as non-secure peripheral. This peripheral
    is always non-secure so this has no effect.
    
    Change-Id: I727f6774e6faf23f8f1811c584575e63efb3bad0
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- platform/ext/target/lairdconnectivity/common/bl5340/target_cfg.c
+++ platform/ext/target/lairdconnectivity/common/bl5340/target_cfg.c
@@ -728,7 +728,6 @@ enum tfm_plat_err_t spu_periph_init_cfg(void)
     spu_peripheral_config_non_secure((uint32_t)NRF_QSPI, false);
 #endif
     spu_peripheral_config_non_secure((uint32_t)NRF_NFCT, false);
-    spu_peripheral_config_non_secure((uint32_t)NRF_GPIOTE1_NS, false);
     spu_peripheral_config_non_secure((uint32_t)NRF_MUTEX, false);
     spu_peripheral_config_non_secure((uint32_t)NRF_QDEC0, false);
     spu_peripheral_config_non_secure((uint32_t)NRF_QDEC1, false);
