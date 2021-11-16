710acf56a58942018e7517f60e20d54e7b851a99
Kevin Peng <kevin.peng@arm.com>
2021-11-16 11:42:41 +0800

    Interface: Improvements on framework_feature
    - Restrict variable replacement on "@VAR@" only
    - Set PSA_FRAMEWORK_ISOLATION_LEVEL before configure_file
    - Add invalid config check on:
        TFM_LIB_MODEL AND PSA_FRAMEWORK_ISOLATION_LEVEL
    
    Change-Id: Ib3a681716dfb148f62ca4ea0c03b2de9a65b7ca5
    Signed-off-by: Kevin Peng <kevin.peng@arm.com>

--- config/tfm_ipc_config_default.cmake
+++ config/tfm_ipc_config_default.cmake
@@ -8,7 +8,3 @@
 ############################ Partitions ########################################
 
 set(TFM_PSA_API          ON          CACHE BOOL      "Use PSA API instead of secure library model")
-
-# Dummy PSA_FRAMEWORK_ISOLATION_LEVEL to let framework_feature.h.in "#define" it always.
-# PSA_FRAMEWORK_ISOLATION_LEVEL should replace TFM_ISOLATION_LEVEL in the future.
-set(PSA_FRAMEWORK_ISOLATION_LEVEL   ON)
