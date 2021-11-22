0845113cf763e73bb22ef58f86e78758fe4389f2
Joakim Andersson <joakim.andersson@nordicsemi.no>
2021-11-22 17:37:59 +0100

    platform: nordic_nrf: Add platform GPIO and read service
    Add nordic_nrf platform services for GPIO and read as core platform
    services.
    The services are added in a way that makes it possible for out-of-tree
    boards to include these core services while still being able to add
    their own.
    
    Change-Id: Ie3152fefd126d819963717246ed9da07d791d59f
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    (cherry picked from commit e6b05a052009f88fa261c27f4b1f71f68189bda4)

--- platform/ext/target/nordic_nrf/nrf5340dk_nrf5340_cpuapp/services/src/tfm_platform_system.c
+++ platform/ext/target/nordic_nrf/nrf5340dk_nrf5340_cpuapp/services/src/tfm_platform_system.c
@@ -7,6 +7,8 @@
 
 #include "platform/include/tfm_platform_system.h"
 #include "cmsis.h"
+#include "tfm_platform_hal_ioctl.h"
+#include "tfm_ioctl_core_api.h"
 
 void tfm_platform_hal_system_reset(void)
 {
@@ -18,10 +20,16 @@ enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
                                                psa_invec  *in_vec,
                                                psa_outvec *out_vec)
 {
-    (void)request;
-    (void)in_vec;
-    (void)out_vec;
+	/* Core IOCTL services */
+	switch (request) {
+	case TFM_PLATFORM_IOCTL_READ_SERVICE:
+		return tfm_platform_hal_read_service(in_vec, out_vec);
+	case TFM_PLATFORM_IOCTL_GPIO_SERVICE:
+		return tfm_platform_hal_gpio_service(in_vec, out_vec);
+	/* Board specific IOCTL services */
 
-    /* Not needed for this platform */
-    return TFM_PLATFORM_ERR_NOT_SUPPORTED;
+	/* Not a supported IOCTL service.*/
+	default:
+		return TFM_PLATFORM_ERR_NOT_SUPPORTED;
+	}
 }
