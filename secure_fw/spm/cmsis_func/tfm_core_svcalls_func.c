b702cba1dea5c25f6e0c8f01e722625a64ada3b8
Bence Balogh <bence.balogh@arm.com>
2021-12-09 15:25:45 +0100

    SPM: Fix typo in tfm_core_svcalls_handler.c
    Change-Id: I762e94b22068671a812b5e0baea50f3312ad7f9b
    Signed-off-by: Bence Balogh <bence.balogh@arm.com>

--- secure_fw/spm/cmsis_func/tfm_core_svcalls_func.c
+++ secure_fw/spm/cmsis_func/tfm_core_svcalls_func.c
@@ -97,7 +97,7 @@ uint32_t tfm_core_svc_handler(uint32_t *msp, uint32_t *psp, uint32_t exc_return)
 #endif
     default:
 #ifdef PLATFORM_SVC_HANDLERS
-        svc_args[0] = platform_svc_handlers(svc_num, svc_args, exc_return);
+        svc_args[0] = platform_svc_handlers(svc_number, svc_args, exc_return);
 #else
         SPMLOG_ERRMSG("Unknown SVC number requested!\r\n");
 #endif
