ac6067fb027a0a5a9cf5c27b9643c7a04d6ba711
Chris Brand <chris.brand@cypress.com>
2022-01-13 13:49:15 -0800

    Platform: PSoC64: Tweak *.cmake
    Move everything that isn't related to the different S and NS CPU
    into config.cmake.
    
    Change-Id: Ie3b552a4513ba9a6a76902a1eade053750ed88b0
    Signed-off-by: Chris Brand <chris.brand@cypress.com>

--- platform/ext/target/cypress/psoc64/config.cmake
+++ platform/ext/target/cypress/psoc64/config.cmake
@@ -11,6 +11,8 @@ set(TFM_NS_CLIENT_IDENTIFICATION        OFF         CACHE BOOL      "Enable NS c
 
 ############################ Platform ##########################################
 
+add_definitions(-DCYB0644ABZI_S2D44)
+
 set(TFM_MULTI_CORE_TOPOLOGY             ON          CACHE BOOL      "Whether to build for a dual-cpu architecture")
 set(NUM_MAILBOX_QUEUE_SLOT              4           CACHE BOOL      "Number of mailbox queue slots")
 set(PLATFORM_SLIH_IRQ_TEST_SUPPORT      ON          CACHE BOOL      "Platform supports SLIH IRQ tests")
@@ -22,6 +24,9 @@ set(CY_MTB_PDL_CAT1_LIB_VERSION         "release-v2.0.0" CACHE STRING "The versi
 
 set(CY_P64_UTILS_LIB_PATH               "DOWNLOAD"  CACHE PATH      "Path to p64_utils repo (or DOWNLOAD to fetch automatically")
 set(CY_P64_UTILS_LIB_VERSION            "release-v1.0.0" CACHE STRING "The version of p64_utils to use")
+
+# Configure PDL to use RAM interrup vectors. This is required by P64_utils
+add_definitions(-DRAM_VECTORS_SUPPORT)
 
 # Disable default PDL IPC configuration. Use Cy_Platform_Init() in
 # in psoc6_system_init_cm4.c and psoc6_system_init_cm0p.c instead.
