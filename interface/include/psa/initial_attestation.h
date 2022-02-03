ae7e941a03bc29e91e2008f864faa6fab74bf584
Maulik  Patel <Maulik.Patel@arm.com>
2022-02-03 20:26:14 +0000

    Attest: Clean-up public header
    Remove old comment which is not valid anymore. Supported
    PSA Attestation API version is 1.0.0.
    
    Signed-off-by: Maulik Patel <maulik.patel@arm.com>
    Change-Id: Ia8d2234b87590f92c15355b902fe6e49fd6d4cb9

--- interface/include/psa/initial_attestation.h
+++ interface/include/psa/initial_attestation.h
@@ -1,16 +1,9 @@
 /*
- * Copyright (c) 2018-2021, Arm Limited. All rights reserved.
+ * Copyright (c) 2018-2022, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
-
-/***************************************************************************/
-/*                             DRAFT UNDER REVIEW                          */
-/*   These APIs are still evolving and are meant as a prototype for review.*/
-/*   The APIs will change depending on feedback and will be firmed up      */
-/*   to a stable set of APIs once all the feedback has been considered.    */
-/***************************************************************************/
 
 #ifndef __PSA_INITIAL_ATTESTATION_H__
 #define __PSA_INITIAL_ATTESTATION_H__
