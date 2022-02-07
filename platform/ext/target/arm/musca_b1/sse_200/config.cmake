e6516405c1ed360b507e2b8e226f6d81014957c9
Arnold Gabriel Benedict <arnoldgabriel.benedict@arm.com>
2022-02-07 10:18:28 +0000

    Platform: musca_b1/sse_200: Add GPLED API
    Signed-off-by: Arnold Gabriel Benedict <arnoldgabriel.benedict@arm.com>
    Change-Id: If5013b58535ceacd2dc01a7fe70a3ae585c05a97

--- platform/ext/target/arm/musca_b1/sse_200/config.cmake
+++ platform/ext/target/arm/musca_b1/sse_200/config.cmake
@@ -7,6 +7,7 @@
 
 
 set(TFM_CRYPTO_TEST_ALG_CFB             OFF         CACHE BOOL      "Test CFB cryptography mode")
+set(PLATFORM_GPLED_ENABLED              OFF         CACHE BOOL      "Use the general purpose LEDs on the platform board")
 
 if (NOT FORWARD_PROT_MSG)
     set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms")
@@ -46,4 +47,12 @@ if(BL2)
 else()
     #No header if no bootloader, but keep IMAGE_CODE_SIZE the same
     set(BL2_TRAILER_SIZE 0xC00 CACHE STRING "Trailer size")
+endif()
+
+if(PLATFORM_GPLED_ENABLED)
+    #If required, Musca_b1_sse_200 platform supports GPLED services
+    set(PLATFORM_GPLED_SUPPORTED        ON          CACHE BOOL      "Whether to enable the gpled functionality on supported platforms")
+else()
+    #By default, GPLED support is disabled
+    set(PLATFORM_GPLED_SUPPORTED        OFF         CACHE BOOL      "Whether to enable the gpled functionality on supported platforms")
 endif()
