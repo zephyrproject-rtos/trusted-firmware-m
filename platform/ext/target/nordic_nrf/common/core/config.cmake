0b346ebbd9a1cc4cbefb941e16935b10c28edff9
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-02-15 13:28:18 +0100

    platform: nordic_nrf: Set FLIH and SLIH IRQ tests as supported
    Set FLIH and SLIH IRQ tests as supported for the nordic_nrf platform
    boards.
    
    Change-Id: Ia60550a77a74446e57511096e6155cdd81f05cae
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/core/config.cmake
+++ platform/ext/target/nordic_nrf/common/core/config.cmake
@@ -8,3 +8,6 @@
 set(HAL_NORDIC_PATH "DOWNLOAD" CACHE PATH "Path to the Nordic HAL (or DOWNLOAD to fetch automatically)")
 set(HAL_NORDIC_VERSION "004b62c0b74c62bd8489e0a702a498899e0463d1" CACHE STRING "Version of the Nordic HAL to download") # nrfx 2.6.0
 set(HAL_NORDIC_REMOTE "https://github.com/zephyrproject-rtos/hal_nordic" CACHE STRING "Remote of the Nordic HAL to download")
+
+set(PLATFORM_SLIH_IRQ_TEST_SUPPORT      ON          CACHE BOOL    "Platform supports SLIH IRQ tests")
+set(PLATFORM_FLIH_IRQ_TEST_SUPPORT      ON          CACHE BOOL    "Platform supports FLIH IRQ tests")
