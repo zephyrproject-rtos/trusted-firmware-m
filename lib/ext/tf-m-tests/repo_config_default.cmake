f5120dabda3750fb9150eccfe348c7a32e0cdadd
Jianliang Shen <jianliang.shen@arm.com>
2022-02-22 15:02:15 +0800

    Build: Update tf-m-tests commit ID
    Signed-off-by: Jianliang Shen <jianliang.shen@arm.com>
    Change-Id: I79abecaf2316a5db16cfada21dc9e5eca19b4e2d

--- lib/ext/tf-m-tests/repo_config_default.cmake
+++ lib/ext/tf-m-tests/repo_config_default.cmake
@@ -8,5 +8,5 @@
 # Default configs of tf-m-tests repo
 
 set(TFM_TEST_REPO_PATH                  "DOWNLOAD"  CACHE PATH      "Path to TFM-TEST repo (or DOWNLOAD to fetch automatically")
-set(TFM_TEST_REPO_VERSION               "0f81aa0"   CACHE STRING    "The version of tf-m-tests to use")
+set(TFM_TEST_REPO_VERSION               "5fcfd37"   CACHE STRING    "The version of tf-m-tests to use")
 set(CMSIS_5_PATH                        "DOWNLOAD"  CACHE PATH      "Path to CMSIS_5 (or DOWNLOAD to fetch automatically")
