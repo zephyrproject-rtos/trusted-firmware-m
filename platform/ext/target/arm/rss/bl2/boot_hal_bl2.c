fc9ce0fc7a442eb188eeacd5eedc0a4a438657a3
Anders Dellien <anders.dellien@arm.com>
2022-03-14 10:54:28 +0000

    RSS: Implement RSS/SCP boot flow
    This patch adds implementations of the "weak" callback functions
    from the platform, where callbacks/signalling over MHU to the SCP
    are added.
    
    To summarize:
     * After loading SCP BL1 code, the RSS waits for a signal from SCP
       to indicate that BL1 has started.
     * At this point, SCP waits for a signal from RSS.
     * After loading AP BL1 code, RSS signals SCP which then releases
       the AP cores.
    
    Change-Id: I4178762801efdd825f890d9c34a8d63724845ef8
    Signed-off-by: Anders Dellien <anders.dellien@arm.com>

---
+++ platform/ext/target/arm/rss/bl2/boot_hal_bl2.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2019-2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "boot_hal.h"
+
+#include <string.h>
+
+#include "bootutil/bootutil_log.h"
+#include "device_definition.h"
+#include "platform_regs.h"
+#include "platform_base_address.h"
+#ifdef CRYPTO_HW_ACCELERATOR
+#include "crypto_hw.h"
+#include "bootutil/fault_injection_hardening.h"
+#endif /* CRYPTO_HW_ACCELERATOR */
+
+int32_t boot_platform_post_init(void)
+{
+    enum mhu_v2_x_error_t status;
+#ifdef CRYPTO_HW_ACCELERATOR
+    int32_t result;
+
+    result = crypto_hw_accelerator_init();
+    if (result) {
+        return 1;
+    }
+
+    (void)fih_delay_init();
+#endif /* CRYPTO_HW_ACCELERATOR */
+
+    status = mhu_v2_x_driver_init(&MHU_SCP_TO_RSS_DEV, MHU_REV_READ_FROM_HW);
+    if (status != MHU_V_2_X_ERR_NONE) {
+        BOOT_LOG_ERR("SCP->RSS MHU driver initialization failed");
+        return 1;
+    }
+    BOOT_LOG_INF("SCP->RSS MHU driver initialized successfully");
+
+    status = mhu_v2_x_driver_init(&MHU_RSS_TO_SCP_DEV, MHU_REV_READ_FROM_HW);
+    if (status != MHU_V_2_X_ERR_NONE) {
+        BOOT_LOG_ERR("RSS->SCP MHU driver initialization failed");
+        return 1;
+    }
+    BOOT_LOG_INF("RSS->SCP MHU driver initialized successfully");
+
+    return 0;
+}
+
+int boot_platform_post_load(uint32_t image_id)
+{
+    /* FIXME: provide enum for image_ids */
+    if (image_id == 3) {
+        struct rss_sysctrl_t *sysctrl =
+                                     (struct rss_sysctrl_t *)RSS_SYSCTRL_BASE_S;
+
+        /* Remove the image header and move the image to the start of SCP memory
+         * FIXME: Would be better to set SCP VTOR, but not currently possible
+         */
+        memmove((void *)SCP_SRAM_BASE_S,
+                (void *)(SCP_SRAM_BASE_S + BL2_HEADER_SIZE),
+                SCP_BL1_SIZE - BL2_HEADER_SIZE);
+
+        /* Release SCP CPU from wait */
+        sysctrl->gretreg = 0x1;
+        BOOT_LOG_INF("Waiting for SCP BL1 started event");
+        uint32_t channel_stat = 0;
+        while (channel_stat == 0) {
+            mhu_v2_x_channel_receive(&MHU_SCP_TO_RSS_DEV, 0, &channel_stat);
+        }
+        BOOT_LOG_INF("Got SCP BL1 started event");
+    } else if (image_id == 2) {
+        BOOT_LOG_INF("Telling SCP to start AP cores");
+        mhu_v2_x_initiate_transfer(&MHU_RSS_TO_SCP_DEV);
+        /* Slot 0 is used in the SCP protocol */
+        mhu_v2_x_channel_send(&MHU_RSS_TO_SCP_DEV, 0, 1);
+    }
+
+    return 0;
+}
