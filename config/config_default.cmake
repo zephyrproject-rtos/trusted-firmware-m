853a547e5b9c4f6c1fd2438bae9bf2e3980b23aa
Summer Qin <summer.qin@arm.com>
2022-04-01 16:53:45 +0800

    Crypto: Update the URL (or path) to retrieve MbedTLS
    Mbed TLS GitHub migration is complete from https://github.com/ARMmbed/mbedtls
    to https://github.com/Mbed-TLS/mbedtls.
    Update the Mbed TLS path in TF-M repo.
    
    Change-Id: I9dc0d6d90fc4d7c831e960a37feb2d58f343d61c
    Signed-off-by: Summer Qin <summer.qin@arm.com>

--- config/config_default.cmake
+++ config/config_default.cmake
@@ -156,7 +156,7 @@ set(TFM_FWU_BUF_SIZE                    ""          CACHE STRING    "Size of the
 
 set(MBEDCRYPTO_PATH                     "DOWNLOAD"  CACHE PATH      "Path to Mbed Crypto (or DOWNLOAD to fetch automatically")
 set(MBEDCRYPTO_VERSION                  "mbedtls-3.1.0" CACHE STRING "The version of Mbed Crypto to use")
-set(MBEDCRYPTO_GIT_REMOTE               "https://github.com/ARMmbed/mbedtls.git" CACHE STRING "The URL (or path) to retrieve MbedTLS from.")
+set(MBEDCRYPTO_GIT_REMOTE               "https://github.com/Mbed-TLS/mbedtls.git" CACHE STRING "The URL (or path) to retrieve MbedTLS from.")
 set(MBEDCRYPTO_BUILD_TYPE               "${CMAKE_BUILD_TYPE}" CACHE STRING "Build type of Mbed Crypto library")
 set(TFM_MBEDCRYPTO_CONFIG_PATH
   "${CMAKE_SOURCE_DIR}/lib/ext/mbedcrypto/mbedcrypto_config/tfm_mbedcrypto_config_default.h" CACHE PATH
