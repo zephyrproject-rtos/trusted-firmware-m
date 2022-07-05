19bf274a942db77741ac2c926b58e27a78d4fcc2
Chris Brand <chris.brand@cypress.com>
2022-07-05 11:15:17 -0700

    Build: fetch_remote_library function to fetch remote libraries.
    Signed-off-by: Roman Mazurak <roman.mazurak@infineon.com>
    Signed-off-by: Chris Brand <chris.brand@cypress.com>
    Change-Id: Ia140e49dc3d776d885584b6f3939107cbc457510

--- platform/ext/target/nordic_nrf/common/core/hal_nordic.cmake
+++ platform/ext/target/nordic_nrf/common/core/hal_nordic.cmake
@@ -1,24 +1,20 @@
 
 #-------------------------------------------------------------------------------
 # Copyright (c) 2021, Nordic Semiconductor ASA.
+# Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
+# or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 
-if (HAL_NORDIC_PATH STREQUAL "DOWNLOAD")
-    message(STATUS "Pulling Nordic HAL from ${HAL_NORDIC_REMOTE} @ ${HAL_NORDIC_VERSION}")
-    FetchContent_Declare(hal_nordic
-        GIT_REPOSITORY ${HAL_NORDIC_REMOTE}
-        GIT_TAG ${HAL_NORDIC_VERSION}
-        GIT_SHALLOW FALSE # Disable shallow fetch because version is a SHA.
-        GIT_PROGRESS TRUE
-        GIT_SUBMODULES ""
-    )
-
-    FetchContent_GetProperties(hal_nordic)
-    if(NOT hal_nordic_POPULATED)
-        FetchContent_Populate(hal_nordic)
-        set(HAL_NORDIC_PATH ${hal_nordic_SOURCE_DIR} CACHE PATH "Path to the Nordic HAL (or DOWNLOAD to fetch automatically)" FORCE)
-    endif()
-endif()
+fetch_remote_library(
+    LIB_NAME                hal_nordic
+    LIB_SOURCE_PATH_VAR     HAL_NORDIC_PATH
+    FETCH_CONTENT_ARGS
+        GIT_REPOSITORY      ${HAL_NORDIC_REMOTE}
+        GIT_TAG             ${HAL_NORDIC_VERSION}
+        GIT_SHALLOW         FALSE # Disable shallow fetch because version is a SHA.
+        GIT_PROGRESS        TRUE
+        GIT_SUBMODULES      ""
+)
