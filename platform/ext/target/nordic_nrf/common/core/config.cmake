8c98a4edd6497e31ddbe0dc2457af2a4300273ae
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2022-09-26 15:55:56 +0200

    Platform: nordic: Disable support for optional IAT claims
    ATTEST_INCLUDE_OPTIONAL_CLAIMS is enabled by default, but Nordic does
    not yet support these optional claims.
    
    Disabling this option makes this clearer to the user, and also
    prevents IAT tests from failing when claims are checked.
    
    Change-Id: I6975fcad55dfc8b767aa5f8ed28dae3b8e8e37a2
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/core/config.cmake
+++ platform/ext/target/nordic_nrf/common/core/config.cmake
@@ -23,6 +23,9 @@ set(NULL_POINTER_EXCEPTION_DETECTION    FALSE       CACHE BOOL
    May require more MPU regions than are available depending on the configuration."
   )
 
+# Optional IAT claims, such as hw_version, are not supported by Nordic yet
+set(ATTEST_INCLUDE_OPTIONAL_CLAIMS OFF CACHE BOOL "")
+
 set(PLATFORM_SLIH_IRQ_TEST_SUPPORT      ON          CACHE BOOL    "Platform supports SLIH IRQ tests")
 set(PLATFORM_FLIH_IRQ_TEST_SUPPORT      ON          CACHE BOOL    "Platform supports FLIH IRQ tests")
 
