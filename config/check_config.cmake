joerchan/sdk-trusted-firmware-m

partitions: platform: Add option for disabling nv_counter
In some configurations there is no need for the non-volatile
counters. Allow users to opt-out of this feature to reduce the
memory footprint of TF-M.

This will also enable some minimal builds of TF-M to not reserve any
extra memory regions used for storing data persistently.

Change-Id: Ie82f8e8c31d6bf5b13f430cc51da7307e32f66a1
Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

config/check_config.cmake
@@ -94,6 +94,7 @@ tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE AND NOT TFM_PARTITION_INTERNA
 # PS only uses the platform partition when PS_ROLLBACK_PROTECTION is ON, but
 # the dependency in the manifest file means the dependency is unconditional
 tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE AND NOT TFM_PARTITION_PLATFORM)
+tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE AND TFM_PLATFORM_NV_COUNTER_MODULE_DISABLED)
 tfm_invalid_config(PS_ROLLBACK_PROTECTION AND NOT PS_ENCRYPTION)
 
 ########################## FIH #################################################

zephyrproject-rtos/trusted-firmware-m

config/check_config.cmake
@@ -86,6 +86,7 @@ tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE AND NOT TFM_PARTITION_INTERNA
 # PS only uses the platform partition when PS_ROLLBACK_PROTECTION is ON, but
 # the dependency in the manifest file means the dependency is unconditional
 tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE AND NOT TFM_PARTITION_PLATFORM)
+tfm_invalid_config(PS_ROLLBACK_PROTECTION AND TFM_PLATFORM_NV_COUNTER_MODULE_DISABLED)
 tfm_invalid_config(PS_ROLLBACK_PROTECTION AND NOT PS_ENCRYPTION)
 
 ########################## FIH #################################################