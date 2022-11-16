020f1f37e81664c2b3ee80740f2f9c63c6e74c2a
David Hu <david.hu@arm.com>
2022-11-16 15:02:23 +0800

    Test: Enable SP when PSA Arch test is selected
    Automatically enable corresponding SPs when PSA Arch test is selected.
    
    Signed-off-by: David Hu <david.hu@arm.com>
    Change-Id: I697242a013c18b2e052fc037385fd8e48bee50a0

--- config/tests/config_test_psa_api.cmake
+++ config/tests/config_test_psa_api.cmake
@@ -19,7 +19,29 @@ if (("${TEST_PSA_API}" STREQUAL "CRYPTO") AND NOT (ITS_MAX_ASSET_SIZE GREATER 12
 endif()
 
 if ("${TEST_PSA_API}" STREQUAL "IPC")
+    # PSA Arch test partitions only support IPC model so far
+    set(CONFIG_TFM_SPM_BACKEND      "IPC"       CACHE STRING    "The SPM backend [IPC, SFN]")
     set(TFM_PARTITION_FF_TEST   ON)
 else()
     set(TFM_PARTITION_FF_TEST   OFF)
+endif()
+
+if ("${TEST_PSA_API}" STREQUAL "INITIAL_ATTESTATION")
+    set(TFM_PARTITION_INITIAL_ATTESTATION      ON       CACHE BOOL      "Enable Initial Attestation partition")
+    set(TFM_PARTITION_CRYPTO                   ON       CACHE BOOL      "Enable Crypto partition")
+    set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON       CACHE BOOL      "Enable Internal Trusted Storage partition")
+    set(TFM_PARTITION_PLATFORM                 ON       CACHE BOOL      "Enable Platform partition")
+endif()
+
+if ("${TEST_PSA_API}" STREQUAL "CRYPTO")
+    set(TFM_PARTITION_CRYPTO                   ON       CACHE BOOL      "Enable Crypto partition")
+    set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON       CACHE BOOL      "Enable Internal Trusted Storage partition")
+    set(TFM_PARTITION_PLATFORM                 ON       CACHE BOOL      "Enable Platform partition")
+endif()
+
+if ("${TEST_PSA_API}" STREQUAL "STORAGE")
+    set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON       CACHE BOOL      "Enable Internal Trusted Storage partition")
+    set(TFM_PARTITION_PROTECTED_STORAGE        ON       CACHE BOOL      "Enable Protected Storage partition")
+    set(TFM_PARTITION_CRYPTO                   ON       CACHE BOOL      "Enable Crypto partition")
+    set(TFM_PARTITION_PLATFORM                 ON       CACHE BOOL      "Enable Platform partition")
 endif()
