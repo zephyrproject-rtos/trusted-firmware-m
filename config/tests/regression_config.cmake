5cb08f9513e952f00ce6d1de0159ca9356eede23
shejia01 <jianliang.shen@arm.com>
2022-11-17 19:05:18 +0800

    FPU: Add sfn test support
    Make FPU test work under SFN model.
    
    Signed-off-by: Jianliang Shen <jianliang.shen@arm.com>
    Change-Id: Ibb3ae18802d2aaf883964a5d25e9c82ba5c41692

--- config/tests/regression_config.cmake
+++ config/tests/regression_config.cmake
@@ -35,12 +35,3 @@ if(TEST_S_ATTESTATION OR TEST_NS_ATTESTATION)
     set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON       CACHE BOOL      "Enable Internal Trusted Storage partition")
     set(TFM_PARTITION_CRYPTO                   ON       CACHE BOOL      "Enable Crypto partition")
 endif()
-
-if(TEST_S_FPU OR TEST_NS_FPU)
-    # FPU test partition only support IPC model so far
-    if(CONFIG_TFM_SPM_BACKEND STREQUAL "SFN")
-        message(FATAL_ERROR "FPU test partition is implemented in IPC model. SFN cannot support it.")
-    endif()
-    set(CONFIG_TFM_SPM_BACKEND                "IPC"       CACHE STRING    "The SPM backend [IPC, SFN]")
-    message("Set CONFIG_TFM_SPM_BACKEND to IPC Backend to support FPU regressions")
-endif()
