0a9ff1d4c8898d0b6abc8c2d64817c5f07940f79
Xinyu Zhang <xinyu.zhang@arm.com>
2022-11-23 14:51:05 +0800

    Platform: Remove warning on config redefinition
    It is not reasonable to show warning message when platforms redefine
    configs on their own platforms. Directly overriding config values is
    good enough.
    
    Signed-off-by: Xinyu Zhang <xinyu.zhang@arm.com>
    Change-Id: I1c9df54a154dbbb3e1a927bc4b2af7a608d8c3d2

--- platform/ext/target/arm/corstone1000/config_tfm_target.h
+++ platform/ext/target/arm/corstone1000/config_tfm_target.h
@@ -9,17 +9,11 @@
 #define __CONFIG_TFM_TARGET_H__
 
 /* Use stored NV seed to provide entropy */
-#if CRYPTO_NV_SEED
-#pragma message("CRYPTO_NV_SEED is redefined to 0.")
 #undef CRYPTO_NV_SEED
-#endif
 #define CRYPTO_NV_SEED                         0
 
 /* Size of output buffer in platform service. */
-#if PLATFORM_SERVICE_OUTPUT_BUFFER_SIZE != 256
-#pragma message("PLATFORM_SERVICE_OUTPUT_BUFFER_SIZE is redefined to 256.")
 #undef PLATFORM_SERVICE_OUTPUT_BUFFER_SIZE
-#endif
 #define PLATFORM_SERVICE_OUTPUT_BUFFER_SIZE    256
 
 #endif /* __CONFIG_TFM_TARGET_H__ */
