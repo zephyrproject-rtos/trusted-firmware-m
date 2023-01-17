1cb44978d6becaeb7f84250be7b1f567d87da94f
Raef Coles <raef.coles@arm.com>
2023-01-17 13:26:25 +0000

    CC3XX: Add remap option to DMA in ROM library
    To allow remapping of TCM addresses in RSS, primarily
    
    Change-Id: I9c875678adc1867de4a1b407680bb077a61f47a2
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/accelerator/cc312/cc312-rom/cc3xx_dma.c
+++ platform/ext/accelerator/cc312/cc312-rom/cc3xx_dma.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Arm Limited. All rights reserved.
+ * Copyright (c) 2021-2023, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -7,8 +7,59 @@
 
 #include "cc3xx_dma.h"
 
+#include <string.h>
+
 #include "cc3xx_reg_defs.h"
 #include "cc3xx_engine_state.h"
+
+static cc3xx_dma_remap_region_t remap_regions[CC3XX_DMA_REMAP_REGION_AM] = {0};
+
+static uint32_t remap_cpusel = 0;
+
+cc3xx_err_t cc3xx_dma_remap_region_init(uint32_t remap_region_idx,
+                                        cc3xx_dma_remap_region_t *region)
+{
+    if (remap_region_idx >= CC3XX_DMA_REMAP_REGION_AM) {
+        return CC3XX_ERR_GENERIC_ERROR;
+    }
+
+    memcpy(&remap_regions[remap_region_idx], region, sizeof(*region));
+
+    return CC3XX_ERR_SUCCESS;
+}
+
+cc3xx_err_t cc3xx_dma_remap_region_clear(uint32_t remap_region_idx)
+{
+    if (remap_region_idx >= CC3XX_DMA_REMAP_REGION_AM) {
+        return CC3XX_ERR_GENERIC_ERROR;
+    }
+
+    memset(&remap_regions[remap_region_idx], 0, sizeof(cc3xx_dma_remap_region_t));
+
+    return CC3XX_ERR_SUCCESS;
+}
+
+cc3xx_err_t cc3xx_dma_tcm_cpusel(uint32_t cpuid)
+{
+    remap_cpusel = cpuid;
+}
+
+static uintptr_t remap_addr(uintptr_t addr)
+{
+    uint32_t idx;
+    cc3xx_dma_remap_region_t *region;
+
+    for (idx = 0; idx < CC3XX_DMA_REMAP_REGION_AM; idx++) {
+        region = &remap_regions[idx];
+        if (addr >= region->region_base
+            && addr < region->region_base + region->region_size) {
+            return (addr - region->region_base) + region->remap_base
+                    + (region->remap_cpusel_offset * remap_cpusel);
+        }
+    }
+
+    return addr;
+}
 
 cc3xx_err_t cc3xx_dma_copy_data(void* dest, const void* src, size_t length)
 {
@@ -48,6 +99,8 @@ out:
 
 cc3xx_err_t cc3xx_dma_input_data(const void* buf, size_t length)
 {
+    uintptr_t remapped_buf;
+
     /* There's a max size of data the DMA can deal with */
     if (length >= 0x10000) {
         return CC3XX_ERR_INVALID_DATA;
@@ -59,8 +112,11 @@ cc3xx_err_t cc3xx_dma_input_data(const void* buf, size_t length)
     /* Reset the AXI_ERROR and SYM_DMA_COMPLETED interrupts */
     *CC3XX_REG_HOST_RGF_HOST_RGF_ICR |= 0x900U;
 
+    /* remap the address, particularly for TCMs */
+    remapped_buf = remap_addr((uintptr_t)buf);
+
     /* Set the data source */
-    *CC3XX_REG_DIN_SRC_LLI_WORD0 = (uint32_t)buf;
+    *CC3XX_REG_DIN_SRC_LLI_WORD0 = remapped_buf;
     /* Writing the length triggers the DMA */
     *CC3XX_REG_DIN_SRC_LLI_WORD1 = length;
 
@@ -81,8 +137,13 @@ cc3xx_err_t cc3xx_dma_input_data(const void* buf, size_t length)
 
 cc3xx_err_t cc3xx_dma_set_output(void* buf, size_t length)
 {
+    uintptr_t remapped_buf;
+
+    /* remap the address, particularly for TCMs */
+    remapped_buf = remap_addr((uintptr_t)buf);
+
     /* Set the data target */
-    *CC3XX_REG_DOUT_DST_LLI_WORD0 = (uint32_t)buf;
+    *CC3XX_REG_DOUT_DST_LLI_WORD0 = remapped_buf;
     /* And the length */
     *CC3XX_REG_DOUT_DST_LLI_WORD1 = length;
 
