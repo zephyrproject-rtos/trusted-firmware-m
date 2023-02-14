7e8be9709d5f53ac122d004a906a746f7ab457a7
Raef Coles <raef.coles@arm.com>
2023-02-14 15:17:37 +0000

    RSS: Run all provisioning in BL1
    Move all provisioning into BL1. This is currently WIP as not all
    provisioning data is loaded from the bundle, and CM/DM split isn't done
    properly yet.
    
    Change-Id: Ie32c090fabc8b9ca354553f1f3a13236cf12ce9a
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/arm/rss/common/config.cmake
+++ platform/ext/target/arm/rss/common/config.cmake
@@ -18,6 +18,7 @@ set(CRYPTO_HW_ACCELERATOR               ON         CACHE BOOL     "Whether to en
 set(PLATFORM_DEFAULT_OTP                OFF        CACHE BOOL     "Use trusted on-chip flash to implement OTP memory")
 set(PLATFORM_DEFAULT_CRYPTO_KEYS        OFF        CACHE BOOL     "Use default crypto keys implementation.")
 set(PLATFORM_DEFAULT_PROVISIONING       OFF        CACHE BOOL     "Use default provisioning implementation")
+set(TFM_BL1_DEFAULT_PROVISIONING        OFF        CACHE BOOL     "Whether BL1_1 will use default provisioning")
 # CFB mode is not supported by CC312
 set(TFM_CRYPTO_TEST_ALG_CFB             OFF        CACHE BOOL     "Test CFB cryptography mode")
 set(PLATFORM_DEFAULT_NV_COUNTERS        OFF        CACHE BOOL     "Use default nv counter implementation.")
