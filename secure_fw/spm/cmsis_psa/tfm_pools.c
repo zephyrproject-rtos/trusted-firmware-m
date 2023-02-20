8b9ab1617617ac7ef7b5457d21ffb3f542167513
Ken Liu <Ken.Liu@arm.com>
2023-02-20 16:29:20 +0800

    SPM: Simplify pool object validating function
    The previous method performs many calculations which can be optimized.
    
    Signed-off-by: Ken Liu <Ken.Liu@arm.com>
    Change-Id: I23f0634ac0d63d2e8c3b0e7c9adc2798bb1cc42f

--- secure_fw/spm/cmsis_psa/tfm_pools.c
+++ secure_fw/spm/cmsis_psa/tfm_pools.c
@@ -47,7 +47,7 @@ psa_status_t tfm_pool_init(struct tfm_pool_instance_t *pool, size_t poolsz,
 
     /* Prepare instance and insert to pool list */
     pool->chunksz = chunksz;
-    pool->chunk_count = num;
+    pool->pool_sz = poolsz;
 
     return PSA_SUCCESS;
 }
@@ -83,23 +83,17 @@ bool is_valid_chunk_data_in_pool(struct tfm_pool_instance_t *pool,
                                  uint8_t *data)
 {
     const uintptr_t chunks_start = (uintptr_t)(pool->chunks);
-    const size_t chunks_size = pool->chunksz + sizeof(struct tfm_pool_chunk_t);
-    const size_t chunk_count = pool->chunk_count;
-    const uintptr_t chunks_end = chunks_start + chunks_size * chunk_count;
-    uintptr_t pool_chunk_address = 0;
+    const size_t chunks_offset = (uintptr_t)data - chunks_start;
 
     /* Check that the message was allocated from the pool. */
-    if ((uintptr_t)data < chunks_start || (uintptr_t)data >= chunks_end) {
+    if ((uintptr_t)data < chunks_start || chunks_offset >= pool->pool_sz) {
         return false;
     }
 
-    pool_chunk_address =
-        (uint32_t)TO_CONTAINER(data, struct tfm_pool_chunk_t, data);
-
-    /* Make sure that the chunk containing the message is aligned on */
-    /* chunk boundary in the pool. */
-    if ((pool_chunk_address - chunks_start) % chunks_size != 0) {
+    if ((chunks_offset % (pool->chunksz + sizeof(struct tfm_pool_chunk_t)))
+        != offsetof(struct tfm_pool_chunk_t, data)) {
         return false;
     }
+
     return true;
 }
