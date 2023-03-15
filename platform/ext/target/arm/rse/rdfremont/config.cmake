e7e1ca8b60186b532c6355594e515df24407d02b
Shriram K <shriram.k@arm.com>
2023-03-15 13:05:38 +0530

    RSE: Fremont: Support for RD-Fremont-Cfg1 variant
    Add support for RD-Fremont-Cfg1 platform variant. It's a variant of the
    RD-Fremont platform with reduced 3x3 mesh size and core count reduced to
    8. Hence, add support for configuring the PLAT_LCP_COUNT variable based
    on the TFM_PLATFORM_VARIANT flag.
    
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I67198427dacbdf01a28e23c0370f241e45cc60a6

--- platform/ext/target/arm/rse/rdfremont/config.cmake
+++ platform/ext/target/arm/rse/rdfremont/config.cmake
@@ -19,7 +19,11 @@ set(MCUBOOT_IMAGE_NUMBER                6        CACHE STRING  "Number of images
 set(RSE_USE_HOST_UART                   OFF      CACHE BOOL    "Whether RSE should use the UART from the host system (opposed to dedicated UART private to RSE)")
 set(RSE_USE_HOST_FLASH                  OFF      CACHE BOOL    "Enable RSE using the host flash.")
 set(RSE_HAS_EXPANSION_PERIPHERALS       ON       CACHE BOOL    "Whether RSE has sub-platform specific peripherals in the expansion layer")
-set(PLAT_LCP_COUNT                      16       CACHE STRING  "Number of LCPs to load")
+if (TFM_PLATFORM_VARIANT STREQUAL "1")
+    set(PLAT_LCP_COUNT                      8        CACHE STRING  "Number of LCPs to load")
+else()
+    set(PLAT_LCP_COUNT                      16       CACHE STRING  "Number of LCPs to load")
+endif()
 set(RSE_LOAD_NS_IMAGE                   OFF      CACHE BOOL    "Whether to load an NS image")
 set(CONFIG_TFM_SPM_BACKEND              IPC      CACHE STRING  "The SPM backend")
 
