b30cb415a04980a552ba63b868862bac4edcb416
David Brown <david.brown@linaro.org>
2023-07-06 13:22:32 -0600

    Revert "platform: nordic_nrf: Add nrf9161 DK and nrf9120 SoC support"
    This reverts commit 99825b3c1e06e906016bf7ecc14505377d0a26ec.

--- platform/ext/target/nordic_nrf/nrf9161dk_nrf9161/config.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2023, Nordic Semiconductor ASA.
-#
-# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
-#
-
-set(PLATFORM_PATH platform/ext/target/${TFM_PLATFORM}/..)
-
-include(${PLATFORM_PATH}/common/nrf91/config.cmake)
