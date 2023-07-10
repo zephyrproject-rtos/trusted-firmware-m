zephyrproject-rtos/trusted-firmware-m

commit 9a6d90a247c5f072dfc5368a07539eb3624cf863

SPM: Allocate handles locally if no connection-based services in SFN
It is a typical function-call-like procedure when TF-M is under SFN
model and all services are stateless. Handle does not need to be
retained all the time because its lifecycle ends when the service
routine returned. Allocate handles locally in the stack so it can be
used before service routine and freed by stack-popping after its
lifecycle ends.

This patch makes the following changes:
1. Move dynamic connection pool management codes to spm_connection_pool.c.
2. Implement local static handle management in spm_local_connection.c.
3. Update architecture codes to surport operation on PSPLIM which is
   required by local static handle.

Signed-off-by: Xinyu Zhang <xinyu.zhang@arm.com>
Change-Id: I80ac0921f410ebfcd5789a970691bffc1ad5054b

secure_fw/spm/core/arch/tfm_arch_v6m_v7m.h
@@ -16,6 +16,8 @@
 #error "Armv6-M/Armv7-M can only support multi-core TF-M now."
 #endif
 
+extern uint32_t psp_limit;
+
 #if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
 #define EXC_RETURN_FTYPE                        (1 << 4)
 #endif
@@ -100,17 +102,27 @@ __STATIC_INLINE bool is_stack_alloc_fp_space(uint32_t lr)
 #endif
 
 /**
+ * \brief Get PSP Limit.
+ *
+ * \retval psp limit        Limit of PSP stack.
+ */
+__STATIC_INLINE uint32_t tfm_arch_get_psplim(void)
+{
+    return psp_limit;
+}
+
+/**
  * \brief Set PSP limit value.
  *
  * \param[in] psplim        PSP limit value to be written.
  */
 __STATIC_INLINE void tfm_arch_set_psplim(uint32_t psplim)
 {
-    /*
-     * Defined as an empty function now.
-     * The PSP limit value can be used in more strict memory check.
-     */
-    (void)psplim;
+    if (psplim > __get_PSP()) {
+        tfm_core_panic();
+    }
+
+    psp_limit = psplim;
 }
 
 /**

config/config_base.h
@@ -231,9 +231,11 @@
 
 /* SPM Partition Configs */
 
+#ifdef CONFIG_TFM_CONNECTION_POOL_ENABLE
 /* The maximal number of secure services that are connected or requested at the same time */
 #ifndef CONFIG_TFM_CONN_HANDLE_MAX_NUM
 #define CONFIG_TFM_CONN_HANDLE_MAX_NUM          8
+#endif
 #endif
 
 /* Disable the doorbell APIs */

config/profile/config_profile_small.h
@@ -142,9 +142,6 @@
 
 /* SPM Partition Configs */
 
-/* The maximal number of secure services that are connected or requested at the same time */
-#define CONFIG_TFM_CONN_HANDLE_MAX_NUM         3
-
 /* Disable the doorbell APIs */
 #define CONFIG_TFM_DOORBELL_API                0
 

secure_fw/spm/CMakeLists.txt
@@ -58,6 +58,8 @@ target_sources(tfm_spm
         $<$<BOOL:${CONFIG_TFM_SPM_BACKEND_IPC}>:core/thread.c>
         $<$<BOOL:${TFM_NS_MANAGE_NSID}>:ns_client_ext/tfm_ns_ctx.c>
         ns_client_ext/tfm_spm_ns_ctx.c
+        $<$<OR:$<BOOL:${CONFIG_TFM_SPM_BACKEND_IPC}>,$<BOOL:${CONFIG_TFM_CONNECTION_BASED_SERVICE_API}>>:core/spm_connection_pool.c>
+        $<$<NOT:$<OR:$<BOOL:${CONFIG_TFM_SPM_BACKEND_IPC}>,$<BOOL:${CONFIG_TFM_CONNECTION_BASED_SERVICE_API}>>>:core/spm_local_connection.c>
         #TODO add other arches
         $<$<STREQUAL:${TFM_SYSTEM_ARCHITECTURE},armv8.1-m.main>:core/arch/tfm_arch_v8m_main.c>
         $<$<STREQUAL:${TFM_SYSTEM_ARCHITECTURE},armv8-m.base>:core/arch/tfm_arch_v8m_base.c>
@@ -115,6 +117,11 @@ set_source_files_properties(tfm_secure_api.c
             $<$<C_COMPILER_ID:ARMClang>:-Wno-implicit-function-declaration>
             $<$<C_COMPILER_ID:GNU>:-Wno-implicit-function-declaration>
             $<$<C_COMPILER_ID:IAR>:>
+)
+
+target_compile_definitions(tfm_config
+    INTERFACE
+        $<$<OR:$<BOOL:${CONFIG_TFM_SPM_BACKEND_IPC}>,$<BOOL:${CONFIG_TFM_CONNECTION_BASED_SERVICE_API}>>:CONFIG_TFM_CONNECTION_POOL_ENABLE>
 )
 
 ############################ TFM arch ##########################################

secure_fw/spm/core/arch/tfm_arch_v6m_v7m.c
@@ -20,6 +20,8 @@
 #error "Unsupported ARM Architecture."
 #endif
 
+uint32_t psp_limit;
+
 /* Delcaraction flag to control the scheduling logic in PendSV. */
 uint32_t scheduler_lock = SCHEDULER_UNLOCKED;
 

secure_fw/spm/core/spm.h
@@ -131,6 +131,7 @@ struct service_t {
 int32_t tfm_spm_partition_get_running_partition_id(void);
 
 /******************** Service handle management functions ********************/
+void spm_init_connection_space(void);
 
 struct connection_t *spm_allocate_connection(void);
 

secure_fw/spm/core/spm_connection_pool.c
@@ -1,0 +1,152 @@
+/*
+ * Copyright (c) 2018-2023, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "critical_section.h"
+#include "internal_status_code.h"
+#include "spm.h"
+#include "tfm_pools.h"
+#include "load/service_defs.h"
+
+#if !(defined CONFIG_TFM_CONN_HANDLE_MAX_NUM) || (CONFIG_TFM_CONN_HANDLE_MAX_NUM == 0)
+#error "CONFIG_TFM_CONN_HANDLE_MAX_NUM must be defined and not zero."
+#endif
+
+/* Pools */
+TFM_POOL_DECLARE(connection_pool, sizeof(struct connection_t),
+                 CONFIG_TFM_CONN_HANDLE_MAX_NUM);
+
+/*********************** Connection handle conversion APIs *******************/
+
+#define CONVERSION_FACTOR_BITOFFSET    3
+#define CONVERSION_FACTOR_VALUE        (1 << CONVERSION_FACTOR_BITOFFSET)
+/* Set 32 as the maximum */
+#define CONVERSION_FACTOR_VALUE_MAX    0x20
+
+#if CONVERSION_FACTOR_VALUE > CONVERSION_FACTOR_VALUE_MAX
+#error "CONVERSION FACTOR OUT OF RANGE"
+#endif
+
+static uint32_t loop_index;
+
+/*
+ * A connection instance connection_t allocated inside SPM is actually a memory
+ * address among the connection pool. Return this connection to the client directly
+ * exposes information of secure memory address. In this case, converting the
+ * connection into another handle value does not represent the memory address
+ * to avoid exposing secure memory directly to clients.
+ *
+ * This function converts the connection instance into another value by scaling
+ * the connection in pool offset, the converted value is named as a user handle.
+ *
+ * The formula:
+ *  handle =      (p_connection - POOL_START) * CONVERSION_FACTOR_VALUE +
+ *                CLIENT_HANDLE_VALUE_MIN + loop_index
+ * where:
+ *  CONVERSION_FACTOR_VALUE = 1 << CONVERSION_FACTOR_BITOFFSET, and should not
+ *  exceed CONVERSION_FACTOR_VALUE_MAX.
+ *
+ *  p_connection    in RANGE[POOL_START, POOL_END]
+ *  handle          in RANGE[CLIENT_HANDLE_VALUE_MIN, 0x3FFFFFFF]
+ *  loop_index      in RANGE[0, CONVERSION_FACTOR_VALUE - 1]
+ *
+ *  note:
+ *  loop_index is used to promise same connection instance is converted into
+ *  different user handles in short time.
+ */
+psa_handle_t connection_to_handle(struct connection_t *p_connection)
+{
+    psa_handle_t handle;
+
+    loop_index = (loop_index + 1) % CONVERSION_FACTOR_VALUE;
+    handle = (psa_handle_t)((((uintptr_t)p_connection -
+                  (uintptr_t)connection_pool) << CONVERSION_FACTOR_BITOFFSET) +
+                  CLIENT_HANDLE_VALUE_MIN + loop_index);
+
+    return handle;
+}
+
+/*
+ * This function converts a user handle into a corresponded connection instance.
+ * The converted value is validated before returning, an invalid handle instance
+ * is returned as NULL.
+ *
+ * The formula:
+ *  p_connection =    ((handle - CLIENT_HANDLE_VALUE_MIN) /
+ *                    CONVERSION_FACTOR_VALUE) + POOL_START
+ * where:
+ *  CONVERSION_FACTOR_VALUE = 1 << CONVERSION_FACTOR_BITOFFSET, and should not
+ *  exceed CONVERSION_FACTOR_VALUE_MAX.
+ *
+ *  p_connection    in RANGE[POOL_START, POOL_END]
+ *  handle          in RANGE[CLIENT_HANDLE_VALUE_MIN, 0x3FFFFFFF]
+ *  loop_index      in RANGE[0, CONVERSION_FACTOR_VALUE - 1]
+ */
+struct connection_t *handle_to_connection(psa_handle_t handle)
+{
+    struct connection_t *p_connection;
+
+    if (handle == PSA_NULL_HANDLE) {
+        return NULL;
+    }
+
+    p_connection = (struct connection_t *)((((uintptr_t)handle -
+                    CLIENT_HANDLE_VALUE_MIN) >> CONVERSION_FACTOR_BITOFFSET) +
+                    (uintptr_t)connection_pool);
+
+    return p_connection;
+}
+
+/* Service handle management functions */
+void spm_init_connection_space(void)
+{
+    if (tfm_pool_init(connection_pool,
+                      POOL_BUFFER_SIZE(connection_pool),
+                      sizeof(struct connection_t),
+                      CONFIG_TFM_CONN_HANDLE_MAX_NUM) != PSA_SUCCESS) {
+        tfm_core_panic();
+    }
+}
+
+struct connection_t *spm_allocate_connection(void)
+{
+    struct connection_t *p_connection;
+
+    /* Get buffer for handle list structure from handle pool */
+    p_connection = (struct connection_t *)tfm_pool_alloc(connection_pool);
+    if (!p_connection) {
+        return NULL;
+    }
+
+    spm_memset(p_connection, 0, sizeof(*p_connection));
+
+    p_connection->status = TFM_HANDLE_STATUS_IDLE;
+
+    return p_connection;
+}
+
+psa_status_t spm_validate_connection(const struct connection_t *p_connection)
+{
+    /* Check the handle address is valid */
+    if (is_valid_chunk_data_in_pool(connection_pool,
+                                    (uint8_t *)p_connection) != true) {
+        return SPM_ERROR_GENERIC;
+    }
+
+    return PSA_SUCCESS;
+}
+
+void spm_free_connection(struct connection_t *p_connection)
+{
+    struct critical_section_t cs_assert = CRITICAL_SECTION_STATIC_INIT;
+
+    SPM_ASSERT(p_connection != NULL);
+
+    CRITICAL_SECTION_ENTER(cs_assert);
+    /* Back handle buffer to pool */
+    tfm_pool_free(connection_pool, p_connection);
+    CRITICAL_SECTION_LEAVE(cs_assert);
+}

secure_fw/spm/core/spm_ipc.c
@@ -42,139 +42,9 @@
 #include "load/spm_load_api.h"
 #include "tfm_nspm.h"
 
-#if !(defined CONFIG_TFM_CONN_HANDLE_MAX_NUM) || (CONFIG_TFM_CONN_HANDLE_MAX_NUM == 0)
-#error "CONFIG_TFM_CONN_HANDLE_MAX_NUM must be defined and not zero."
-#endif
-
 /* Partition and service runtime data list head/runtime data table */
 static struct service_head_t services_listhead;
 struct service_t *stateless_services_ref_tbl[STATIC_HANDLE_NUM_LIMIT];
-
-/* Pools */
-TFM_POOL_DECLARE(connection_pool, sizeof(struct connection_t),
-                 CONFIG_TFM_CONN_HANDLE_MAX_NUM);
-
-/*********************** Connection handle conversion APIs *******************/
-
-#define CONVERSION_FACTOR_BITOFFSET    3
-#define CONVERSION_FACTOR_VALUE        (1 << CONVERSION_FACTOR_BITOFFSET)
-/* Set 32 as the maximum */
-#define CONVERSION_FACTOR_VALUE_MAX    0x20
-
-#if CONVERSION_FACTOR_VALUE > CONVERSION_FACTOR_VALUE_MAX
-#error "CONVERSION FACTOR OUT OF RANGE"
-#endif
-
-static uint32_t loop_index;
-
-/*
- * A handle instance psa_handle_t allocated inside SPM is actually a memory
- * address among the handle pool. Return this handle to the client directly
- * exposes information of secure memory address. In this case, converting the
- * handle into another value does not represent the memory address to avoid
- * exposing secure memory directly to clients.
- *
- * This function converts the handle instance into another value by scaling the
- * handle in pool offset, the converted value is named as a user handle.
- *
- * The formula:
- *  handle =      (p_connection - POOL_START) * CONVERSION_FACTOR_VALUE +
- *                CLIENT_HANDLE_VALUE_MIN + loop_index
- * where:
- *  CONVERSION_FACTOR_VALUE = 1 << CONVERSION_FACTOR_BITOFFSET, and should not
- *  exceed CONVERSION_FACTOR_VALUE_MAX.
- *
- *  p_connection    in RANGE[POOL_START, POOL_END]
- *  handle          in RANGE[CLIENT_HANDLE_VALUE_MIN, 0x3FFFFFFF]
- *  loop_index      in RANGE[0, CONVERSION_FACTOR_VALUE - 1]
- *
- *  note:
- *  loop_index is used to promise same handle instance is converted into
- *  different user handles in short time.
- */
-psa_handle_t connection_to_handle(struct connection_t *p_connection)
-{
-    psa_handle_t handle;
-
-    loop_index = (loop_index + 1) % CONVERSION_FACTOR_VALUE;
-    handle = (psa_handle_t)((((uintptr_t)p_connection -
-                  (uintptr_t)connection_pool) << CONVERSION_FACTOR_BITOFFSET) +
-                  CLIENT_HANDLE_VALUE_MIN + loop_index);
-
-    return handle;
-}
-
-/*
- * This function converts a user handle into a corresponded handle instance.
- * The converted value is validated before returning, an invalid handle instance
- * is returned as NULL.
- *
- * The formula:
- *  p_connection =    ((handle - CLIENT_HANDLE_VALUE_MIN) /
- *                    CONVERSION_FACTOR_VALUE) + POOL_START
- * where:
- *  CONVERSION_FACTOR_VALUE = 1 << CONVERSION_FACTOR_BITOFFSET, and should not
- *  exceed CONVERSION_FACTOR_VALUE_MAX.
- *
- *  p_connection    in RANGE[POOL_START, POOL_END]
- *  handle          in RANGE[CLIENT_HANDLE_VALUE_MIN, 0x3FFFFFFF]
- *  loop_index      in RANGE[0, CONVERSION_FACTOR_VALUE - 1]
- */
-struct connection_t *handle_to_connection(psa_handle_t handle)
-{
-    struct connection_t *p_connection;
-
-    if (handle == PSA_NULL_HANDLE) {
-        return NULL;
-    }
-
-    p_connection = (struct connection_t *)((((uintptr_t)handle -
-                    CLIENT_HANDLE_VALUE_MIN) >> CONVERSION_FACTOR_BITOFFSET) +
-                    (uintptr_t)connection_pool);
-
-    return p_connection;
-}
-
-/* Service handle management functions */
-struct connection_t *spm_allocate_connection(void)
-{
-    struct connection_t *p_handle;
-
-    /* Get buffer for handle list structure from handle pool */
-    p_handle = (struct connection_t *)tfm_pool_alloc(connection_pool);
-    if (!p_handle) {
-        return NULL;
-    }
-
-    spm_memset(p_handle, 0, sizeof(*p_handle));
-
-    p_handle->status = TFM_HANDLE_STATUS_IDLE;
-
-    return p_handle;
-}
-
-psa_status_t spm_validate_connection(const struct connection_t *handle)
-{
-    /* Check the handle address is valid */
-    if (is_valid_chunk_data_in_pool(connection_pool,
-                                    (uint8_t *)handle) != true) {
-        return SPM_ERROR_GENERIC;
-    }
-
-    return PSA_SUCCESS;
-}
-
-void spm_free_connection(struct connection_t *p_connection)
-{
-    struct critical_section_t cs_assert = CRITICAL_SECTION_STATIC_INIT;
-
-    SPM_ASSERT(p_connection != NULL);
-
-    CRITICAL_SECTION_ENTER(cs_assert);
-    /* Back handle buffer to pool */
-    tfm_pool_free(connection_pool, p_connection);
-    CRITICAL_SECTION_LEAVE(cs_assert);
-}
 
 /* Partition management functions */
 
@@ -438,10 +308,7 @@ uint32_t tfm_spm_init(void)
     uint32_t service_setting;
     fih_int fih_rc = FIH_FAILURE;
 
-    tfm_pool_init(connection_pool,
-                  POOL_BUFFER_SIZE(connection_pool),
-                  sizeof(struct connection_t),
-                  CONFIG_TFM_CONN_HANDLE_MAX_NUM);
+    spm_init_connection_space();
 
     UNI_LISI_INIT_NODE(PARTITION_LIST_ADDR, next);
     UNI_LISI_INIT_NODE(&services_listhead, next);

commit 9d05a5367df970af4d5bdf17135c78a303d8aad6

SPM: Skip PSP overflow checking when setting PSP Limit
Setting the PSP Limit is a fundamental operation which can be
called at any time by a handler mode code (not using PSP),
even when the PSP is not set then the overflow check panics.
This patch removes unnecessary check.The caller should guarantee
valid values when setting PSP and PSP Limit. Fixes the issue
introduced by commit 9a6d90a247c5f072dfc5368a07539eb3624cf863

NOTE: v6m&v7m do not have hardware PSP Limit register, but the
PSP limit value is still necessary for local storage usage.
Hence a software limit variable is applied for v6m&v7m.

Signed-off-by: Emekcan Aras <emekcan.aras@arm.com>
Change-Id: I6ee377274d1dc37ac5408838bfaf104277644e62

secure_fw/spm/core/arch/tfm_arch_v6m_v7m.h
@@ -118,10 +118,6 @@ __STATIC_INLINE uint32_t tfm_arch_get_psplim(void)
  */
 __STATIC_INLINE void tfm_arch_set_psplim(uint32_t psplim)
 {
-    if (psplim > __get_PSP()) {
-        tfm_core_panic();
-    }
-
     psp_limit = psplim;
 }
 