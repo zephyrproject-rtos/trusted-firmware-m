fa51dd6c313feaa518bb064c5e2fbd84a59932cb
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2023-07-17 10:48:13 +0200

    platform: nordic_nrf: Don't configure TFM_SYSTEM_DSP OFF
    preload.cmake should contain platform-specific configuration. But TF-M
    does not support TFM_SYSTEM_DSP so it is not necessary to configure it
    for each platform.
    
    NB: These platforms do support DSP instructions.
    
    Change-Id: Ifd3a67ac2276fa8d7ceec79582f8cec02b7cac54
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/nrf9120/preload.cmake
+++ platform/ext/target/nordic_nrf/common/nrf9120/preload.cmake
@@ -12,7 +12,6 @@
 # Set architecture and CPU
 set(TFM_SYSTEM_PROCESSOR cortex-m33)
 set(TFM_SYSTEM_ARCHITECTURE armv8-m.main)
-set(TFM_SYSTEM_DSP OFF)
 set(CONFIG_TFM_FP_ARCH "fpv5-sp-d16")
 
 add_compile_definitions(
