0169d1002361478ac1186cf5fd3332755d7caece
Jamie Fox <jamie.fox@arm.com>
2023-08-10 18:17:27 +0100

    Platform: RSS: Fix multi-core mem check
    The new multi-core mem check did not work on RSS as the
    tfm_get_mem_region_security_attr() function results in "secure"
    attribute, since the vectors have either been copied into Secure memory
    from MHU, or other-core memory is being accessed via the ATU window in
    Secure region. This patch works around the issue by setting the
    attribute to non-secure.
    
    Additionally, in the 'non-secure' memory check, use the
    tfm_hal_memory_check() function to check the access against the current
    MPU configuration using the cmse_check_address_range intrinsic, as this
    is preferable to manually comparing to defined regions.
    
    Signed-off-by: Jamie Fox <jamie.fox@arm.com>
    Change-Id: If73515762fcad27eb029925b6b4756eb35a64086

--- platform/ext/target/arm/rss/common/tfm_hal_multi_core.c
+++ platform/ext/target/arm/rss/common/tfm_hal_multi_core.c
@@ -1,11 +1,14 @@
 /*
- * Copyright (c) 2022, Arm Limited. All rights reserved.
+ * Copyright (c) 2022-2023, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
  */
 
 #include "tfm_hal_multi_core.h"
+#include "tfm_hal_isolation.h"
+#include "psa/service.h"
+#include "current.h"
 
 void tfm_hal_boot_ns_cpu(uintptr_t start_addr)
 {
@@ -23,23 +26,54 @@ void tfm_hal_wait_for_ns_cpu_ready(void)
 void tfm_hal_get_mem_security_attr(const void *p, size_t s,
                                    struct security_attr_info_t *p_attr)
 {
-    /* Check static memory layout to get memory attributes */
-    tfm_get_mem_region_security_attr(p, s, p_attr);
+    /* Multi-core mem-check expects that the memory will be Non-secure, but on
+     * RSS it will be Secure (either MHU messages copied into local memory or a
+     * Secure-access ATU window). Pretend it is NS to keep the mem-check happy.
+     */
+    (void)p;
+    (void)s;
+    p_attr->is_valid = true;
+    p_attr->is_secure = false;
 }
 
 void tfm_hal_get_secure_access_attr(const void *p, size_t s,
                                     struct mem_attr_info_t *p_attr)
 {
-    /* Check static memory layout to get memory attributes */
-    tfm_get_secure_mem_region_attr(p, s, p_attr);
-#if TFM_LVL >= 2
-    p_attr->is_mpu_enabled = true;
-#endif
+    /* Should be unreachable */
+    (void)p;
+    (void)s;
+    (void)p_attr;
+    psa_panic();
 }
 
 void tfm_hal_get_ns_access_attr(const void *p, size_t s,
                                 struct mem_attr_info_t *p_attr)
 {
-    /* Check static memory layout to get memory attributes */
-    tfm_get_ns_mem_region_attr(p, s, p_attr);
+#if TFM_ISOLATION_LEVEL >= 2
+    p_attr->is_mpu_enabled = true;
+#endif
+    p_attr->is_xn = true;
+
+    /* Assume unpriv access is not required */
+    p_attr->is_unpriv_rd_allow = false;
+    p_attr->is_unpriv_wr_allow = false;
+
+    if (tfm_hal_memory_check((GET_CURRENT_COMPONENT())->boundary, (uintptr_t)p, s,
+                             TFM_HAL_ACCESS_READWRITE) == TFM_HAL_SUCCESS) {
+        /* MPU region is RW */
+        p_attr->is_valid = true;
+        p_attr->is_priv_rd_allow = true;
+        p_attr->is_priv_wr_allow = true;
+    } else if (tfm_hal_memory_check((GET_CURRENT_COMPONENT())->boundary, (uintptr_t)p, s,
+                                    TFM_HAL_ACCESS_READABLE) == TFM_HAL_SUCCESS) {
+        /* MPU region is RO */
+        p_attr->is_valid = true;
+        p_attr->is_priv_rd_allow = true;
+        p_attr->is_priv_wr_allow = false;
+    } else {
+        /* MPU region is not accessible */
+        p_attr->is_valid = false;
+        p_attr->is_priv_rd_allow = false;
+        p_attr->is_priv_wr_allow = false;
+    }
 }
