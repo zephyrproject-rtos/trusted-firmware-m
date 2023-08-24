dd0019466155fc07d4802c5241c8abe5975d39c0
Summer Qin <summer.qin@arm.com>
2023-08-24 13:57:12 +0800

    Test: More invalid config checks for PSA Arch crypto test
    TF-M Profile Medium, Profile Medium-ARoT-less and Profile Small
    contain partial crypto APIs only while PSA Arch crypto test covers
    all APIs test cases. Add invalid config checks for this case.
    
    Signed-off-by: Summer Qin <summer.qin@arm.com>
    Change-Id: Ic3cad423cd272f7ce1b4b811fb4a4da1cc92e630

--- config/check_config.cmake
+++ config/check_config.cmake
@@ -44,6 +44,11 @@ tfm_invalid_config(TEST_PSA_API STREQUAL "INTERNAL_TRUSTED_STORAGE" AND NOT TFM_
 tfm_invalid_config(TEST_PSA_API STREQUAL "PROTECTED_STORAGE" AND NOT TFM_PARTITION_PROTECTED_STORAGE)
 tfm_invalid_config(TEST_PSA_API STREQUAL "STORAGE" AND NOT TFM_PARTITION_INTERNAL_TRUSTED_STORAGE)
 tfm_invalid_config(TEST_PSA_API STREQUAL "STORAGE" AND NOT TFM_PARTITION_PROTECTED_STORAGE)
+# PSA Arch crypto test intends to test all PSA crypto APIs. Therefore PSA Arch crypto test
+# cannot support to test TF-M Profile Medium, Profile Medium-ARoT-less and Profile Small.
+tfm_invalid_config(TEST_PSA_API STREQUAL "CRYPTO" AND TFM_PROFILE STREQUAL "profile_medium")
+tfm_invalid_config(TEST_PSA_API STREQUAL "CRYPTO" AND TFM_PROFILE STREQUAL "profile_medium_arotless")
+tfm_invalid_config(TEST_PSA_API STREQUAL "CRYPTO" AND TFM_PROFILE STREQUAL "profile_small")
 
 ########################## BL2 #################################################
 
