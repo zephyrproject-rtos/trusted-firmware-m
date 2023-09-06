ae23025113ed474d25b1fbfe120d48d5a9914491
Joel Goddard <Joel.Goddard@arm.com>
2023-09-06 18:01:22 +0100

    RSS: Fremont: Add cmake files
    Add required cmake files to compile BL1_1, BL1_2, BL2, Secure and
    Non-Secure for RD-Fremont RSS.
    
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Change-Id: I5b62d1b602f8144f7e0d594e67d3aef5ae240d83

---
+++ platform/ext/target/arm/rss/rdfremont/config.cmake
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2023, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+# Note: Cached varaibles always take the first value set, while normal
+# variables always take the last.
+
+set(TFM_BL1_MEMORY_MAPPED_FLASH         OFF      CACHE BOOL    "Whether BL1 can directly access flash content")
+set(PLATFORM_HAS_BOOT_DMA               OFF      CACHE BOOL    "Enable dma support for memory transactions for bootloader")
+set(PLAT_MHU_VERSION                    3        CACHE STRING  "Supported MHU version by platform")
+set(MCUBOOT_IMAGE_NUMBER                2        CACHE STRING  "Number of images supported by MCUBoot")
+set(RSS_USE_HOST_UART                   OFF      CACHE BOOL    "Whether RSS should use the UART from the host system (opposed to dedicated UART private to RSS)")
+set(RSS_USE_HOST_FLASH                  OFF      CACHE BOOL    "Enable RSS using the host flash.")
+set(RSS_HAS_EXPANSION_PERIPHERALS       ON       CACHE BOOL    "Whether RSS has sub-platform specific peripherals in the expansion layer")
+set(RSS_LOAD_NS_IMAGE                   OFF      CACHE BOOL    "Whether to load an NS image")
+
+# Once all cache options are set, set common options as fallback
+include(${CMAKE_CURRENT_LIST_DIR}/../common/config.cmake)
+
+if (RSS_LOAD_NS_IMAGE)
+    set(CONFIG_TFM_USE_TRUSTZONE        ON)
+else()
+    set(CONFIG_TFM_USE_TRUSTZONE        OFF)
+endif()
