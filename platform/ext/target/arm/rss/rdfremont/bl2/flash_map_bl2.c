2a239e8fd59968249f96d617ee34ead52078ec11
Joel Goddard <Joel.Goddard@arm.com>
2023-09-07 15:44:18 +0100

    RSS: Fremont: Add hal apis to load firmware images
    Add following files which provides necessary api for loading firmware
    images:
    * Add boot_hal_bl2.c with MCUBoot required functions for loading
      SecureFW and Non-SecureFW from flash.
    * Add flash_map_bl2.c with flash maps and the load addresses for
      SecureFW and Non-SecureFW images as needed by MBUBoot bootloader.
    
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Signed-off-by: Sathyam Panda <sathyam.panda@arm.com>
    Change-Id: Ic02cbc7e9b2c7ff3fca2a8c40c13d7b467e0c2e3

---
+++ platform/ext/target/arm/rss/rdfremont/bl2/flash_map_bl2.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2023, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "flash_map/flash_map.h"
+
+#include "Driver_Flash.h"
+#include "bl2_image_id.h"
+#include "target.h"
+
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof((arr)[0]))
+
+/* Flash device names must be specified by target */
+extern ARM_DRIVER_FLASH FLASH_DEV_NAME;
+
+/* Default Drivers list */
+const ARM_DRIVER_FLASH *flash_driver[] = {
+    &FLASH_DEV_NAME,
+};
+const int flash_driver_entry_num = ARRAY_SIZE(flash_driver);
+
+const struct flash_area flash_map[] = {
+    {
+        .fa_id = FLASH_AREA_2_ID,
+        .fa_device_id = FLASH_DEVICE_ID,
+        .fa_driver = &FLASH_DEV_NAME,
+        .fa_off = FLASH_AREA_2_OFFSET,
+        .fa_size = FLASH_AREA_2_SIZE,
+    },
+    {
+        .fa_id = FLASH_AREA_3_ID,
+        .fa_device_id = FLASH_DEVICE_ID,
+        .fa_driver = &FLASH_DEV_NAME,
+        .fa_off = FLASH_AREA_3_OFFSET,
+        .fa_size = FLASH_AREA_3_SIZE,
+    },
+    {
+        .fa_id = FLASH_AREA_4_ID,
+        .fa_device_id = FLASH_DEVICE_ID,
+        .fa_driver = &FLASH_DEV_NAME,
+        .fa_off = FLASH_AREA_4_OFFSET,
+        .fa_size = FLASH_AREA_4_SIZE,
+    },
+    {
+        .fa_id = FLASH_AREA_5_ID,
+        .fa_device_id = FLASH_DEVICE_ID,
+        .fa_driver = &FLASH_DEV_NAME,
+        .fa_off = FLASH_AREA_5_OFFSET,
+        .fa_size = FLASH_AREA_5_SIZE,
+    },
+};
+
+const int flash_map_entry_num = ARRAY_SIZE(flash_map);
+
+int boot_get_image_exec_ram_info(uint32_t image_id,
+                                 uint32_t *exec_ram_start,
+                                 uint32_t *exec_ram_size)
+{
+    int rc = -1;
+
+    if (image_id == RSS_FIRMWARE_SECURE_ID) {
+        *exec_ram_start = S_IMAGE_LOAD_ADDRESS;
+        *exec_ram_size  = SECURE_IMAGE_MAX_SIZE;
+        rc = 0;
+    } else if (image_id == RSS_FIRMWARE_NON_SECURE_ID) {
+        *exec_ram_start = NS_IMAGE_LOAD_ADDRESS;
+        *exec_ram_size  = NON_SECURE_IMAGE_MAX_SIZE;
+        rc = 0;
+    }
+
+    return rc;
+}
