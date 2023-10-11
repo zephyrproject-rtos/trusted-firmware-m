34cf9b91b9aa37162c7c9eae874c204283506b0d
Dávid Házi <david.hazi@arm.com>
2023-10-11 11:10:41 +0200

    Build: Handle building split build without BL2
    Skip image signing if BL2 is OFF.
    
    Signed-off-by: Dávid Házi <david.hazi@arm.com>
    Co-authored-by: David Hu <david.hu@arm.com>
    Change-Id: I9d2b632c68bd7d93c806c46285213db3fd9886a1

--- cmake/spe-CMakeLists.cmake
+++ cmake/spe-CMakeLists.cmake
@@ -41,80 +41,81 @@ target_link_libraries(tfm_api_ns
         $<$<BOOL:${CONFIG_TFM_USE_TRUSTZONE}>:${CMAKE_CURRENT_SOURCE_DIR}/interface/lib/s_veneers.o>
 )
 
-if (MCUBOOT_IMAGE_NUMBER GREATER 1)
+if(BL2 AND PLATFORM_DEFAULT_IMAGE_SIGNING)
 
-    add_custom_target(tfm_app_binaries
-        DEPENDS tfm_ns_binaries
-        DEPENDS $<IF:$<BOOL:${MCUBOOT_GENERATE_SIGNING_KEYPAIR}>,generated_private_key,>
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts
+    if (MCUBOOT_IMAGE_NUMBER GREATER 1)
 
-        #Sign non-secure binary image with provided secret key
-        COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/wrapper/wrapper.py
-            --version ${MCUBOOT_IMAGE_VERSION_NS}
-            --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_ns.o
-            --key ${MCUBOOT_KEY_NS}
-            --public-key-format $<IF:$<BOOL:${MCUBOOT_HW_KEY}>,full,hash>
-            --align ${MCUBOOT_ALIGN_VAL}
-            --pad
-            --pad-header
-            -H ${BL2_HEADER_SIZE}
-            -s ${MCUBOOT_SECURITY_COUNTER_NS}
-            -L ${MCUBOOT_ENC_KEY_LEN}
-            -d \"\(0, ${MCUBOOT_S_IMAGE_MIN_VER}\)\"
-            ${CMAKE_BINARY_DIR}/bin/tfm_ns.bin
-            $<$<STREQUAL:${MCUBOOT_UPGRADE_STRATEGY},OVERWRITE_ONLY>:--overwrite-only>
-            $<$<BOOL:${MCUBOOT_CONFIRM_IMAGE}>:--confirm>
-            $<$<BOOL:${MCUBOOT_ENC_IMAGES}>:-E${MCUBOOT_KEY_ENC}>
-            $<$<BOOL:${MCUBOOT_MEASURED_BOOT}>:--measured-boot-record>
-            ${CMAKE_BINARY_DIR}/bin/tfm_ns_signed.bin
+        add_custom_target(tfm_app_binaries
+            DEPENDS tfm_ns_binaries
+            DEPENDS $<IF:$<BOOL:${MCUBOOT_GENERATE_SIGNING_KEYPAIR}>,generated_private_key,>
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts
 
-        # Create concatenated binary image from the two independently signed
-        # binary file. This only uses the local assemble.py script (not from
-        # upstream mcuboot) because that script is geared towards zephyr
-        # support
-        COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/assemble.py
-            --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_s.o
-            --secure ${CMAKE_CURRENT_SOURCE_DIR}/bin/tfm_s_signed.bin
-            --non_secure ${CMAKE_BINARY_DIR}/bin/tfm_ns_signed.bin
-            --output ${CMAKE_BINARY_DIR}/tfm_s_ns_signed.bin
-        # merge bootloader and application into Hex image for upload
-        COMMAND srec_cat ${CMAKE_CURRENT_SOURCE_DIR}/bin/bl2.bin -Binary -offset 0xA000000
-            ${CMAKE_BINARY_DIR}/tfm_s_ns_signed.bin -Binary -offset 0xA020000
-            -o ${CMAKE_BINARY_DIR}/tfm.hex -Intel
-    )
-else()
+            #Sign non-secure binary image with provided secret key
+            COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/wrapper/wrapper.py
+                --version ${MCUBOOT_IMAGE_VERSION_NS}
+                --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_ns.o
+                --key ${MCUBOOT_KEY_NS}
+                --public-key-format $<IF:$<BOOL:${MCUBOOT_HW_KEY}>,full,hash>
+                --align ${MCUBOOT_ALIGN_VAL}
+                --pad
+                --pad-header
+                -H ${BL2_HEADER_SIZE}
+                -s ${MCUBOOT_SECURITY_COUNTER_NS}
+                -L ${MCUBOOT_ENC_KEY_LEN}
+                -d \"\(0, ${MCUBOOT_S_IMAGE_MIN_VER}\)\"
+                ${CMAKE_BINARY_DIR}/bin/tfm_ns.bin
+                $<$<STREQUAL:${MCUBOOT_UPGRADE_STRATEGY},OVERWRITE_ONLY>:--overwrite-only>
+                $<$<BOOL:${MCUBOOT_CONFIRM_IMAGE}>:--confirm>
+                $<$<BOOL:${MCUBOOT_ENC_IMAGES}>:-E${MCUBOOT_KEY_ENC}>
+                $<$<BOOL:${MCUBOOT_MEASURED_BOOT}>:--measured-boot-record>
+                ${CMAKE_BINARY_DIR}/bin/tfm_ns_signed.bin
 
-    add_custom_target(tfm_app_binaries
-        DEPENDS tfm_ns_binaries
-        DEPENDS $<IF:$<BOOL:${MCUBOOT_GENERATE_SIGNING_KEYPAIR}>,generated_private_key,>
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts
+            # Create concatenated binary image from the two independently signed
+            # binary file. This only uses the local assemble.py script (not from
+            # upstream mcuboot) because that script is geared towards zephyr
+            # support
+            COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/assemble.py
+                --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_s.o
+                --secure ${CMAKE_CURRENT_SOURCE_DIR}/bin/tfm_s_signed.bin
+                --non_secure ${CMAKE_BINARY_DIR}/bin/tfm_ns_signed.bin
+                --output ${CMAKE_BINARY_DIR}/tfm_s_ns_signed.bin
+            # merge bootloader and application into Hex image for upload
+            COMMAND srec_cat ${CMAKE_CURRENT_SOURCE_DIR}/bin/bl2.bin -Binary -offset 0xA000000
+                ${CMAKE_BINARY_DIR}/tfm_s_ns_signed.bin -Binary -offset 0xA020000
+                -o ${CMAKE_BINARY_DIR}/tfm.hex -Intel
+        )
+    else()
+        add_custom_target(tfm_app_binaries
+            DEPENDS tfm_ns_binaries
+            DEPENDS $<IF:$<BOOL:${MCUBOOT_GENERATE_SIGNING_KEYPAIR}>,generated_private_key,>
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts
 
-        # concatenate S + NS binaries into tfm_s_ns.bin
-        COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/assemble.py
-            --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_s_ns.o
-            --secure ${CMAKE_CURRENT_SOURCE_DIR}/bin/tfm_s.bin
-            --non_secure ${CMAKE_BINARY_DIR}/bin/tfm_ns.bin
-            --output ${CMAKE_BINARY_DIR}/bin/tfm_s_ns.bin
+            # concatenate S + NS binaries into tfm_s_ns.bin
+            COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/assemble.py
+                --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_s_ns.o
+                --secure ${CMAKE_CURRENT_SOURCE_DIR}/bin/tfm_s.bin
+                --non_secure ${CMAKE_BINARY_DIR}/bin/tfm_ns.bin
+                --output ${CMAKE_BINARY_DIR}/bin/tfm_s_ns.bin
 
-        # sign the combined tfm_s_ns.bin file
-        COMMAND ${Python3_EXECUTABLE}
-            ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/wrapper/wrapper.py
-            --version ${MCUBOOT_IMAGE_VERSION_S}
-            --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_s_ns.o
-            --key ${MCUBOOT_KEY_S}
-            --public-key-format $<IF:$<BOOL:${MCUBOOT_HW_KEY}>,full,hash>
-            --align ${MCUBOOT_ALIGN_VAL}
-            --pad
-            --pad-header
-            -H ${BL2_HEADER_SIZE}
-            -s ${MCUBOOT_SECURITY_COUNTER_S}
-            -L ${MCUBOOT_ENC_KEY_LEN}
-            $<$<STREQUAL:${MCUBOOT_UPGRADE_STRATEGY},OVERWRITE_ONLY>:--overwrite-only>
-            $<$<BOOL:${MCUBOOT_CONFIRM_IMAGE}>:--confirm>
-            $<$<BOOL:${MCUBOOT_ENC_IMAGES}>:-E${MCUBOOT_KEY_ENC}>
-            $<$<BOOL:${MCUBOOT_MEASURED_BOOT}>:--measured-boot-record>
-            ${CMAKE_BINARY_DIR}/bin/tfm_s_ns.bin
-            ${CMAKE_BINARY_DIR}/tfm_s_ns_signed.bin
-    )
+            # sign the combined tfm_s_ns.bin file
+            COMMAND ${Python3_EXECUTABLE}
+                ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/scripts/wrapper/wrapper.py
+                --version ${MCUBOOT_IMAGE_VERSION_S}
+                --layout ${CMAKE_CURRENT_SOURCE_DIR}/image_signing/layout_files/signing_layout_s_ns.o
+                --key ${MCUBOOT_KEY_S}
+                --public-key-format $<IF:$<BOOL:${MCUBOOT_HW_KEY}>,full,hash>
+                --align ${MCUBOOT_ALIGN_VAL}
+                --pad
+                --pad-header
+                -H ${BL2_HEADER_SIZE}
+                -s ${MCUBOOT_SECURITY_COUNTER_S}
+                -L ${MCUBOOT_ENC_KEY_LEN}
+                $<$<STREQUAL:${MCUBOOT_UPGRADE_STRATEGY},OVERWRITE_ONLY>:--overwrite-only>
+                $<$<BOOL:${MCUBOOT_CONFIRM_IMAGE}>:--confirm>
+                $<$<BOOL:${MCUBOOT_ENC_IMAGES}>:-E${MCUBOOT_KEY_ENC}>
+                $<$<BOOL:${MCUBOOT_MEASURED_BOOT}>:--measured-boot-record>
+                ${CMAKE_BINARY_DIR}/bin/tfm_s_ns.bin
+                ${CMAKE_BINARY_DIR}/tfm_s_ns_signed.bin
+        )
+    endif()
 endif()
-
