a622e8573f93be9d2ef6516b72ac9804b6a891e9
Andrej Butok <andrey.butok@nxp.com>
2023-11-02 14:13:28 +0100

    Build: nxp: Port lpcxpresso55s69 on split build
    Change-Id: I192f4d5068060a47435d329ac0a2681b0fa840db
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- platform/ext/target/nxp/lpcxpresso55s69/cpuarch.cmake
+++ platform/ext/target/nxp/lpcxpresso55s69/cpuarch.cmake
@@ -1,11 +1,12 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2020, Arm Limited. All rights reserved.
+# Copyright 2023 NXP. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 
-# preload.cmake is used to set things that related to the platform that are both
+# cpuarch.cmake is used to set things that related to the platform that are both
 # immutable and global, which is to say they should apply to any kind of project
 # that uses this platform. In practise this is normally compiler definitions and
 # variables related to hardware.
@@ -15,7 +16,7 @@ set(TFM_SYSTEM_PROCESSOR cortex-m33)
 set(TFM_SYSTEM_ARCHITECTURE armv8-m.main)
 
 
-# Set processor type for NXP SDK
+# Set processor type for NXP MCUx SDK
 add_definitions(-DCPU_LPC55S69JBD100_cm33_core0)
 
 # Define serial port ID
