13256271da0f31eaf49c39465c4bd47283ba8792
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-11-24 15:52:24 +0100

    platform: nordic_nrf: Move identical configurations to common config
    Move identical configurations to common config file.
    
    Change-Id: Ieb981be9ab7b452be7be6e53c9623ff0b7c312f9
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/nrf5340/config.cmake
+++ platform/ext/target/nordic_nrf/common/nrf5340/config.cmake
@@ -7,8 +7,3 @@
 #-------------------------------------------------------------------------------
 
 include(${PLATFORM_PATH}/common/core/config.cmake)
-
-set(SECURE_UART1                        ON         CACHE BOOL      "Enable secure UART1")
-set(NRF_NS_STORAGE                      OFF        CACHE BOOL      "Enable non-secure storage partition")
-set(BL2                                 ON         CACHE BOOL      "Whether to build BL2")
-set(NRF_NS_SECONDARY                    ${BL2}     CACHE BOOL      "Enable non-secure secondary partition")
