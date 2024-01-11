09c4d0dc78f0d833ce8fe403d5523a0bac9a6f25
Raef Coles <raef.coles@arm.com>
2024-01-11 11:21:46 +0000

    RSS: fix check_config.cmake
    By adding subplatform check_configs
    
    Change-Id: I44e02309f6abe8c181b137d679f00c998723602f
    Signed-off-by: Raef Coles <raef.coles@arm.com>

---
+++ platform/ext/target/arm/rss/rdfremont/check_config.cmake
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2024, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+include(${CMAKE_CURRENT_LIST_DIR}/../common/check_config.cmake)
