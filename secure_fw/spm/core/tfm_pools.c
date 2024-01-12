841081e27c8a732a64fc8a65ab8df988d116edf6
Jamie Fox <jamie.fox@arm.com>
2024-01-12 19:03:21 +0000

    SPM: Add allocation state bookkeeping to pool allocator
    Changes the pool allocator to keep track of the allocation state
    of each chunk. Adds a check of allocation state to the
    is_valid_chunk_data_in_pool() function so that callers are
    checking that a pointer is a chunk in the pool and that it is
    currently allocated.
    
    Signed-off-by: Jamie Fox <jamie.fox@arm.com>
    Change-Id: I7c9585e7afcecca5edf90e132501b72f969b4d22

--- secure_fw/spm/core/tfm_pools.c
+++ secure_fw/spm/core/tfm_pools.c
@@ -17,6 +17,9 @@
 #include "lists.h"
 #include "tfm_pools.h"
 
+/* Magic value that indicates a pool chunk has been allocated */
+#define POOL_MAGIC_ALLOCATED UINT32_C(0xF0F0CCAA)
+
 psa_status_t tfm_pool_init(struct tfm_pool_instance_t *pool, size_t poolsz,
                            size_t chunksz, size_t num)
 {
@@ -67,6 +70,8 @@ void *tfm_pool_alloc(struct tfm_pool_instance_t *pool)
     node = UNI_LIST_NEXT_NODE(pool, next);
     UNI_LIST_REMOVE_NODE(pool, node, next);
 
+    node->magic = POOL_MAGIC_ALLOCATED;
+
     return &(((struct tfm_pool_chunk_t *)node)->data);
 }
 
@@ -75,6 +80,8 @@ void tfm_pool_free(struct tfm_pool_instance_t *pool, void *ptr)
     struct tfm_pool_chunk_t *pchunk;
 
     pchunk = TO_CONTAINER(ptr, struct tfm_pool_chunk_t, data);
+
+    pchunk->magic = 0;
 
     UNI_LIST_INSERT_AFTER(pool, pchunk, next);
 
@@ -89,6 +96,7 @@ bool is_valid_chunk_data_in_pool(struct tfm_pool_instance_t *pool,
 {
     const uintptr_t chunks_start = (uintptr_t)(pool->chunks);
     const size_t chunks_offset = (uintptr_t)data - chunks_start;
+    struct tfm_pool_chunk_t *pchunk;
 
     /* Check that the message was allocated from the pool. */
     if ((uintptr_t)data < chunks_start || chunks_offset >= pool->pool_sz) {
@@ -97,6 +105,12 @@ bool is_valid_chunk_data_in_pool(struct tfm_pool_instance_t *pool,
 
     if ((chunks_offset % (pool->chunksz + sizeof(struct tfm_pool_chunk_t)))
         != offsetof(struct tfm_pool_chunk_t, data)) {
+        return false;
+    }
+
+    pchunk = TO_CONTAINER(data, struct tfm_pool_chunk_t, data);
+
+    if (pchunk->magic != POOL_MAGIC_ALLOCATED) {
         return false;
     }
 
