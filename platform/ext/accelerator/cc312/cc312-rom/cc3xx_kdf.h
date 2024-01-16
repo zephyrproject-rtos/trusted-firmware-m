91a2ecb1ecbf36d1e7c49af029e5ccebf513c619
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-01-16 10:33:11 +0000

    CC3XX: Update copyright banner
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I341d223afecf20a90c0894489f2b9938f7e9f546

--- platform/ext/accelerator/cc312/cc312-rom/cc3xx_kdf.h
+++ platform/ext/accelerator/cc312/cc312-rom/cc3xx_kdf.h
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2023, Arm Limited. All rights reserved.
+ * Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
