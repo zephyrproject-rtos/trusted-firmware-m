zephyrproject-rtos/trusted-firmware-m

commit 754b7e9f9360ccd1fd0ec44a28cb871b03f05348

RSS: Add guards to optional MHU device uses
The MHUs for non-secure communication between RSS and AP
(MHU_RSS_TO_AP_NS_DEV and MHU_AP_TO_RSS_NS_DEV) are not always present
so add guards macros to ensure devices exists where they are used.

Signed-off-by: Joel Goddard <joel.goddard@arm.com>
Change-Id: Id920924f312dd20aced49885e5bf50980057eae5

platform/ext/target/arm/rss/common/tfm_interrupts.c
@@ -56,6 +56,7 @@ void CMU_MHU0_Receiver_Handler(void)
     spm_handle_interrupt(mbox_irq_info[0].p_pt, mbox_irq_info[0].p_ildi);
 }
 
+#ifdef MHU_AP_TO_RSS_NS
 /* Platform specific inter-processor communication interrupt handler. */
 void CMU_MHU1_Receiver_Handler(void)
 {
@@ -67,6 +68,7 @@ void CMU_MHU1_Receiver_Handler(void)
      */
     spm_handle_interrupt(mbox_irq_info[1].p_pt, mbox_irq_info[1].p_ildi);
 }
+#endif
 
 enum tfm_hal_status_t mailbox_irq_init(void *p_pt,
                                        const struct irq_load_info_t *p_ildi)

platform/ext/target/arm/rss/common/rss_comms/rss_comms_hal.c
@@ -217,10 +217,12 @@ int32_t tfm_hal_client_id_translate(void *owner, int32_t client_id_in)
         return ((client_id_in & CLIENT_ID_USER_INPUT_MASK) |
                (MHU0_CLIENT_ID_BASE & CLIENT_ID_MHU_BASE_MASK) |
                (NS_CLIENT_ID_FLAG_MASK));
+#ifdef MHU_RSS_TO_AP_NS
     } else if ((uintptr_t)owner == (uintptr_t)&MHU_RSS_TO_AP_NS_DEV) {
         return ((client_id_in & CLIENT_ID_USER_INPUT_MASK) |
                (MHU1_CLIENT_ID_BASE & CLIENT_ID_MHU_BASE_MASK) |
                (NS_CLIENT_ID_FLAG_MASK));
+#endif
     } else {
         SPMLOG_DBGMSG("[COMMS] client_id translation failed: invalid owner\r\n");
         return 0;

commit 67fff9f8e89e32194fd1aa845bdaeb1d73b9ae59

RSS: fix conditional compilation typo
Fix typo that was introduced by 754b7e9 [1]

[1]: https://git.trustedfirmware.org/TF-M/trusted-firmware-m.git/commit/?id=754b7e9f9360ccd1fd0ec44a28cb871b03f05348

Signed-off-by: David Vincze <david.vincze@arm.com>
Change-Id: I2acc367a57f9ecfd64b9dafca588b4254558ff92

platform/ext/target/arm/rss/common/tfm_interrupts.c
@@ -57,7 +57,7 @@ void CMU_MHU0_Receiver_Handler(void)
     spm_handle_interrupt(mbox_irq_info[0].p_pt, mbox_irq_info[0].p_ildi);
 }
 
-#ifdef MHU_AP_TO_RSS_NS
+#ifdef MHU_AP_NS_TO_RSS
 /* Platform specific inter-processor communication interrupt handler. */
 void CMU_MHU1_Receiver_Handler(void)
 {
@@ -71,7 +71,7 @@ void CMU_MHU1_Receiver_Handler(void)
      */
     spm_handle_interrupt(mbox_irq_info[1].p_pt, mbox_irq_info[1].p_ildi);
 }
-#endif
+#endif /* MHU_AP_NS_TO_RSS */
 
 enum tfm_hal_status_t mailbox_irq_init(void *p_pt,
                                        const struct irq_load_info_t *p_ildi)