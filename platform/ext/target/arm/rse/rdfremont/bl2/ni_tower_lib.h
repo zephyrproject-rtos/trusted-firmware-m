b36f0c771398a1eced60380de747f1f23459a6ca
Arnold Gabriel Benedict <arnoldgabriel.benedict@arm.com>
2024-02-25 14:08:12 +0000

    RSE: Fremont: Add system control NI-Tower interface ids
    System Control NI-Tower is the interconnect between the AXI interfaces
    of RSE, SCP/MCP and the CMN interconnect. Add interface IDs for every
    xSNI and xMNI interfaces connected to this system control NI-Tower. The
    interface ID is pre-configured by the platform hardware configurations.
    These IDs are required to fetch offset address for register bases of
    NI-Tower components and subfeatures. Also, the target IDs can be used
    while configuring PSAMs within the NI-Tower.
    
    Signed-off-by: Arnold Gabriel Benedict <arnoldgabriel.benedict@arm.com>
    Change-Id: I47a21a7e82b453a11c2785754b7ada81476d7eaa

---
+++ platform/ext/target/arm/rse/rdfremont/bl2/ni_tower_lib.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __NI_TOWER_LIB_H__
+#define __NI_TOWER_LIB_H__
+
+#include <stdint.h>
+
+/* Interface ID of xSNI components - completer interfaces */
+enum sysctrl_xSNI_ids {
+    /* Request from AP */
+    SYSCTRL_APP_ASNI_ID = 0x0,
+    /* Request from LCP */
+    SYSCTRL_LCP_ASNI_ID,
+    /* Request from MCP ATU */
+    SYSCTRL_MCP_ASNI_ID,
+    /* Request from RSE ATU */
+    SYSCTRL_RSE_MAIN_ASNI_ID,
+    /* Request from RSE and SCP targeting LCP address space */
+    SYSCTRL_RSE_SCP_ASNI_ID,
+    /* Request from SCP ATU */
+    SYSCTRL_SCP_ASNI_ID
+};
+
+/* Interface ID of xMNI components - requester interfaces */
+enum sysctrl_xMNI_ids {
+    /* Targets AP address space */
+    SYSCTRL_APP_AMNI_ID = 0x0,
+    /* Targets memory map shared between AP and MCP */
+    SYSCTRL_APP_MCP_AMNI_ID,
+    /* Targets memory map shared between AP and SCP */
+    SYSCTRL_APP_SCP_AMNI_ID,
+    /* Targets LCP address space */
+    SYSCTRL_LCP_AMNI_ID,
+    /* Targets memory map shared between LCP and SCP */
+    SYSCTRL_LCP_SCP_AMNI_ID,
+    /* Targets Shared SRAM between RSE, SCP and MCP */
+    SYSCTRL_RSM_AMNI_ID,
+    /* Targets MCP address space */
+    SYSCTRL_RSE_MCP_AMNI_ID,
+    /* Targets SCP address space */
+    SYSCTRL_RSE_SCP_AMNI_ID,
+    /* Targets CMN GPV registers */
+    SYSCTRL_CMN_PMNI_ID,
+    /* Targets Shared RAM between RSE/SCP/MCP */
+    SYSCTRL_RSM_PMNI_ID,
+    /* Targets SYSCTRL SMMU registers */
+    SYSCTRL_TCU_PMNI_ID,
+    /* Targets the System Control NI-Tower registers (default target) */
+    SYSCTRL_CONFIG_SPACE_ID = 0xF
+};
+
+#endif /* __NI_TOWER_LIB_H__ */
