8ca156e22f9bd940cf445b4a6b5f465f0348837c
Raef Coles <raef.coles@arm.com>
2024-03-20 16:22:20 +0000

    RSE: Add custom fatal error permissions
    To respect security state when logging.
    
    Change-Id: I6c4399b9210b5c56c7bd3793b71afe800cef1bbc
    Signed-off-by: Raef Coles <raef.coles@arm.com>

---
+++ platform/ext/target/arm/rse/common/platform_fatal_error.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#include "fatal_error.h"
+
+#include "device_definition.h"
+
+static bool prevent_recursion = false;
+
+bool log_error_permissions_check(uint32_t err, bool is_fatal)
+{
+    enum lcm_tp_mode_t tp_mode = LCM_TP_MODE_PCI;
+    enum lcm_lcs_t lcs = LCM_LCS_SE;
+    enum lcm_bool_t sp_enabled = LCM_TRUE;
+
+    /* Avoid issues where one of the following functions also throws a fatal
+     * error.
+     */
+    if (prevent_recursion) {
+        return false;
+    }
+
+    prevent_recursion = true;
+
+    if (lcm_get_tp_mode(&LCM_DEV_S, &tp_mode) != LCM_ERROR_NONE) {
+        goto out;
+    }
+    if (lcm_get_lcs(&LCM_DEV_S, &lcs) != LCM_ERROR_NONE) {
+        goto out;
+    }
+    if (lcm_get_sp_enabled(&LCM_DEV_S, &sp_enabled) != LCM_ERROR_NONE) {
+        goto out;
+    }
+
+    if (tp_mode == LCM_TP_MODE_TCI || (lcs != LCM_LCS_SE && sp_enabled != LCM_TRUE)) {
+        prevent_recursion = false;
+        return true;
+    }
+out:
+    prevent_recursion = false;
+
+    return false;
+}
