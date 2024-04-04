1436367730476a1e7b4adba2e4e940475bc54bad
Raef Coles <raef.coles@arm.com>
2024-04-04 16:32:46 +0100

    RSE: Remove pre-check on OTP writes
    As LCM OTP masking causes unwritten OTP to return random values, causing
    pre-check errors
    
    Change-Id: Ic8ca5a3c47c7bac052db62fbad53d51a5caf47a8
    Signed-off-by: Raef Coles <raef.coles@arm.com>

--- platform/ext/target/arm/rse/common/native_drivers/lcm_drv.c
+++ platform/ext/target/arm/rse/common/native_drivers/lcm_drv.c
@@ -844,14 +844,6 @@ enum lcm_error_t lcm_otp_write(struct lcm_dev_t *dev, uint32_t offset, uint32_t 
         return LCM_ERROR_INVALID_OFFSET;
     }
 
-    /* Reject the write if we'd have to unset a bit. */
-    for (idx = 0; idx < len / sizeof(uint32_t); idx++) {
-        if (p_buf_word[idx] !=
-           (p_lcm->raw_otp[(offset / sizeof(uint32_t)) + idx] | p_buf_word[idx])) {
-            return LCM_ERROR_INVALID_WRITE;
-        }
-    }
-
     /* Write the zero count if needed */
     err = write_zero_count_if_needed(dev, offset, len, buf);
     if (err != LCM_ERROR_NONE) {
