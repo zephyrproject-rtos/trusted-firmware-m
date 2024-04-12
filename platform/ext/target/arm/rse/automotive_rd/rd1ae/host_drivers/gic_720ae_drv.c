de94d3708786ef3d35a1a08c6a059608056e3f1f
Ziad Elhanafy <ziad.elhanafy@arm.com>
2024-04-12 16:45:38 +0100

    rse: rd1ae: Add GIC-720AE driver
    Add a GIC-72OAE multi view driver to program the related registers.
    
    Signed-off-by: Ziad Elhanafy <ziad.elhanafy@arm.com>
    Change-Id: Id383ef807333859e0b725fab8e8206311f2eaa5c

---
+++ platform/ext/target/arm/rse/automotive_rd/rd1ae/host_drivers/gic_720ae_drv.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+#include "gic_720ae_drv.h"
+#include "bootutil/bootutil_log.h"
+#include "gic_720ae_reg.h"
+
+#include <stdint.h>
+
+/* Definitions to access GICD and GICR registers. */
+#define GICD_READ_REG(dev, ofs) \
+    *(volatile uint32_t *)((dev)->gicd_base + (ofs))
+
+#define GICD_WRITE_REG(dev, ofs, val) \
+    (*(volatile uint32_t *)((dev)->gicd_base + (ofs))) = (uint32_t)(val)
+
+#define GICR_READ_REG(dev, cpu, ofs) \
+    *(volatile uint32_t *)((dev)->gicr_base + (dev)->rdist_sz * (cpu) + (ofs))
+
+#define GICR_WRITE_REG(dev, cpu, ofs, val) \
+    (*(volatile uint32_t *) \
+    ((dev)->gicr_base + (dev)->rdist_sz * (cpu) + (ofs))) = (uint32_t)(val)
+
+/* Definitions to calculate SPI register index and bit offset. */
+#define SPI_TO_IVIEWRn_IDX(spi)     (((spi) - 32) >> 4)
+#define SPI_TO_IVIEWRnE_IDX(spi)    (((spi) - 4096) >> 4)
+#define SPI_TO_IVIEWRn_BIT(spi)     (((spi) % 16) << 1)
+#define SPI_IS_VALID(spi)           ((spi) >= 32 && (spi) < 1020)
+#define SPI_IS_VALID_EXT(spi)       ((spi) >= 4096 && (spi) < 5120)
+
+static int assign_pe_to_view(struct gic_mv_dev_t *dev, uint32_t mpid,
+                             uint8_t view)
+{
+    uint32_t val;
+    uint32_t rdist_id;
+
+    if (view >= dev->view_num) {
+        BOOT_LOG_ERR("GIC: Error assigning PE to invaild View:%d", view);
+        return -1;
+    }
+
+    for (rdist_id = 0; rdist_id < dev->rdist_num; rdist_id++) {
+        val = GICR_READ_REG(dev, rdist_id, GICR_TYPER + 4);
+        if (val == mpid)
+            break;
+    }
+
+    if (rdist_id >= dev->rdist_num) {
+        BOOT_LOG_ERR("GIC: Error could not find re-dist for PE:%x", mpid);
+        return -1;
+    }
+
+    /* Update MPID corresponding GICR_VIEWR to view-id */
+    GICR_WRITE_REG(dev, rdist_id, GICR_VIEWR, view);
+
+    return 0;
+}
+
+static int assign_spi_to_view(struct gic_mv_dev_t *dev, uint16_t spi,
+                              uint8_t view)
+{
+    uint32_t val, offset;
+    uint32_t nreg, nbit;
+
+    if (view >= dev->view_num) {
+        BOOT_LOG_ERR("GIC: Error assigning SPI to invaild View:%d", view);
+        return -1;
+    }
+
+    if (SPI_IS_VALID(spi)) {
+        nreg = SPI_TO_IVIEWRn_IDX(spi);
+        offset = GICD_IVIEWnR + 4 * nreg;
+    } else if (SPI_IS_VALID_EXT(spi)) {
+        nreg = SPI_TO_IVIEWRnE_IDX(spi);
+        offset = GICD_IVIEWnRE + 4 * nreg;
+    } else {
+        BOOT_LOG_ERR("GIC: Error assigning invalid SPI:5d to View:%d", spi,
+                     view);
+        return -1;
+    }
+    nbit = SPI_TO_IVIEWRn_BIT(spi);
+
+    /* Update SPI corresponding GICD_VIEWR to view-id */
+    val = GICD_READ_REG(dev, offset);
+    val &= (~(GICR_VIEWR_MASK << nbit));
+    val |= (view << nbit);
+    GICD_WRITE_REG(dev, offset, val);
+
+    return 0;
+}
+
+int gic_multiple_view_config_pe(struct gic_mv_dev_t *dev,
+                                struct gic_mv_p2v_map *pe_map, uint32_t num)
+{
+    int ret;
+    uint32_t idx;
+
+    for (idx = 0; idx < num; idx++) {
+        ret = assign_pe_to_view(dev, pe_map[idx].mpid, pe_map[idx].view);
+        if (ret)
+            return ret;
+    }
+
+    return 0;
+}
+
+int gic_multiple_view_config_spi(struct gic_mv_dev_t *dev,
+                                 struct gic_mv_i2v_map *spi_map, uint32_t num)
+{
+    int ret;
+    uint32_t idx, val;
+
+    for (idx = 0; idx < num; idx++) {
+        ret = assign_spi_to_view(dev, spi_map[idx].spi, spi_map[idx].view);
+        if (ret)
+            return ret;
+    }
+
+    /*
+     * The final GICD_CTLR result of each view is the LOGICAL AND of
+     * the GICD_CTLR from view-0 and the GICD_CTLR from each view.
+     * Therefore in the multiview initialization, set the EnableGrp0 and
+     * EnableGrp1 of view-0's GICD_CTLR from firmware so that the software
+     * in each view can control the EnableGrp from their side.
+     * Note that in Safety Island, there is only a single security state.
+     */
+    val = GICD_READ_REG(dev, GICD_CTLR);
+    val |= 0x03;
+    GICD_WRITE_REG(dev, GICD_CTLR, val);
+
+    return 0;
+}
+
+int gic_multiple_view_device_probe(struct gic_mv_dev_t *dev,
+                                   uintptr_t base_addr, uint16_t view_num,
+                                   uint16_t rdist_num)
+{
+    uint32_t val;
+
+    dev->view_num  = view_num;
+    dev->rdist_num = rdist_num;
+    dev->gicd_base = base_addr;
+    dev->gicr_base = base_addr + SI_GICR_OFFSET;
+
+    /* Check the GIC version to decide GICR frame size. */
+    val = GICD_READ_REG(dev, GICD_PIDR2) & GICD_PIDR2_ARCH_MASK;
+    val >>= 4;
+    if (val == GICD_PIDR2_ARCH_GICv3)
+        dev->rdist_sz = SI_GICR_FRAME_SZ_v3;
+    else if (val == GICD_PIDR2_ARCH_GICv4)
+        dev->rdist_sz = SI_GICR_FRAME_SZ_v4;
+    else {
+        BOOT_LOG_ERR("GIC: Un-supported GIC version:%d", val);
+        return -1;
+    }
+
+    /* Read GICD_CFGID bit-53 to detect multiple view feature. */
+    val = GICD_READ_REG(dev, GICD_CFGID + 4);
+    if (!(val & GICD_CFGID_HI_MV)) {
+        BOOT_LOG_ERR("GIC: Could not detect Multiple Views Support!");
+        return -1;
+    }
+
+    BOOT_LOG_INF("GIC: Multiple Views Support detected!");
+
+    return 0;
+}
