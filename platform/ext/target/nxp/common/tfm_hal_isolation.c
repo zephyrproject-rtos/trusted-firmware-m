zephyrproject-rtos/trusted-firmware-m

commit 143401e82b385fbe8eac4bd87d8a4238f76dd210

SPM: Manage SPM boundary during psa api execution
When triggering SVC handler to execute psa api execution,
this means that it has entered SPM domain, so it's necessary
to judge whether it needs to switch boundary between SPM and
current partition.
In the same way, the same judgement is needed when psa api
execution ends before returning to previous partition.

Signed-off-by: Chendi Sun <chendi.sun@arm.com>
Change-Id: I28e9fae3b2105022695ae7df14d063c6940e0537

platform/ext/target/nxp/common/tfm_hal_isolation.c
@@ -33,8 +33,15 @@ extern const struct memory_region_limits memory_regions;
 
 /* It can be retrieved from the MPU_TYPE register. */
 #define MPU_REGION_NUM                  8
+
+#if TFM_ISOLATION_LEVEL == 3
 #define PROT_BOUNDARY_VAL \
-    ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK) | \
+     ((1U << HANDLE_ATTR_SPM_POS) & HANDLE_ATTR_SPM_MASK))
+#else
+#define PROT_BOUNDARY_VAL \
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+#endif
 
 #ifdef CONFIG_TFM_ENABLE_MEMORY_PROTECT
 static uint32_t n_configured_regions = 0;
@@ -429,6 +436,7 @@ FIH_RET_TYPE(enum tfm_hal_status_t) tfm_hal_activate_boundary(
     uint32_t local_handle = (uint32_t)boundary;
     bool privileged = !!(local_handle & HANDLE_ATTR_PRIV_MASK);
 #if TFM_ISOLATION_LEVEL == 3
+    bool is_spm = !!(local_handle & HANDLE_ATTR_SPM_MASK);
     fih_int fih_rc = FIH_FAILURE;
     struct mpu_armv8m_region_cfg_t localcfg;
     uint32_t i, mmio_index;
@@ -442,6 +450,10 @@ FIH_RET_TYPE(enum tfm_hal_status_t) tfm_hal_activate_boundary(
     __set_CONTROL(ctrl.w);
 
 #if TFM_ISOLATION_LEVEL == 3
+    if (is_spm) {
+        FIH_RET(fih_int_encode(TFM_HAL_SUCCESS));
+    }
+
     if (!p_ldinf) {
         FIH_RET(fih_int_encode(TFM_HAL_ERROR_GENERIC));
     }

platform/ext/target/arm/mps2/an521/tfm_hal_isolation.c
@@ -28,8 +28,15 @@
 
 /* It can be retrieved from the MPU_TYPE register. */
 #define MPU_REGION_NUM                  8
+
+#if TFM_ISOLATION_LEVEL == 3
+#define PROT_BOUNDARY_VAL \
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK) | \
+     ((1U << HANDLE_ATTR_SPM_POS) & HANDLE_ATTR_SPM_MASK))
+#else
 #define PROT_BOUNDARY_VAL \
     ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+#endif
 
 #ifdef CONFIG_TFM_ENABLE_MEMORY_PROTECT
 static uint32_t n_configured_regions = 0;
@@ -517,6 +524,7 @@ FIH_RET_TYPE(enum tfm_hal_status_t) tfm_hal_activate_boundary(
     uint32_t local_handle = (uint32_t)boundary;
     bool privileged = !!(local_handle & HANDLE_ATTR_PRIV_MASK);
 #if TFM_ISOLATION_LEVEL == 3
+    bool is_spm = !!(local_handle & HANDLE_ATTR_SPM_MASK);
     struct mpu_armv8m_region_cfg_t localcfg;
     uint32_t i;
 #if CONFIG_TFM_MMIO_REGION_ENABLE == 1
@@ -533,6 +541,10 @@ FIH_RET_TYPE(enum tfm_hal_status_t) tfm_hal_activate_boundary(
     __set_CONTROL(ctrl.w);
 
 #if TFM_ISOLATION_LEVEL == 3
+    if (is_spm) {
+        FIH_RET(fih_int_encode(TFM_HAL_SUCCESS));
+    }
+
     if (!p_ldinf) {
         FIH_RET(fih_int_encode(TFM_HAL_ERROR_GENERIC));
     }

platform/ext/target/arm/musca_b1/tfm_hal_isolation.c
@@ -27,8 +27,15 @@
 
 /* It can be retrieved from the MPU_TYPE register. */
 #define MPU_REGION_NUM                  8
+
+#if TFM_ISOLATION_LEVEL == 3
+#define PROT_BOUNDARY_VAL \
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK) | \
+     ((1U << HANDLE_ATTR_SPM_POS) & HANDLE_ATTR_SPM_MASK))
+#else
 #define PROT_BOUNDARY_VAL \
     ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+#endif
 
 #ifdef CONFIG_TFM_ENABLE_MEMORY_PROTECT
 static uint32_t n_configured_regions = 0;
@@ -383,6 +390,7 @@ enum tfm_hal_status_t tfm_hal_activate_boundary(
     uint32_t local_handle = (uint32_t)boundary;
     bool privileged = !!(local_handle & HANDLE_ATTR_PRIV_MASK);
 #if TFM_ISOLATION_LEVEL == 3
+    bool is_spm = !!(local_handle & HANDLE_ATTR_SPM_MASK);
     struct mpu_armv8m_region_cfg_t localcfg;
     uint32_t i, mmio_index;
     struct platform_data_t *plat_data_ptr;
@@ -395,6 +403,10 @@ enum tfm_hal_status_t tfm_hal_activate_boundary(
     __set_CONTROL(ctrl.w);
 
 #if TFM_ISOLATION_LEVEL == 3
+    if (is_spm) {
+        FIH_RET(fih_int_encode(TFM_HAL_SUCCESS));
+    }
+
     if (!p_ldinf) {
         return TFM_HAL_ERROR_GENERIC;
     }

platform/ext/target/nuvoton/common/tfm_hal_isolation.c
@@ -28,8 +28,15 @@
 
 /* It can be retrieved from the MPU_TYPE register. */
 #define MPU_REGION_NUM                  8
+
+#if TFM_ISOLATION_LEVEL == 3
 #define PROT_BOUNDARY_VAL \
-    ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK) | \
+     ((1U << HANDLE_ATTR_SPM_POS) & HANDLE_ATTR_SPM_MASK))
+#else
+#define PROT_BOUNDARY_VAL \
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+#endif
 
 #ifdef CONFIG_TFM_ENABLE_MEMORY_PROTECT
 static uint32_t n_configured_regions = 0;
@@ -370,6 +377,7 @@ enum tfm_hal_status_t tfm_hal_activate_boundary(
     uint32_t local_handle = (uint32_t)boundary;
     bool privileged = !!(local_handle & HANDLE_ATTR_PRIV_MASK);
 #if TFM_ISOLATION_LEVEL == 3
+    bool is_spm = !!(local_handle & HANDLE_ATTR_SPM_MASK);
     struct mpu_armv8m_region_cfg_t localcfg;
     uint32_t i, mmio_index;
     struct platform_data_t *plat_data_ptr;
@@ -382,6 +390,10 @@ enum tfm_hal_status_t tfm_hal_activate_boundary(
     __set_CONTROL(ctrl.w);
 
 #if TFM_ISOLATION_LEVEL == 3
+    if (is_spm) {
+        FIH_RET(fih_int_encode(TFM_HAL_SUCCESS));
+    }
+
     if (!p_ldinf) {
         return TFM_HAL_ERROR_GENERIC;
     }

platform/ext/target/stm/common/stm32l5xx/secure/tfm_hal_isolation.c
@@ -30,8 +30,15 @@
 
 /* It can be retrieved from the MPU_TYPE register. */
 #define MPU_REGION_NUM                  8
+
+#if TFM_ISOLATION_LEVEL == 3
+#define PROT_BOUNDARY_VAL \
+    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK) | \
+     ((1U << HANDLE_ATTR_SPM_POS) & HANDLE_ATTR_SPM_MASK))
+#else
 #define PROT_BOUNDARY_VAL \
     ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+#endif
 
 #ifdef CONFIG_TFM_ENABLE_MEMORY_PROTECT
 static uint32_t n_configured_regions = 0;
@@ -361,6 +368,7 @@ enum tfm_hal_status_t tfm_hal_activate_boundary(
     uint32_t local_handle = (uint32_t)boundary;
     bool privileged = !!(local_handle & HANDLE_ATTR_PRIV_MASK);
 #if TFM_ISOLATION_LEVEL == 3
+    bool is_spm = !!(local_handle & HANDLE_ATTR_SPM_MASK);
     struct mpu_armv8m_region_cfg_t localcfg;
     uint32_t i, mmio_index;
     struct platform_data_t *plat_data_ptr;
@@ -373,6 +381,10 @@ enum tfm_hal_status_t tfm_hal_activate_boundary(
     __set_CONTROL(ctrl.w);
 
 #if TFM_ISOLATION_LEVEL == 3
+    if (is_spm) {
+        FIH_RET(fih_int_encode(TFM_HAL_SUCCESS));
+    }
+
     if (!p_ldinf) {
         return TFM_HAL_ERROR_GENERIC;
     }

platform/ext/target/arm/mps2/an521/mmio_defs.h
@@ -21,6 +21,8 @@ extern "C" {
 #define HANDLE_ATTR_NS_POS              0U
 #define HANDLE_ATTR_NS_MASK             (0x1UL << HANDLE_ATTR_NS_POS)
 #if TFM_ISOLATION_LEVEL == 3
+#define HANDLE_ATTR_SPM_POS             2U
+#define HANDLE_ATTR_SPM_MASK            (0x1UL << HANDLE_ATTR_SPM_POS)
 #define HANDLE_PER_ATTR_BITS            (0x4)
 #define HANDLE_ATTR_RW_POS              (1 << (HANDLE_PER_ATTR_BITS - 1))
 #define HANDLE_ATTR_INDEX_MASK          (HANDLE_ATTR_RW_POS - 1)

platform/ext/target/arm/musca_b1/mmio_defs.h
@@ -21,6 +21,8 @@ extern "C" {
 #define HANDLE_ATTR_NS_POS              0U
 #define HANDLE_ATTR_NS_MASK             (0x1UL << HANDLE_ATTR_NS_POS)
 #if TFM_ISOLATION_LEVEL == 3
+#define HANDLE_ATTR_SPM_POS             2U
+#define HANDLE_ATTR_SPM_MASK            (0x1UL << HANDLE_ATTR_SPM_POS)
 #define HANDLE_PER_ATTR_BITS            (0x4)
 #define HANDLE_ATTR_RW_POS              (1 << (HANDLE_PER_ATTR_BITS - 1))
 #define HANDLE_ATTR_INDEX_MASK          (HANDLE_ATTR_RW_POS - 1)

platform/ext/target/nuvoton/common/mmio_defs.h
@@ -21,6 +21,8 @@ extern "C" {
 #define HANDLE_ATTR_NS_POS              0U
 #define HANDLE_ATTR_NS_MASK             (0x1UL << HANDLE_ATTR_NS_POS)
 #if TFM_ISOLATION_LEVEL == 3
+#define HANDLE_ATTR_SPM_POS             2U
+#define HANDLE_ATTR_SPM_MASK            (0x1UL << HANDLE_ATTR_SPM_POS)
 #define HANDLE_PER_ATTR_BITS            (0x4)
 #define HANDLE_ATTR_RW_POS              (1 << (HANDLE_PER_ATTR_BITS - 1))
 #define HANDLE_ATTR_INDEX_MASK          (HANDLE_ATTR_RW_POS - 1)

commit 46973656bbc608667544d96b74b23dc85a32d7c9

Platform: nxp: Fix compilation error in tfm_hal_isolation.c
Fix compilation error in tfm_hal_isolation.c.
It was broken by 143401e82b385fbe8eac4bd87d8a4238f76dd210

Change-Id: I29469a1fa7ee9b40c62225caec27265ae92e98e4
Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

platform/ext/target/nxp/common/tfm_hal_isolation.c
@@ -40,7 +40,7 @@ extern const struct memory_region_limits memory_regions;
      ((1U << HANDLE_ATTR_SPM_POS) & HANDLE_ATTR_SPM_MASK))
 #else
 #define PROT_BOUNDARY_VAL \
-    (((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
+    ((1U << HANDLE_ATTR_PRIV_POS) & HANDLE_ATTR_PRIV_MASK)
 #endif
 
 #ifdef CONFIG_TFM_ENABLE_MEMORY_PROTECT