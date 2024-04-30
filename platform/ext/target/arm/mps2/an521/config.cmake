a90d3b8d8ade755dcd51248da8be607ae222ce64
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-04-30 19:11:17 +0200

    Revert "Build: Use built-in EC-P256 keys for BL2 signatures"
    This reverts commit 9a087b30d4d43bfa2e1ab26085ff1485e1952cb3.
    
    Reason for revert: Merged by mistake
    
    Change-Id: I1e4c4c0f67902e2cbad6703f1a246c67809e102b

--- platform/ext/target/arm/mps2/an521/config.cmake
+++ platform/ext/target/arm/mps2/an521/config.cmake
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2020-2024, Arm Limited. All rights reserved.
+# Copyright (c) 2020-2023, Arm Limited. All rights reserved.
 # Copyright (c) 2022 Cypress Semiconductor Corporation (an Infineon company)
 # or an affiliate of Cypress Semiconductor Corporation. All rights reserved.
 #
@@ -22,8 +22,3 @@ set(CONFIG_TFM_USE_TRUSTZONE          ON)
 set(TFM_MULTI_CORE_TOPOLOGY           OFF)
 
 set(PLATFORM_HAS_ISOLATION_L3_SUPPORT ON)
-
-set(MCUBOOT_USE_PSA_CRYPTO            ON               CACHE BOOL      "Enable the cryptographic abstraction layer to use PSA Crypto APIs")
-set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
-set(MCUBOOT_HW_KEY                    OFF              CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
-set(MCUBOOT_BUILTIN_KEY               ON               CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
