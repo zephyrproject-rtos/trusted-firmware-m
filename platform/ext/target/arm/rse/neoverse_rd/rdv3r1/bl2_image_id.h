b94641e81e2f02506a222611d2b41748387d89e7
Joel Goddard <Joel.Goddard@arm.com>
2024-06-13 11:39:18 +0100

    RSE: RD-V3-R1: Add BL2 firmware load config
    Add functions and definitions for loading RD-V3-R1 firmware images at
    BL2. This can load SCP, MCP, all LCP, AP-BL1, NSEC and SEC firmware.
    
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Signed-off-by: Arnold Gabriel Benedict <arnoldgabriel.benedict@arm.com>
    Change-Id: I568dbc754f1fb0aa6d6d78f7915455d7c87b4eef

---
+++ platform/ext/target/arm/rse/neoverse_rd/rdv3r1/bl2_image_id.h
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __BL2_IMAGE_ID_H__
+#define __BL2_IMAGE_ID_H__
+
+/*
+ * ID of firmware images loaded by BL2
+ * Images are loaded by BL2 in reverse order of ID
+ * Cannot use enum as this is included in linker files.
+ */
+/* RSE firmware */
+#define RSE_FIRMWARE_SECURE_ID       0
+#define RSE_FIRMWARE_NON_SECURE_ID   1
+/* Host Firmware */
+#define RSE_FIRMWARE_AP_BL1_ID       2
+#define RSE_FIRMWARE_LCP_ID          3
+#define RSE_FIRMWARE_MCP_ID          4
+#define RSE_FIRMWARE_SCP_ID          5
+/* Number of firmware loaded by BL2 */
+#define RSE_FIRMWARE_COUNT           6
+
+#endif /* __BL2_IMAGE_ID_H__ */
--- platform/ext/target/arm/rse/neoverse_rd/rdv3r1/size_defs.h
+++ platform/ext/target/arm/rse/neoverse_rd/rdv3r1/size_defs.h
@@ -5,5 +5,16 @@
  */
 
 /* BL2 */
-#define SIZE_DEF_BL2_IMAGE          (0x20000) /* 128 KB */
+#define SIZE_DEF_BL2_IMAGE          (0x20000)  /* 128 KB */
 
+/* Secure Firmware */
+#define SIZE_DEF_S_IMAGE            (0x60000)  /* 384 KB */
+
+/* Non-Secure Firmware */
+#define SIZE_DEF_NS_IMAGE           (0x60000)  /* 384 KB */
+
+/* Host Firmware */
+#define SIZE_DEF_SCP_IMAGE          (0x100000) /* 1 MB */
+#define SIZE_DEF_MCP_IMAGE          (0x80000)  /* 512 KB */
+#define SIZE_DEF_LCP_IMAGE          (0x10000)  /* 64 KB */
+#define SIZE_DEF_AP_BL1_IMAGE       (0x100000) /* 1 MB */
