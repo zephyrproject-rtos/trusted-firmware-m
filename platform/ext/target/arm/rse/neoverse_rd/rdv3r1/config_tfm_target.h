ac70af14f4fdd16e5fb282f52044f7655e7a7374
Joel Goddard <Joel.Goddard@arm.com>
2024-06-13 11:42:49 +0100

    RSE: RD-V3-R1: Add runtime config
    Add functions and definitions for configuring RD-V3-R1 runtime firmware.
    This includes RSE comms permission for limiting partition access from
    AP and configuration for partitions.
    
    Signed-off-by: Joel Goddard <joel.goddard@arm.com>
    Change-Id: I9e5948bb915aaf2cb8da91d5b33d012f0110d737

--- platform/ext/target/arm/rse/neoverse_rd/rdv3r1/config_tfm_target.h
+++ platform/ext/target/arm/rse/neoverse_rd/rdv3r1/config_tfm_target.h
@@ -7,13 +7,41 @@
 #ifndef __CONFIG_TFM_TARGET_H__
 #define __CONFIG_TFM_TARGET_H__
 
+/* Use stored NV seed to provide entropy */
+#define CRYPTO_NV_SEED                         0
+
+/* Use external RNG to provide entropy */
+#define CRYPTO_EXT_RNG                         1
+
+/* Set the initial attestation token profile */
+#undef ATTEST_TOKEN_PROFILE_PSA_IOT_1
+#undef ATTEST_TOKEN_PROFILE_PSA_2_0_0
+
+#define ATTEST_TOKEN_PROFILE_ARM_CCA           1
+
+/* The stack size of the NS Agent Mailbox Secure Partition */
+#define NS_AGENT_MAILBOX_STACK_SIZE            0x1000
+
 /* Run the scheduler after handling a secure interrupt if the NSPE was pre-empted */
 #define CONFIG_TFM_SCHEDULE_WHEN_NS_INTERRUPTED 1
+
+/*
+ * tfm_hal_post_partition_init_hook is called if this option is enabled.
+ * It's called by SPM right before starting scheduler.
+ */
+#define CONFIG_TFM_POST_PARTITION_INIT_HOOK     1
 
 /* Default RSE SYSCLK/CPU0CLK value in Hz */
 #define SYSCLK         100000000UL /* 100 MHz */
 
 /* Clock configuration value to write to CLK_CFG1.SYSCLKCFG to drive SYSCLKCFG signal */
 #define SYSCLKCFG_VAL  0
+
+/* Maximum RSE Comms payload size using Embed protocol */
+#define RSE_COMMS_PAYLOAD_MAX_SIZE (0x40 + 0x1000)
+
+/* SCMI Comms SCP doorbell IRQ number and handler */
+#define SCP_DOORBELL_IRQ         CMU_MHU4_Receiver_IRQn
+#define SCP_DOORBELL_IRQ_HANDLER CMU_MHU4_Receiver_Handler
 
 #endif /* __CONFIG_TFM_TARGET_H__ */
