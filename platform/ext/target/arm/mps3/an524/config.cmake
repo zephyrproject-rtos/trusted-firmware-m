aa7e9614880c5492af4baa68d24a9f1bb4580388
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-07-18 15:08:38 +0200

    Platform: AN524/Corstone300/Corstone310 use P256M in BL2
    Also use MCUBOOT_BUILTIN_KEY instead of MCUBOOT_HW_KEY. Enable
    all MPS3 based platforms with this single patch.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I29b6070eec8da040123d2a62d77e0cd81b14482a

--- platform/ext/target/arm/mps3/an524/config.cmake
+++ platform/ext/target/arm/mps3/an524/config.cmake
@@ -23,3 +23,8 @@ set(PROVISIONING_KEYS_CONFIG            ""        CACHE FILEPATH  "The config fi
 set(PROVISIONING_CODE_PADDED_SIZE       "0x2000"  CACHE STRING    "")
 set(PROVISIONING_VALUES_PADDED_SIZE     "0x400"   CACHE STRING    "")
 set(PROVISIONING_DATA_PADDED_SIZE       "0x400"   CACHE STRING    "")
+
+set(MCUBOOT_USE_PSA_CRYPTO            ON               CACHE BOOL      "Enable the cryptographic abstraction layer to use PSA Crypto APIs")
+set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
+set(MCUBOOT_HW_KEY                    OFF              CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
+set(MCUBOOT_BUILTIN_KEY               ON               CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
