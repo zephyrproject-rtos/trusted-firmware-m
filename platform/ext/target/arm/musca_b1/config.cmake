6c5ff2a617efb445a9e2e3d6f992085daed1313e
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-07-18 13:57:01 +0200

    Platform: Musca-B1: Enable BL2 with P256 signatures
    Use MCUBOOT_BUILTIN_KEY instead of MCUBOOT_HW_KEY as well
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: Ia4291447cbed1099b8c8d3e6108497b0cfc18ecf

--- platform/ext/target/arm/musca_b1/config.cmake
+++ platform/ext/target/arm/musca_b1/config.cmake
@@ -30,3 +30,8 @@ set(PLATFORM_HAS_ISOLATION_L3_SUPPORT   ON)
 
 set(PLATFORM_HAS_FIRMWARE_UPDATE_SUPPORT ON)
 set(MCUBOOT_DATA_SHARING                ON)
+
+set(MCUBOOT_USE_PSA_CRYPTO            ON               CACHE BOOL      "Enable the cryptographic abstraction layer to use PSA Crypto APIs")
+set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
+set(MCUBOOT_HW_KEY                    OFF              CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
+set(MCUBOOT_BUILTIN_KEY               ON               CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
