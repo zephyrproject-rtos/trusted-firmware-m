4a5b1649e6acf9633d4cea68d79f3b2a3088378f
Dávid Házi <david.hazi@arm.com>
2024-09-12 13:19:50 +0200

    CS315: Fix tfm profile support
    HKDF is used by the early bootloader stages,
    enabled these crypto parts in every profile.
    
    Signed-off-by: Dávid Házi <david.hazi@arm.com>
    Change-Id: I45ab53b3b7e2e310e064b825c1df27a246c97367

--- platform/ext/target/arm/mps4/corstone315/config.cmake
+++ platform/ext/target/arm/mps4/corstone315/config.cmake
@@ -60,3 +60,5 @@ set(MCUBOOT_USE_PSA_CRYPTO            ON               CACHE BOOL      "Enable t
 set(MCUBOOT_SIGNATURE_TYPE            "EC-P256"        CACHE STRING    "Algorithm to use for signature validation [RSA-2048, RSA-3072, EC-P256, EC-P384]")
 set(MCUBOOT_HW_KEY                    OFF              CACHE BOOL      "Whether to embed the entire public key in the image metadata instead of the hash only")
 set(MCUBOOT_BUILTIN_KEY               ON               CACHE BOOL      "Use builtin key(s) for validation, no public key data is embedded into the image metadata")
+
+set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH ${CMAKE_CURRENT_LIST_DIR}/mbedtls_extra_config.h CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to configure feature support")
