490f8f862e4d0004df9dd64de2059a00fd05f28b
Hao Zhang <Hao.Zhang@analog.com>
2024-10-15 13:05:40 -0400

    [zep fromtree] platform: ext: adi Add NS support
    This commit added NS support to able to execute
    zephyr regression tests.
    
    MAX32657 supports small profile for now.
    CONFIG_TFM_PROFILE_TYPE_SMALL=y
    
    -- -------- NS TEST Configuration --------------------
    -- TEST_NS_ATTESTATION                       OFF
    -- TEST_NS_CRYPTO                            ON
    -- TEST_NS_ITS                               ON
    -- TEST_NS_PS                                OFF
    -- TEST_NS_QCBOR                             OFF
    -- TEST_NS_T_COSE                            OFF
    -- TEST_NS_PLATFORM                          OFF
    -- TEST_NS_FWU                               OFF
    -- TEST_NS_IPC                               OFF
    -- TEST_NS_FLIH_IRQ                          OFF
    -- TEST_NS_MULTI_CORE                        OFF
    -- TEST_NS_MANAGE_NSID                       OFF
    -- TEST_NS_SFN_BACKEND                       ON
    -- TEST_NS_FPU                               OFF
    -- ---------------------------------------------------
    
    Added UART here to get test output.
    
    To build it, tf-m-tests shall be added in west file:
    west config manifest.project-filter -- +tf-m-tests
    west update
    
    Change-Id: Ie60b62e83c59cec00f1ad197b3249ee7c7d89205
    Signed-off-by: Hao Zhang <Hao.Zhang@analog.com>
    (cherry picked from commit 4ae67f6f36aa7e26b30abd158956449d9db38377)

---
+++ platform/ext/target/adi/max32657/cmsis.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2024-2025 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __MAX32657_CMSIS_H__
+#define __MAX32657_CMSIS_H__
+
+#include "max32657.h"
+
+#endif /* __MAX32657_CMSIS_H__ */
