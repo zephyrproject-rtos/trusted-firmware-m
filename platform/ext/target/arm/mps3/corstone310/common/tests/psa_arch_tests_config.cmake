5106fd01621ec2a3a7394f5ae35785092978a2c0
Gergely Korcsák <gergely.korcsak@arm.com>
2024-11-07 15:32:37 +0100

    mps3: Change psa arch tests target from cs3x0 to mps3
    Signed-off-by: Gergely Korcsák <gergely.korcsak@arm.com>
    Change-Id: I5d1820484e7d6a80064c5e6a0446bc781b0c9c56

--- platform/ext/target/arm/mps3/corstone310/common/tests/psa_arch_tests_config.cmake
+++ platform/ext/target/arm/mps3/corstone310/common/tests/psa_arch_tests_config.cmake
@@ -1,8 +1,8 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2023, Arm Limited. All rights reserved.
+# Copyright (c) 2023-2024, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 
-set(PSA_API_TEST_TARGET          "cs3x0"    CACHE STRING  "PSA_API_TARGET name")
+set(PSA_API_TEST_TARGET          "mps3"    CACHE STRING  "PSA_API_TARGET name")
