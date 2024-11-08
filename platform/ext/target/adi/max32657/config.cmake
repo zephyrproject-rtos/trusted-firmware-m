b49f662fa6959fde76adc4a9c618cab0de203955
Jayashree Srinivasan <Jayashree.Srinivasan@analog.com>
2024-11-08 16:18:15 -0500

    platform: ext: adi: max32657: Add platform and fetch hal_adi
    Add support for Analog Devices MAX32657 platform and fetch
    ADI HAL library.
    
    Co-authored-by: Hao Zhang <Hao.Zhang@analog.com>
    Co-authored-by: Sadik Ozer <sadik.ozer@analog.com>
    Change-Id: If884aa9a35664f6117574b0d4cde363a19e4eca5
    Signed-off-by: Jayashree Srinivasan <Jayashree.Srinivasan@analog.com>

---
+++ platform/ext/target/adi/max32657/config.cmake
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+# Portions Copyright (C) 2024 Analog Devices, Inc.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+set(HAL_ADI_PATH                        "DOWNLOAD"  CACHE PATH      "Path to hal_adi (or DOWNLOAD to fetch automatically")
+set(HAL_ADI_VERSION                     "dd1c525"   CACHE STRING    "The version of hal_adi to use")
