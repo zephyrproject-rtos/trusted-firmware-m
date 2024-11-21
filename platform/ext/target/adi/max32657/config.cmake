9e4d4383531439cf4af3f9390ac6e60f62591310
Sadik Ozer <sadik.ozer@analog.com>
2024-11-21 18:38:16 +0300

    platform: ext: adi: Enable TF-M for MAX32657
    Update CMakeFile for tf-m integration
    Enable tf-m flags in config file
    
    Co-authored-by: Jayashree Srinivasan <Jayashree.Srinivasan@analog.com>
    Co-authored-by: Hao Zhang <Hao.Zhang@analog.com>
    Co-authored-by: Tanmaya Mishra <Tanmaya.Mishra@analog.com>
    Change-Id: I67484cdd9c4b8d3c94873a2d1fc8e69ef7eb1d08
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- platform/ext/target/adi/max32657/config.cmake
+++ platform/ext/target/adi/max32657/config.cmake
@@ -1,11 +1,19 @@
 #-------------------------------------------------------------------------------
-# Portions Copyright (C) 2024 Analog Devices, Inc.
+# Portions Copyright (C) 2024-2025 Analog Devices, Inc.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
 
 set(BL2                                 ON         CACHE BOOL     "Whether to build BL2")
+set(CONFIG_TFM_USE_TRUSTZONE            ON)
+set(TFM_PARTITION_PLATFORM              OFF          CACHE BOOL      "Enable Platform partition")
+set(TFM_PARTITION_CRYPTO                ON          CACHE BOOL      "Enable Crypto partition")
+set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON       CACHE BOOL      "Enable Internal Trusted Storage partition")
+set(TFM_PARTITION_NS_AGENT_TZ           ON         CACHE BOOL      "Enable Non-Secure Agent in Secure partition")
+set(CONFIG_TFM_BOOT_STORE_MEASUREMENTS          OFF  CACHE BOOL      "Store measurement values from all the boot stages. Used for initial attestation token.")
+set(CONFIG_TFM_BOOT_STORE_ENCODED_MEASUREMENTS  OFF  CACHE BOOL      "Enable storing of encoded measurements in boot.")
+set(CONFIG_TFM_HALT_ON_CORE_PANIC               ON   CACHE BOOL      "On fatal errors in the secure firmware, halt instead of rebooting.")
 
 set(HAL_ADI_PATH                        "DOWNLOAD"  CACHE PATH      "Path to hal_adi (or DOWNLOAD to fetch automatically")
 set(HAL_ADI_VERSION                     "dd1c525"   CACHE STRING    "The version of hal_adi to use")
