28c807b4f4d7924dbb9e3925f7f1ca6d5bf04b48
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-11-25 14:01:32 +0000

    CC3XX: Avoid shuffle on copies if DPA mitigations are disabled
    Resort to a simple word-aligned copy to fulfill driver requirements
    when interacting with key management units on some hardware
    platforms, but do not randomly shuffle bytes when copying.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: Ic0531a7c28b74e6f9743bfd02afa31a5c1b19ec6

--- platform/ext/target/arm/drivers/cc3xx/common/cc3xx_stdlib.c
+++ platform/ext/target/arm/drivers/cc3xx/common/cc3xx_stdlib.c
@@ -30,10 +30,17 @@ void cc3xx_secure_erase_buffer(uint32_t *buf, size_t word_count)
     }
 }
 
+/* The CC3XX driver uses word-aligned copies through this function when required.
+ * If DPA mitigations are enabled, the copy shuffles words randomly on copy,
+ * otherwise the copy happens linearly on 4-byte aligned boundaries. Note that a
+ * platform might require accesses to be 4-byte aligned due to hardware constraints,
+ * i.e. when using a KMU to load keys to be used with CryptoCell
+ */
 #ifndef CC3XX_CONFIG_STDLIB_EXTERNAL_DPA_HARDENED_WORD_COPY
 void cc3xx_dpa_hardened_word_copy(volatile uint32_t *dst,
                                   volatile const uint32_t *src, size_t word_count)
 {
+#if defined(CC3XX_CONFIG_DPA_MITIGATIONS_ENABLE)
     uint8_t permutation_buf[word_count]; /* This is a VLA */
     size_t idx;
 
@@ -46,11 +53,17 @@ void cc3xx_dpa_hardened_word_copy(volatile uint32_t *dst,
     for(idx = 0; idx < word_count; idx++) {
         dst[permutation_buf[idx]] = src[permutation_buf[idx]];
     }
+#else
+    for(size_t idx = 0; idx < word_count; idx++) {
+        dst[idx] = src[idx];
+    }
+#endif /* CC3XX_CONFIG_DPA_MITIGATIONS_ENABLE */
 }
 #else
 void cc3xx_dpa_hardened_word_copy(volatile uint32_t *dst,
                                   volatile const uint32_t *src, size_t word_count)
 {
+    /* This is the externally provided implementation for hardened word copy */
     dpa_hardened_word_copy(dst, src, word_count);
 }
 #endif /* CC3XX_CONFIG_STDLIB_EXTERNAL_DPA_HARDENED_WORD_COPY */
