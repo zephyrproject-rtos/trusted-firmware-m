4b76e3bad7f587c869c6f0d7cb3036bfb7134e77
Sadik Ozer <sadik.ozer@analog.com>
2024-12-09 09:20:54 +0300

    [zep fromtree] platform: ext: adi: Add MAX32657 custom OTP file
    - Disable PLATFORM_DEFAULT_OTP to implement MAX32657 OTP
    - Add read, write, get size functions
    - Before writing and after reading OTP cell bits are reverted
    due to default values not match with tfm expectation.
    On default tfm expect otp cell be 0x00 and bit can be transceived from 0
    to 1 but MAX32657 OTP default value is 0xff and bits can be converted
    from 1 to 0. So that before write and after read bits are reverted.
    - Set  bl2_rotpk_X size as 100 (max value) to get fix otp layout
    
    Change-Id: I325f2934a78633d6add6592dc9fdf1c3dcd852ba
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>
    (cherry picked from commit 258b8fd3cde0830b9848c38302e8bd3311eb2a50)

--- platform/ext/target/adi/max32657/config.cmake
+++ platform/ext/target/adi/max32657/config.cmake
@@ -14,6 +14,7 @@ set(TFM_PARTITION_NS_AGENT_TZ           ON         CACHE BOOL      "Enable Non-S
 set(CONFIG_TFM_BOOT_STORE_MEASUREMENTS          OFF  CACHE BOOL      "Store measurement values from all the boot stages. Used for initial attestation token.")
 set(CONFIG_TFM_BOOT_STORE_ENCODED_MEASUREMENTS  OFF  CACHE BOOL      "Enable storing of encoded measurements in boot.")
 set(CONFIG_TFM_HALT_ON_CORE_PANIC               ON   CACHE BOOL      "On fatal errors in the secure firmware, halt instead of rebooting.")
+set(PLATFORM_DEFAULT_OTP                OFF          CACHE BOOL      "Use trusted on-chip flash to implement OTP memory")
 
 set(HAL_ADI_PATH                        "DOWNLOAD"  CACHE PATH      "Path to hal_adi (or DOWNLOAD to fetch automatically")
 set(HAL_ADI_VERSION                     "dd1c525"   CACHE STRING    "The version of hal_adi to use")
