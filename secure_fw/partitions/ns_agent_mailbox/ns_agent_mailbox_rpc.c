877c796d7f01ec4efb4174f2fb58ccc494bf1d8d
David Hu <david.hu2@arm.com>
2024-12-30 23:35:07 +0000

    Dualcpu: Move RPC callback implementation into NS Agent Mailbox SP
    TF-M mailbox RPC layer hides the details of platform specific mailbox
    handling from TF-M general mailbox handling routine.
    When TF-M mailbox and RPC layer were introduced, the whole mailbox
    routine was completed in SPM context. Therefore, the entire RPC layer
    was naturally implemented in tfm_rpc.c under secure_fw/spm folder.
    
    After several major updates, non-secure requests are now handled in
    Non-secure Agent Mailbox Secure Partition.
    RPC callbacks are called in NS Agent Mailbox SP, instead of in SPM
    context. RPC callback implementation is actually coupled with NS Agent
    Mailbox SP behaviour.
    
    Based on descriptions above, move RPC callback implementation and
    register to ns_agent_mailbox_rpc.c under NS Agent Mailbox SP, while
    RPC Handlers of PSA client API remain in tfm_rpc.c.
     - It is more straightforward to implement RPC callback in the scope of
       NS Agent Mailbox SP. It becomes more flexible and easier to implement
       RPC callbacks based on NS Agent Mailbox SP configurations.
     - Refine build dependencies among SPM, NS Agent Mailbox SP and
       platform_s.
    
    Change-Id: Ib82c1be7fd8f734c2a8fce415eaae3afcd323f0f
    Signed-off-by: David Hu <david.hu2@arm.com>

---
+++ secure_fw/partitions/ns_agent_mailbox/ns_agent_mailbox_rpc.c
@@ -0,0 +1,79 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright The TrustedFirmware-M Contributors
+ */
+
+#include <stdbool.h>
+
+#include "async.h"
+#include "config_impl.h"
+#include "spm.h"
+#include "tfm_rpc.h"
+
+static void default_handle_req(void)
+{
+    psa_panic();
+}
+
+static void default_mailbox_reply(const void *owner, int32_t ret)
+{
+    (void)owner;
+    (void)ret;
+
+    psa_panic();
+}
+
+static struct tfm_rpc_ops_t rpc_ops = {
+    .handle_req = default_handle_req,
+    .reply      = default_mailbox_reply,
+};
+
+int32_t tfm_rpc_register_ops(const struct tfm_rpc_ops_t *ops_ptr)
+{
+    if (ops_ptr == NULL) {
+        return TFM_RPC_INVAL_PARAM;
+    }
+
+    if ((ops_ptr->handle_req == NULL) || (ops_ptr->reply == NULL)) {
+        return TFM_RPC_INVAL_PARAM;
+    }
+
+    /* Currently, one and only one mailbox implementation is supported. */
+    if ((rpc_ops.handle_req != default_handle_req) ||
+        (rpc_ops.reply != default_mailbox_reply)) {
+        return TFM_RPC_CONFLICT_CALLBACK;
+    }
+
+    rpc_ops.handle_req = ops_ptr->handle_req;
+    rpc_ops.reply = ops_ptr->reply;
+
+    return TFM_RPC_SUCCESS;
+}
+
+void tfm_rpc_unregister_ops(void)
+{
+    rpc_ops.handle_req = default_handle_req;
+    rpc_ops.reply = default_mailbox_reply;
+}
+
+void tfm_rpc_client_call_handler(void)
+{
+    rpc_ops.handle_req();
+}
+
+#if CONFIG_TFM_SPM_BACKEND_IPC == 1
+void tfm_rpc_client_call_reply(void)
+{
+    psa_msg_t msg;
+    psa_status_t status = psa_get(ASYNC_MSG_REPLY, &msg);
+    struct connection_t *handle = (struct connection_t *)msg.rhandle;
+
+    rpc_ops.reply(handle->client_data, status);
+
+    if (handle->status == TFM_HANDLE_STATUS_TO_FREE) {
+        spm_free_connection(handle);
+    } else {
+        handle->status = TFM_HANDLE_STATUS_IDLE;
+    }
+}
+#endif /* CONFIG_TFM_SPM_BACKEND_IPC == 1 */
--- secure_fw/spm/core/tfm_rpc.c
+++ secure_fw/spm/core/tfm_rpc.c
@@ -7,31 +7,12 @@
  *
  */
 
-#include "async.h"
 #include "config_impl.h"
-#include "spm.h"
 #include "ffm/mailbox_agent_api.h"
 #include "ffm/psa_api.h"
 #include "tfm_rpc.h"
-#include "utilities.h"
 #include "private/assert.h"
-#include "load/partition_defs.h"
 #include "tfm_psa_call_pack.h"
-
-static void default_handle_req(void)
-{
-}
-
-static void default_mailbox_reply(const void *owner, int32_t ret)
-{
-    (void)owner;
-    (void)ret;
-}
-
-static struct tfm_rpc_ops_t rpc_ops = {
-    .handle_req = default_handle_req,
-    .reply      = default_mailbox_reply,
-};
 
 uint32_t tfm_rpc_psa_framework_version(void)
 {
@@ -72,53 +53,3 @@ psa_status_t tfm_rpc_psa_close(psa_handle_t handle, int32_t ns_client_id)
 }
 
 #endif /* CONFIG_TFM_CONNECTION_BASED_SERVICE_API */
-
-int32_t tfm_rpc_register_ops(const struct tfm_rpc_ops_t *ops_ptr)
-{
-    if (!ops_ptr) {
-        return TFM_RPC_INVAL_PARAM;
-    }
-
-    if (!ops_ptr->handle_req || !ops_ptr->reply) {
-        return TFM_RPC_INVAL_PARAM;
-    }
-
-    /* Currently, one and only one mailbox implementation is supported. */
-    if ((rpc_ops.handle_req != default_handle_req) ||
-        (rpc_ops.reply != default_mailbox_reply)) {
-        return TFM_RPC_CONFLICT_CALLBACK;
-    }
-
-    rpc_ops.handle_req = ops_ptr->handle_req;
-    rpc_ops.reply = ops_ptr->reply;
-
-    return TFM_RPC_SUCCESS;
-}
-
-void tfm_rpc_unregister_ops(void)
-{
-    rpc_ops.handle_req = default_handle_req;
-    rpc_ops.reply = default_mailbox_reply;
-}
-
-void tfm_rpc_client_call_handler(void)
-{
-    rpc_ops.handle_req();
-}
-
-#if CONFIG_TFM_SPM_BACKEND_IPC == 1
-void tfm_rpc_client_call_reply(void)
-{
-    psa_msg_t msg;
-    psa_status_t status = psa_get(ASYNC_MSG_REPLY, &msg);
-    struct connection_t *handle = (struct connection_t *)msg.rhandle;
-
-    rpc_ops.reply(handle->client_data, status);
-
-    if (handle->status == TFM_HANDLE_STATUS_TO_FREE) {
-        spm_free_connection(handle);
-    } else {
-        handle->status = TFM_HANDLE_STATUS_IDLE;
-    }
-}
-#endif /* CONFIG_TFM_SPM_BACKEND_IPC == 1 */
