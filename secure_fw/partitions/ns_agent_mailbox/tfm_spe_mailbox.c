f93e5d6a6147bc563bf7c820d458966754e72728
Nicola Mazzucato <nicola.mazzucato@arm.com>
2025-01-02 09:06:36 +0000

    tfm_spe_mailbox: Add `mailbox_process_new_msg` handler
    Implement `process_new_msg` callback into the mailbox-specific
    operations.
    
    Also, return the number of pending slots that have been processed by
    `tfm_mailbox_handle_msg`. Such value is ignored from the handle_req
    callback but returned by `process_new_msg` to provide an indicator of
    the number of slots processed.
    
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
    Change-Id: Ia5bd87952820016bad8714df4abae440f11ec24f

--- secure_fw/partitions/ns_agent_mailbox/tfm_spe_mailbox.c
+++ secure_fw/partitions/ns_agent_mailbox/tfm_spe_mailbox.c
@@ -360,6 +360,8 @@ int32_t tfm_mailbox_handle_msg(void)
     struct mailbox_status_t *ns_status = spe_mailbox_queue.ns_status;
     struct mailbox_msg_t *msg_ptr;
     uint32_t critical_section;
+    int32_t status;
+    int32_t msg_dispatched = 0;
 
     SPM_ASSERT(ns_status != NULL);
 
@@ -403,7 +405,10 @@ int32_t tfm_mailbox_handle_msg(void)
         get_spe_mailbox_msg_handle(idx,
                                    &spe_mailbox_queue.queue[idx].msg_handle);
 
-        if (tfm_mailbox_dispatch(msg_ptr, idx, &reply_slots) != MAILBOX_SUCCESS) {
+        status = tfm_mailbox_dispatch(msg_ptr, idx, &reply_slots);
+        if (status == MAILBOX_SUCCESS) {
+            msg_dispatched++;
+        } else {
             mailbox_clean_queue_slot(idx);
             continue;
         }
@@ -423,7 +428,7 @@ int32_t tfm_mailbox_handle_msg(void)
         tfm_mailbox_hal_notify_peer();
     }
 
-    return MAILBOX_SUCCESS;
+    return (int32_t)msg_dispatched;
 }
 
 int32_t tfm_mailbox_reply_msg(mailbox_msg_handle_t handle, int32_t reply)
@@ -487,10 +492,25 @@ static void mailbox_reply(const void *owner, int32_t ret)
     (void)tfm_mailbox_reply_msg(handle, ret);
 }
 
+/* Process new mailbox message callback */
+static int32_t mailbox_process_new_msg(uint32_t *nr_msg)
+{
+    int32_t status;
+
+    status = tfm_mailbox_handle_msg();
+    if (status < 0) {
+        return status;
+    } else {
+        *nr_msg = (uint32_t)status;
+        return MAILBOX_SUCCESS;
+    }
+}
+
 /* Mailbox specific operations callback for TF-M RPC */
 static const struct tfm_rpc_ops_t mailbox_rpc_ops = {
     .handle_req = mailbox_handle_req,
     .reply      = mailbox_reply,
+    .process_new_msg = mailbox_process_new_msg,
 };
 
 static int32_t tfm_mailbox_init(void)
