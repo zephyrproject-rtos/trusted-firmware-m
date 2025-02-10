654330e79332df87995c9b12bb0bd65602ae152b
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-02-10 10:59:03 +0100

    platform: nordic_nrf: Add nRF54L15 initial support
    Before this there was only support for building the nRF54L15.
    This updates the nRF54L15 files to add an initial support for
    running TF-M.
    
    This is NOT full upstream support. There are important limitations:
    - Hardware crypto acceleration with Cracen is not supported
    - Random number generation with Cracen is not supported
    - The tests suites PSA arch tests and TF-M regression tests
      are not tested
    - BL2 is not supported
    - Some soc related configurations are not supported and they rely
      on hard-coded values (check nordicsemi_nrf54l_init for more info)
    
    This was tested using some basic Zephyr samples.
    
    This change updates the flash_layout.h and the region_maps.h headers
    with a layout that makes sense for the nRF54L15. Only one layout
    is supported at the moment.
    
    The RTE_Device.h is modified to use the UART20 and UART30 ports
    in order to support UART serial output.
    
    The function nordicsemi_nrf54l_init was copied from the soc.c in
    Zephyr and was simplified to support one single configuration.
    Later it can be considered to add configurability if needed.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Change-Id: I0369987cc1f4e7994078202e9d1bcb53c6372281
    (cherry picked from commit 01b4551f0b191c3238e6371bbbe7e7aeb8b64636)

--- platform/ext/target/nordic_nrf/common/nrf54l15/config.cmake
+++ platform/ext/target/nordic_nrf/common/nrf54l15/config.cmake
@@ -8,7 +8,7 @@
 
 include(${PLATFORM_PATH}/common/core/config.cmake)
 
-set(SECURE_UART30                       ON         CACHE BOOL      "Enable secure UART")
-set(NRF_NS_STORAGE                      OFF        CACHE BOOL      "Enable non-secure storage partition")
-set(BL2                                 OFF        CACHE BOOL      "Whether to build BL2")
-set(NRF_NS_SECONDARY                    OFF        CACHE BOOL      "Enable non-secure secondary partition")
+set(SECURE_UART30                       ON         CACHE BOOL      "Enable secure UART" FORCE)
+set(BL2                                 OFF        CACHE BOOL      "Whether to build BL2" FORCE)
+set(NRF_NS_SECONDARY                    OFF        CACHE BOOL      "Enable non-secure secondary partition" FORCE)
+set(NRF_SECURE_UART_INSTANCE            30         CACHE STRING    "The UART instance number to use for secure UART" FORCE)
--- platform/ext/target/nordic_nrf/nrf54l15dk_nrf54l15_cpuapp/services/src/tfm_platform_system.c
+++ platform/ext/target/nordic_nrf/nrf54l15dk_nrf54l15_cpuapp/services/src/tfm_platform_system.c
@@ -16,14 +16,6 @@ void tfm_platform_hal_system_reset(void)
 	NVIC_SystemReset();
 }
 
-
-/* These two functions need to be ported to the upstream project when support
- * for nrf54l15 and Cracen is added upstream. The implementations currently are
- * placed on in sdk-nrf. Remove these dummy implementations when these are ported.
- */
-void CRACEN_IRQHandler(void){};
-int nordicsemi_nrf54l_init(void){};
-
 enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
                                                psa_invec  *in_vec,
                                                psa_outvec *out_vec)
