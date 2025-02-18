f990f307366d1f93d538172c941b51177f12acd3
Antonio de Angelis <Antonio.deAngelis@arm.com>
2025-02-18 16:05:36 +0000

    BL1: Reorganize and cleanup the BL1 rom crypto module
    Reorganize functions for better reading of the source
    and add doxygen comments for clarifying
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I75ca14b2ff8f930a3e6d7f3e92cf4c2625f825a1

--- bl1/bl1_1/shared_lib/interface/crypto.h
+++ bl1/bl1_1/shared_lib/interface/crypto.h
@@ -21,61 +21,152 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Algorithm for which the given key can be used
+ *
+ * @enum tfm_bl1_key_type_t
+ */
 enum tfm_bl1_key_type_t {
     TFM_BL1_KEY_TYPE_ECDSA = 0b00,
     TFM_BL1_KEY_TYPE_LMS   = 0b01,
 };
 
+/**
+ * @brief Policy associated to the given key
+ *
+ * @enum tfm_bl1_key_policy_t
+ */
 enum tfm_bl1_key_policy_t {
     TFM_BL1_KEY_MIGHT_SIGN = 0b00,
     TFM_BL1_KEY_MUST_SIGN  = 0b01,
 };
 
+/**
+ * @brief Describes the EC curves that can be used for
+ *        signature verification in BL1
+ *
+ * @enum tfm_bl1_ecdsa_curve_t
+ *
+ */
 enum tfm_bl1_ecdsa_curve_t {
     TFM_BL1_CURVE_P256,
     TFM_BL1_CURVE_P384,
 };
 
+/**
+ * @brief Describes the allowed hashing algorithms in BL1
+ *
+ * @enum tfm_bl1_hash_alg_t
+ *
+ */
 enum tfm_bl1_hash_alg_t {
     TFM_BL1_HASH_ALG_SHA256 = PSA_ALG_SHA_256,
     TFM_BL1_HASH_ALG_SHA384 = PSA_ALG_SHA_384,
 };
 
-
-/* Calculates a hash of the input data */
+/**
+ * @brief Computes a hash of data as a single integrated operation
+ *
+ * @param[in]  alg         Algorithm of type \ref tfm_bl1_hash_alg_t
+ * @param[in]  data        Buffer containing the input bytes
+ * @param[in]  data_length Size in bytes of the input \p data buffer
+ * @param[out] hash        Buffer containing the produced output
+ * @param[in]  hash_length Size in bytes of the \p hash buffer
+ * @param[out] hash_size   Size in bytes of the produced hash
+ *
+ * @return fih_int 0 on success, non-zero on error
+ */
 fih_int bl1_hash_compute(enum tfm_bl1_hash_alg_t alg,
                          const uint8_t *data,
                          size_t data_length,
                          uint8_t *hash,
                          size_t hash_length,
                          size_t *hash_size);
-
+/**
+ * @brief Initialises a multipart hashing operation
+ *
+ * @param[in] alg  Algorithm of type \ref tfm_bl1_hash_alg_t
+ *
+ * @return fih_int 0 on success, non-zero on error
+ */
 fih_int bl1_hash_init(enum tfm_bl1_hash_alg_t alg);
 
+/**
+ * @brief Updates an ongoing multipart hashing operation with some input data
+ *
+ * @param[in] data        Buffer containing the input bytes
+ * @param[in] data_length Size in bytes of the input \p data buffer
+ *
+ * @return fih_int 0 on success, non-zero on error
+ */
 fih_int bl1_hash_update(const uint8_t *data,
                         size_t data_length);
-
+/**
+ * @brief Finalises an ongoing multipart hashing operation
+ *
+ * @param[out] hash        Buffer containing the produced output
+ * @param[in]  hash_length Size in bytes of the \p hash buffer
+ * @param[out] hash_size   Size in bytes of the produced hash
+ *
+ * @return fih_int 0 on success, non-zero on error
+ */
 fih_int bl1_hash_finish(uint8_t *hash,
                         size_t hash_length,
                         size_t *hash_size);
-
-
-/* Performs AES-256-CTR decryption */
+/**
+ * @brief Performs AES-256 decryption in Counter mode
+ *
+ * @note  In Counter (CTR) mode encryption and decryption
+ *        are implemented in the same way
+ *
+ * @param[in]     key_id             Key ID of type \ref tfm_bl1_key_id_t
+ * @param[in]     key_material       If TFM_BL1_KEY_USER, this contains key material
+ * @param[in,out] counter            Value of the counter to be used for CTR mode
+ * @param[in]     ciphertext         Buffer containing the bytes to decrypt
+ * @param[in]     ciphertext_length  Size in bytes of the \p ciphertext buffer
+ * @param[out]    plaintext          Buffer containing the decrypted bytes
+ *
+ * @return int32_t 0 on success, non-zero on error
+ */
 int32_t bl1_aes_256_ctr_decrypt(enum tfm_bl1_key_id_t key_id,
                                 const uint8_t *key_material,
                                 uint8_t *counter,
                                 const uint8_t *ciphertext,
                                 size_t ciphertext_length,
                                 uint8_t *plaintext);
-
-/* Derives key material from a BL1 key and some label and context. Any
- * cryptographically secure key derivation algorithm is acceptable.
+/**
+ * @brief Derives a key using a cryptographic method which depends on the underlying
+ *        platform capabilities. For example, it can be KDF in counter mode as per
+ *        NIST SP800-108 with AES-CMAC used as PRF
+ *
+ * @param[in]  input_key      Input secret key from which to derive material
+ * @param[in]  label          Label to associated to the derivation. NULL terminated string
+ * @param[in]  label_length   Size in bytes of the string comprising of the terminator
+ * @param[in]  context        Context bytes to associate to the derivation
+ * @param[in]  context_length Size in bytes of the \p context parameter
+ * @param[out] output_key     Derived key material
+ * @param[in]  output_length  Size in bytes of the key to be derived
+ *
+ * @return int32_t 0 on success, non-zero on error
  */
 int32_t bl1_derive_key(enum tfm_bl1_key_id_t input_key, const uint8_t *label,
                        size_t label_length, const uint8_t *context,
                        size_t context_length, uint8_t *output_key,
                        size_t output_length);
-
+/**
+ * @brief Verifies that the provided signature is a valid signature of the input
+ *        hash using ECDSA and the selected curve
+ *
+ * @param[in] curve          Desired curve over which to verify, of type \ref tfm_bl1_ecdsa_curve_t
+ * @param[in] key            Public key in raw uncompressed format, might contain the 0x04 byte at start
+ * @param[in] key_size       Size in bytes of the \p key buffer
+ * @param[in] hash           Hash of which to verify the signature
+ * @param[in] hash_length    Size in bytes of \p hash
+ * @param[in] signature      Signature to verify in raw format (r,s)
+ * @param[in] signature_size Size in bytes of the \p signature to verify
+ *
+ * @return fih_int 0 on success, non-zero on error
+ */
 fih_int bl1_ecdsa_verify(enum tfm_bl1_ecdsa_curve_t curve,
                          uint8_t *key, size_t key_size,
                          const uint8_t *hash,
--- platform/ext/target/arm/rse/common/bl1/cc3xx/cc3xx_rom_crypto.c
+++ platform/ext/target/arm/rse/common/bl1/cc3xx/cc3xx_rom_crypto.c
@@ -28,49 +28,18 @@
 static enum tfm_bl1_hash_alg_t multipart_alg = 0;
 static mbedtls_sha512_context multipart_ctx;
 
-fih_int sha256_init()
-{
-    fih_int fih_rc;
-
-    multipart_alg = TFM_BL1_HASH_ALG_SHA256;
-
-    /* This is only used by TFM_BL1_2 which currently only uses SHA256 */
-    fih_rc = fih_int_encode_zero_equality(cc3xx_lowlevel_hash_init(CC3XX_HASH_ALG_SHA256));
-    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
-        FIH_RET(fih_rc);
-    }
-
-    FIH_RET(FIH_SUCCESS);
-}
-
-fih_int sha256_finish(uint8_t *hash,
-                        size_t hash_length,
-                        size_t *hash_size)
-{
-    cc3xx_lowlevel_hash_finish((uint32_t *)hash, hash_length);
-
-    if (hash_size != NULL) {
-        *hash_size = 32;
-    }
-
-    multipart_alg = 0;
-
-    FIH_RET(FIH_SUCCESS);
-}
-
-fih_int sha256_update(const uint8_t *data,
-                        size_t data_length)
-{
-    fih_int fih_rc;
-
-    fih_rc = fih_int_encode_zero_equality(cc3xx_lowlevel_hash_update(data, data_length));
-    if(fih_not_eq(fih_rc, FIH_SUCCESS)) {
-        FIH_RET(fih_rc);
-    }
-
-    return FIH_SUCCESS;
-}
-
+/**
+ * @brief Computes a hash of the provided data using SHA-256. It calls into
+ *        the cc3xx driver directly
+ *
+ * @param[in]  data        Input data
+ * @param[in]  data_length Size in bytes of \p data
+ * @param[out] hash        Output hash
+ * @param[in]  hash_len    Size in bytes of the \p hash buffer
+ * @param[out] hash_size   Size of the produced hash
+ *
+ * @return fih_int 0 on success, non-zero otherwise
+ */
 static fih_int sha256_compute(const uint8_t *data,
                               size_t data_length,
                               uint8_t *hash, size_t hash_len, size_t *hash_size)
@@ -101,79 +70,18 @@ static fih_int sha256_compute(const uint8_t *data,
     FIH_RET(FIH_SUCCESS);
 }
 
-
-fih_int sha384_init()
-{
-    fih_int fih_rc;
-    int rc;
-
-    multipart_alg = TFM_BL1_HASH_ALG_SHA384;
-
-    mbedtls_sha512_init(&multipart_ctx);
-
-    rc = mbedtls_sha512_starts(&multipart_ctx, 1);
-    fih_rc = fih_int_encode_zero_equality(rc);
-    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
-        goto out;
-    }
-
-    FIH_RET(FIH_SUCCESS);
-
-out:
-    mbedtls_sha512_free(&multipart_ctx);
-    multipart_alg = 0;
-    FIH_RET(fih_rc);
-
-}
-
-fih_int sha384_finish(uint8_t *hash,
-                        size_t hash_length,
-                        size_t *hash_size)
-{
-    fih_int fih_rc;
-    int rc;
-
-    if (hash_length < 48) {
-        fih_rc = FIH_FAILURE;
-        goto out;
-    }
-
-    rc = mbedtls_sha512_finish(&multipart_ctx, hash);
-    fih_rc = fih_int_encode_zero_equality(rc);
-    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
-        goto out;
-    }
-
-    if (hash_size != NULL) {
-        *hash_size = 48;
-    }
-
-out:
-    mbedtls_sha512_free(&multipart_ctx);
-    multipart_alg = 0;
-    FIH_RET(fih_rc);
-}
-
-fih_int sha384_update(const uint8_t *data,
-                        size_t data_length)
-{
-    int rc;
-    fih_int fih_rc;
-
-    rc = mbedtls_sha512_update(&multipart_ctx, data, data_length);
-    fih_rc = fih_int_encode_zero_equality(rc);
-    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
-        goto out;
-    }
-
-    FIH_RET(FIH_SUCCESS);
-
-out:
-    mbedtls_sha512_free(&multipart_ctx);
-    multipart_alg = 0;
-    FIH_RET(fih_rc);
-}
-
+/**
+ * @brief Computes a hash of the provided data using SHA-384. It calls into a Mbed TLS
+ *        based SW implementation of SHA-512/384 as there is no HW support in CryptoCell
+ *
+ * @param[in]  data        Input data
+ * @param[in]  data_length Size in bytes of \p data
+ * @param[out] hash        Output hash
+ * @param[in]  hash_len    Size in bytes of the \p hash buffer
+ * @param[out] hash_size   Size of the produced hash
+ *
+ * @return fih_int 0 on success, non-zero otherwise
+ */
 static fih_int sha384_compute(const uint8_t *data,
                            size_t data_length,
                            uint8_t *hash, size_t hash_len, size_t *hash_size)
@@ -215,62 +123,133 @@ out:
     FIH_RET(fih_rc);
 }
 
-fih_int bl1_hash_init(enum tfm_bl1_hash_alg_t alg)
+static fih_int sha256_init(void)
 {
-    switch(alg) {
-    case TFM_BL1_HASH_ALG_SHA256:
-        FIH_RET(sha256_init());
-    case TFM_BL1_HASH_ALG_SHA384:
-        FIH_RET(sha384_init());
-    default:
-        FIH_RET(FIH_FAILURE);
+    fih_int fih_rc;
+
+    multipart_alg = TFM_BL1_HASH_ALG_SHA256;
+
+    /* This is only used by TFM_BL1_2 which currently only uses SHA256 */
+    fih_rc = fih_int_encode_zero_equality(cc3xx_lowlevel_hash_init(CC3XX_HASH_ALG_SHA256));
+    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
+        FIH_RET(fih_rc);
     }
+
+    FIH_RET(FIH_SUCCESS);
 }
 
-fih_int bl1_hash_finish(uint8_t *hash,
-                        size_t hash_length,
-                        size_t *hash_size)
+static fih_int sha256_update(const uint8_t *data,
+                             size_t data_length)
 {
-    switch(multipart_alg) {
-    case TFM_BL1_HASH_ALG_SHA256:
-        FIH_RET(sha256_finish(hash, hash_length, hash_size));
-    case TFM_BL1_HASH_ALG_SHA384:
-        FIH_RET(sha384_finish(hash, hash_length, hash_size));
-    default:
-        FIH_RET(FIH_FAILURE);
+    fih_int fih_rc;
+
+    fih_rc = fih_int_encode_zero_equality(cc3xx_lowlevel_hash_update(data, data_length));
+    if(fih_not_eq(fih_rc, FIH_SUCCESS)) {
+        FIH_RET(fih_rc);
     }
+
+    return FIH_SUCCESS;
 }
 
-fih_int bl1_hash_update(const uint8_t *data,
-                        size_t data_length)
+static fih_int sha256_finish(uint8_t *hash,
+                             size_t hash_length,
+                             size_t *hash_size)
 {
-    switch(multipart_alg) {
-    case TFM_BL1_HASH_ALG_SHA256:
-        FIH_RET(sha256_update(data, data_length));
-    case TFM_BL1_HASH_ALG_SHA384:
-        FIH_RET(sha384_update(data, data_length));
-    default:
-        FIH_RET(FIH_FAILURE);
+    cc3xx_lowlevel_hash_finish((uint32_t *)hash, hash_length);
+
+    if (hash_size != NULL) {
+        *hash_size = 32;
     }
+
+    multipart_alg = 0;
+
+    FIH_RET(FIH_SUCCESS);
 }
 
-fih_int bl1_hash_compute(enum tfm_bl1_hash_alg_t alg,
-                         const uint8_t *data,
-                         size_t data_length,
-                         uint8_t *hash,
-                         size_t hash_length,
-                         size_t *hash_size)
+static fih_int sha384_init(void)
 {
-    switch(alg) {
-    case TFM_BL1_HASH_ALG_SHA256:
-        FIH_RET(sha256_compute(data, data_length, hash, hash_length, hash_size));
-    case TFM_BL1_HASH_ALG_SHA384:
-        FIH_RET(sha384_compute(data, data_length, hash, hash_length, hash_size));
-    default:
-        FIH_RET(FIH_FAILURE);
+    fih_int fih_rc;
+    int rc;
+
+    multipart_alg = TFM_BL1_HASH_ALG_SHA384;
+
+    mbedtls_sha512_init(&multipart_ctx);
+
+    rc = mbedtls_sha512_starts(&multipart_ctx, 1);
+    fih_rc = fih_int_encode_zero_equality(rc);
+    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
+        goto out;
     }
+
+    FIH_RET(FIH_SUCCESS);
+
+out:
+    mbedtls_sha512_free(&multipart_ctx);
+    multipart_alg = 0;
+    FIH_RET(fih_rc);
+
 }
 
+static fih_int sha384_update(const uint8_t *data,
+                             size_t data_length)
+{
+    int rc;
+    fih_int fih_rc;
+
+    rc = mbedtls_sha512_update(&multipart_ctx, data, data_length);
+    fih_rc = fih_int_encode_zero_equality(rc);
+    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
+        goto out;
+    }
+
+    FIH_RET(FIH_SUCCESS);
+
+out:
+    mbedtls_sha512_free(&multipart_ctx);
+    multipart_alg = 0;
+    FIH_RET(fih_rc);
+}
+
+static fih_int sha384_finish(uint8_t *hash,
+                             size_t hash_length,
+                             size_t *hash_size)
+{
+    fih_int fih_rc;
+    int rc;
+
+    if (hash_length < 48) {
+        fih_rc = FIH_FAILURE;
+        goto out;
+    }
+
+    rc = mbedtls_sha512_finish(&multipart_ctx, hash);
+    fih_rc = fih_int_encode_zero_equality(rc);
+    if (fih_not_eq(fih_rc, FIH_SUCCESS)) {
+        goto out;
+    }
+
+    if (hash_size != NULL) {
+        *hash_size = 48;
+    }
+
+out:
+    mbedtls_sha512_free(&multipart_ctx);
+    multipart_alg = 0;
+    FIH_RET(fih_rc);
+}
+
+/**
+ * @brief Convert from BL1 key to KMU key (driver specific). For keys
+ *        which are not HUK/GUK/Kce_cm, it will load the key material
+ *        reading it from the OTP
+ *
+ * @param[in]  key_id         Id of the key to convert of type \ref tfm_bl1_key_id_t
+ * @param[out] cc3xx_key_type Corresponding KMU keyslot
+ * @param[out] key_buf        Pointer to buffer containing the key material for non HUK/GUK/Kce_cm
+ * @param[in]  key_buf_size   Size in bytes of the buffer pointed by \p key_buf
+ *
+ * @return fih_int 0 on success, non-zero on error
+ */
 static fih_int bl1_key_to_kmu_key(enum tfm_bl1_key_id_t key_id,
                                   enum kmu_hardware_keyslot_t *cc3xx_key_type,
                                   uint8_t **key_buf, size_t key_buf_size)
@@ -301,6 +280,81 @@ static fih_int bl1_key_to_kmu_key(enum tfm_bl1_key_id_t key_id,
     }
 
     FIH_RET(FIH_SUCCESS);
+}
+
+/**
+ * @brief Convert from BL1 curve to CC3XX curve (driver_specific)
+ *
+ * @param[in] bl1_curve BL1 curve to translate to a CC3XX curve
+ *
+ * @return cc3xx_ec_curve_id_t Converted curve as interpreted by CC3XX driver
+ */
+static cc3xx_ec_curve_id_t bl1_curve_to_cc3xx_curve(enum tfm_bl1_ecdsa_curve_t bl1_curve)
+{
+    switch(bl1_curve) {
+    case TFM_BL1_CURVE_P256:
+        return CC3XX_EC_CURVE_SECP_256_R1;
+    case TFM_BL1_CURVE_P384:
+        return CC3XX_EC_CURVE_SECP_384_R1;
+    default:
+        return _CURVE_ID_MAX;
+    };
+}
+
+fih_int bl1_hash_compute(enum tfm_bl1_hash_alg_t alg,
+                         const uint8_t *data,
+                         size_t data_length,
+                         uint8_t *hash,
+                         size_t hash_length,
+                         size_t *hash_size)
+{
+    switch(alg) {
+    case TFM_BL1_HASH_ALG_SHA256:
+        FIH_RET(sha256_compute(data, data_length, hash, hash_length, hash_size));
+    case TFM_BL1_HASH_ALG_SHA384:
+        FIH_RET(sha384_compute(data, data_length, hash, hash_length, hash_size));
+    default:
+        FIH_RET(FIH_FAILURE);
+    }
+}
+
+fih_int bl1_hash_init(enum tfm_bl1_hash_alg_t alg)
+{
+    switch(alg) {
+    case TFM_BL1_HASH_ALG_SHA256:
+        FIH_RET(sha256_init());
+    case TFM_BL1_HASH_ALG_SHA384:
+        FIH_RET(sha384_init());
+    default:
+        FIH_RET(FIH_FAILURE);
+    }
+}
+
+fih_int bl1_hash_update(const uint8_t *data,
+                        size_t data_length)
+{
+    switch(multipart_alg) {
+    case TFM_BL1_HASH_ALG_SHA256:
+        FIH_RET(sha256_update(data, data_length));
+    case TFM_BL1_HASH_ALG_SHA384:
+        FIH_RET(sha384_update(data, data_length));
+    default:
+        FIH_RET(FIH_FAILURE);
+    }
+}
+
+fih_int bl1_hash_finish(uint8_t *hash,
+                        size_t hash_length,
+                        size_t *hash_size)
+{
+    switch(multipart_alg) {
+    case TFM_BL1_HASH_ALG_SHA256:
+        FIH_RET(sha256_finish(hash, hash_length, hash_size));
+    case TFM_BL1_HASH_ALG_SHA384:
+        FIH_RET(sha384_finish(hash, hash_length, hash_size));
+    default:
+        FIH_RET(FIH_FAILURE);
+    }
 }
 
 int32_t bl1_aes_256_ctr_decrypt(enum tfm_bl1_key_id_t key_id,
@@ -379,18 +433,6 @@ int32_t bl1_derive_key(enum tfm_bl1_key_id_t key_id, const uint8_t *label,
     return TFM_PLAT_ERR_SUCCESS;
 }
 
-cc3xx_ec_curve_id_t bl1_curve_to_cc3xx_curve(enum tfm_bl1_ecdsa_curve_t bl1_curve)
-{
-    switch(bl1_curve) {
-    case TFM_BL1_CURVE_P256:
-        return CC3XX_EC_CURVE_SECP_256_R1;
-    case TFM_BL1_CURVE_P384:
-        return CC3XX_EC_CURVE_SECP_384_R1;
-    default:
-        return _CURVE_ID_MAX;
-    };
-}
-
 fih_int bl1_ecdsa_verify(enum tfm_bl1_ecdsa_curve_t curve,
                          uint8_t *key, size_t key_size,
                          const uint8_t *hash,
@@ -399,7 +441,7 @@ fih_int bl1_ecdsa_verify(enum tfm_bl1_ecdsa_curve_t curve,
                          size_t signature_size)
 {
     cc3xx_err_t cc_err;
-    cc3xx_ec_curve_id_t cc_curve = bl1_curve_to_cc3xx_curve(curve);
+    const cc3xx_ec_curve_id_t cc_curve = bl1_curve_to_cc3xx_curve(curve);
     uint32_t point_size = cc3xx_lowlevel_ec_get_modulus_size_from_curve(cc_curve);
     uint32_t pubkey_x[point_size / sizeof(uint32_t)];
     uint32_t pubkey_y[point_size / sizeof(uint32_t)];
