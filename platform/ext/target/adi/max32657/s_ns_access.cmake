2062c2856b5055c76ee6b77bb615589fdd4ff90d
Sadik Ozer <sadik.ozer@analog.com>
2025-02-25 14:27:53 +0300

    platform: ext: adi: Configure S-NS peripherals ownership
    Add configuration flags to define peripherals ownerships
    either secure or non-secure
    Remove ICE_IRQn and ECC_IRQn fron NS due to this peripherals
    not accessible by NS world
    
    User can configure peripheral ownership over s_ns_access_overlay.cmake
    file. This file need to be defined in the project folder root folder.
    
    On default NS world control all peripheral.
    As an example to transfer gpio and timer0 on the secure world
    write below lines in the overlay file.
    
    ...
    set(ADI_NS_PRPH_GPIO0       OFF)
    set(ADI_NS_PRPH_TIMER0      OFF)
    ...
    
    s_ns_access_overlay.cmake file must be defined in the
    project workspace root folder.
    
    Change-Id: I37ab37ef600043707cc88aac046e4c3ce8ff903f
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

---
+++ platform/ext/target/adi/max32657/s_ns_access.cmake
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Portions Copyright (C) 2025 Analog Devices, Inc.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+set(ADI_NS_PRPH_GCR         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_SIR         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_FCR         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_WDT         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_AES         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_AESKEY      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_CRC         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_GPIO0       ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TIMER0      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TIMER1      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TIMER2      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TIMER3      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TIMER4      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TIMER5      ON         CACHE BOOL "")
+set(ADI_NS_PRPH_I3C         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_UART        ON         CACHE BOOL "")
+set(ADI_NS_PRPH_SPI         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_TRNG        ON         CACHE BOOL "")
+set(ADI_NS_PRPH_BTLE_DBB    ON         CACHE BOOL "")
+set(ADI_NS_PRPH_BTLE_RFFE   ON         CACHE BOOL "")
+set(ADI_NS_PRPH_RSTZ        ON         CACHE BOOL "")
+set(ADI_NS_PRPH_BOOST       ON         CACHE BOOL "")
+set(ADI_NS_PRPH_BBSIR       ON         CACHE BOOL "")
+set(ADI_NS_PRPH_BBFCR       ON         CACHE BOOL "")
+set(ADI_NS_PRPH_RTC         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_WUT0        ON         CACHE BOOL "")
+set(ADI_NS_PRPH_WUT1        ON         CACHE BOOL "")
+set(ADI_NS_PRPH_PWR         ON         CACHE BOOL "")
+set(ADI_NS_PRPH_MCR         ON         CACHE BOOL "")
+
+#
+# Allow user set S-NS resources ownership by overlay file
+#
+if(EXISTS "${CMAKE_BINARY_DIR}/../../s_ns_access_overlay.cmake")
+    include(${CMAKE_BINARY_DIR}/../../s_ns_access_overlay.cmake)
+endif()
