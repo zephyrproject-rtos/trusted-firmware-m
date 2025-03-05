6b40279b1f61ce75219072b085edbd8be699b537
Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>
2025-03-05 20:00:22 +0000

    CC3XX: introduce build flags to pick the required tests
    CC3XX_TESTS flag enables all CC3XX tests which can easily overflow the
    flash / RAM. Therefore, introduce new flags to enable only the tests
    needed.
    
    Change-Id: I96109f57cf91ed235453a2899f8f4c231571e672
    Signed-off-by: Amjad Ouled-Ameur <amjad.ouled-ameur@arm.com>

--- platform/ext/target/arm/drivers/cc3xx/tests/src/cc3xx_test_main.c
+++ platform/ext/target/arm/drivers/cc3xx/tests/src/cc3xx_test_main.c
@@ -16,13 +16,25 @@
 
 void add_cc3xx_tests_to_testsuite(struct test_suite_t *p_ts, uint32_t ts_size)
 {
-#ifdef TEST_CC3XX
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_HASH)
     add_cc3xx_hash_tests_to_testsuite(p_ts, ts_size);
+#endif
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_AES)
     add_cc3xx_aes_tests_to_testsuite(p_ts, ts_size);
+#endif
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_CHACHA)
     add_cc3xx_chacha_tests_to_testsuite(p_ts, ts_size);
+#endif
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_PKA)
     add_cc3xx_pka_tests_to_testsuite(p_ts, ts_size);
+#endif
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_ECC)
     add_cc3xx_ecc_tests_to_testsuite(p_ts, ts_size);
+#endif
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_ECDSA)
     add_cc3xx_ecdsa_tests_to_testsuite(p_ts, ts_size);
+#endif
+#if defined(TEST_CC3XX) && defined(TEST_CC3XX_DRBG)
     add_cc3xx_drbg_tests_to_testsuite(p_ts, ts_size);
-#endif /* TEST_CC3XX */
+#endif
 }
