d96a966d3d442c2ebafa485c816aa2d1b24dcfc9
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-03-24 16:46:25 +0100

    [zep fromtree] platform: nordic_nrf: Add initial support for nRF54L10
    This adds nRF54L10 initial support for TF-M.
    
    This is NOT full upstream support. There are important limitations:
    - Hardware crypto acceleration with Cracen is not supported
    - Random number generation with Cracen is not supported
    - The tests suites PSA arch tests and TF-M regression tests
      are not tested
    - BL2 is not supported
    - Some soc related configurations are not supported and they rely
      on hard-coded values (check nordicsemi_nrf54l_init for more info)
    
    This was tested using some basic Zephyr samples.
    
    Only one flash/RAM layout is supported at the moment.
    
    This adds the same level of support with the nRF54L15, the configuration
    is nearly identical, the main difference is the partitioning scheme
    since these devices have different RRAM/RAM sizes.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Change-Id: I396d0d570ebd9b471138b7f02019bc0a1279e0d1
    (cherry picked from commit d301766d63703596114beda0934b8c70497e33ca)

---
+++ platform/ext/target/nordic_nrf/common/nrf54l10/config.cmake
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2020, Nordic Semiconductor ASA.
+# Copyright (c) 2020-2023, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+include(${PLATFORM_PATH}/common/nrf54l/config.cmake)
+
