c08947dc4faf59e5fab08e501fb2f56f88a19f41
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-04-14 15:28:00 +0300

    Revert "cmake: set `CMAKE_INSTALL_MESSAGE` in single location"
    This reverts commit 918f32d9fce5e0ee59fc33844b5317b7626ce37a.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -7,6 +7,9 @@
 #
 #-------------------------------------------------------------------------------
 
+# Skip "up-to-date" prints to avoid flooding the build output. Just print "installing"
+set(CMAKE_INSTALL_MESSAGE LAZY)
+
 install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/
         DESTINATION bin
 )
