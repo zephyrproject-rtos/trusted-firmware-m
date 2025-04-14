e062190cfc65b83965f4167a74a0d6af3913a1b5
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-04-14 15:28:00 +0300

    Revert "platform: nordic_nrf: Support for building nRF54L15 + sync repos"
    This reverts commit 0a94444197d368f71d8ca52393dda59241a86357.

--- platform/ext/target/nordic_nrf/nrf54l15dk_nrf54l15_cpuapp/tests/psa_arch_tests_config.cmake
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2023, Arm Limited. All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-#-------------------------------------------------------------------------------
-
-include(${PLATFORM_PATH}/common/nrf54l15/tests/psa_arch_tests_config.cmake)
