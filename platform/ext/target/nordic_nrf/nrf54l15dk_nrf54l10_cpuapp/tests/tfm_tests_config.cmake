74b746e3d5f12703ddd38c90e77b69154697dd74
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-04-14 15:28:00 +0300

    Revert "platform: nordic_nrf: Add initial support for nRF54L10"
    This reverts commit 857f3697ece6f44a663f007de71205a7b34974ef.

--- platform/ext/target/nordic_nrf/nrf54l15dk_nrf54l10_cpuapp/tests/tfm_tests_config.cmake
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2023, Arm Limited. All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-#-------------------------------------------------------------------------------
-
-include(${PLATFORM_PATH}/common/core/tests/tfm_tests_config.cmake)
