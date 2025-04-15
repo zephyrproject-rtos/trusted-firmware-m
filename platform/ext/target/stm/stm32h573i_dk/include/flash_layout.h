320b5ea9dd2fe2dc9b21ac23edbe2f3230623e15
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-04-15 12:08:07 +0300

    Revert "Platform: STM32H5: Align MCUBOOT_UPGRADE_STRATEGY to flash_layout.h"
    This reverts commit 60ebade5d3d381a210af90191e475d8870b8adbc.

--- platform/ext/target/stm/stm32h573i_dk/include/flash_layout.h
+++ platform/ext/target/stm/stm32h573i_dk/include/flash_layout.h
@@ -57,11 +57,7 @@
 
 /* Flash layout configuration : begin */
 #define MCUBOOT_OVERWRITE_ONLY    /* Defined: the FW installation uses ovewrite method.
-                                     UnDefined: The FW installation uses whatever mode
-                                     is configured through the MCUBOOT_UPGRADE_STRATEGY
-                                     config item in config.cmake. FixMe: MCUboot should
-                                     be configured in a single place, i.e. config.cmake
-                                     to avoid clashes */
+                                      UnDefined: The FW installation uses swap mode. */
 
 /*#define MCUBOOT_PRIMARY_ONLY*/   /* Defined: No secondary (download) slot(s),
                                               only primary slot(s) for each image.
