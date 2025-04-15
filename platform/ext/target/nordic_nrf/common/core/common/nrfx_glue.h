362c68345bb9f96deaafd9cdaabe4756e33125d6
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-04-15 12:08:07 +0300

    Revert "platform: nordic_nrf: Use generic assert for NRFX_ASSERT"
    This reverts commit 891fb22e2bc6a6a19ce2606c47eef7b76f60fb7e.

--- platform/ext/target/nordic_nrf/common/core/common/nrfx_glue.h
+++ platform/ext/target/nordic_nrf/common/core/common/nrfx_glue.h
@@ -33,7 +33,12 @@
 #define NRFX_GLUE_H__
 
 /* Include the spm utilities for the SPM_ASSERT symbol */
+#if defined(DOMAIN_NS) && (DOMAIN_NS == 1)
 #include <assert.h>
+#else
+#include <utilities.h>
+#include "private/assert.h" /* temporarily included - to be removed */
+#endif
 
 #include <soc/nrfx_coredep.h>
 
@@ -60,7 +65,11 @@ extern "C" {
 #if defined(NDEBUG)
 #define NRFX_ASSERT(expression)  if (0 && (expression)) {}
 #else
+#if defined(DOMAIN_NS) && (DOMAIN_NS == 1)
 #define NRFX_ASSERT(expression)  assert(expression)
+#else
+#define NRFX_ASSERT(expression)  SPM_ASSERT(expression)
+#endif
 #endif
 
 /**
