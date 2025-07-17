2d91588b14ba2138e05beb1d0685302e68c05fb1
Nicola Mazzucato <nicola.mazzucato@arm.com>
2025-07-17 10:10:32 +0100

    Docs: Release notes for v2.2.1
    Add release notes for this v2.2.1 release.
    
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>
    Change-Id: Ia9bff5ef608d1efad7d8c078a6a0c17558a07aaf

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -6,7 +6,7 @@
 #-------------------------------------------------------------------------------
 
 # The 'TFM_VERSION_MANUAL' is used for fallback when Git tags are not available
-set(TFM_VERSION_MANUAL "2.2.0")
+set(TFM_VERSION_MANUAL "2.2.1")
 
 execute_process(COMMAND git describe --tags --always
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
