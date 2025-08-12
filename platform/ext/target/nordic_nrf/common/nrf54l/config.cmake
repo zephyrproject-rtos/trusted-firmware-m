0e290d1bd9a75e98d7a18c03ed1252198cdf7139
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2025-08-12 15:33:46 +0200

    [zep fromtree] platform: lv10a: update uart configuration for lv10
    lv10 should use uart20 as the secure uart.
    Update configurations to reflect this
    
    Change-Id: Ic4d5bce62fc810dc5d3f313ff1a34d456c4d5ccd
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
    (cherry picked from commit 01a4fa46d6b9926849da847ba2c37ed17caded43)

--- platform/ext/target/nordic_nrf/common/nrf54l/config.cmake
+++ platform/ext/target/nordic_nrf/common/nrf54l/config.cmake
@@ -8,7 +8,7 @@
 
 include(${PLATFORM_PATH}/common/core/config.cmake)
 
-set(SECURE_UART30                       ON         CACHE BOOL      "Enable secure UART" FORCE)
+set(SECURE_UART20                       ON         CACHE BOOL      "Enable secure UART" FORCE)
 set(BL2                                 OFF        CACHE BOOL      "Whether to build BL2" FORCE)
 set(NRF_NS_SECONDARY                    OFF        CACHE BOOL      "Enable non-secure secondary partition" FORCE)
-set(NRF_SECURE_UART_INSTANCE            30         CACHE STRING    "The UART instance number to use for secure UART" FORCE)
+set(NRF_SECURE_UART_INSTANCE            20         CACHE STRING    "The UART instance number to use for secure UART" FORCE)
