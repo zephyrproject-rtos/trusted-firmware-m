294b21edd4b2974ce008b29fd021d639e8f38272
Raef Coles <raef.coles@arm.com>
2025-08-21 11:44:48 +0100

    BL1: Rename conflicting macro
    Using BL2_HEADER_SIZE conflicts with the existing BL2 header size macro,
    and is confusing.
    
    Change-Id: Ia5eede86a1765a617a374539e0111179e81096b3
    Signed-off-by: Raef Coles <raef.coles@arm.com>
    (cherry picked from commit 690d20b88043fdad8a8730b367a54fa48f891d94)

--- bl1/bl1_2/lib/image.c
+++ bl1/bl1_2/lib/image.c
@@ -39,10 +39,10 @@ fih_int bl1_image_copy_to_sram(uint32_t image_id, uint8_t *out)
     flash_offset = bl1_image_get_flash_offset(image_id);
 
     fih_rc = fih_int_encode_zero_equality(
-                fih_not_eq(BL2_CODE_SIZE + BL2_HEADER_SIZE,
+                fih_not_eq(BL2_CODE_SIZE + BL1_2_HEADER_SIZE,
                            (FLASH_DEV_NAME_BL1.ReadData(flash_offset,
                                                         out,
-                                                        BL2_CODE_SIZE + BL2_HEADER_SIZE))));
+                                                        BL2_CODE_SIZE + BL1_2_HEADER_SIZE))));
 
     FIH_RET(fih_rc);
 }
