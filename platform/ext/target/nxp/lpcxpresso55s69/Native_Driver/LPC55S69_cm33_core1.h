984b99327e2d919a1a286710b38852c49e94dc4f
Waqar Tahir <waqar.tahir@nxp.com>
2025-10-10 10:46:14 +0200

    [zep fromtree] platform: LPCXpresso55s69: Use of local drivers
    All drivers required by lpcxpresso55s69 are placed
    in platform/common folder of NXP, copied from sdk
    25.09.00. Instead of fetching drivers at run time,
    all required drivers are placed in platform or
    common directory. This is to re-use the drivers
    by upcoming platforms as well as keep using same
    drivers during zephyr builds.
    
    Change-Id: I5500dad96ebbe5d000d140a5bab25349ce36cc5f
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>
    (cherry picked from commit 0b704878e8a30559d0e13c5e7bb1e8bab0787d6d)

---
+++ platform/ext/target/nxp/lpcxpresso55s69/Native_Driver/LPC55S69_cm33_core1.h
@@ -0,0 +1,97 @@
+/*
+** ###################################################################
+**     Processors:          LPC55S69JBD100_cm33_core1
+**                          LPC55S69JBD64_cm33_core1
+**                          LPC55S69JEV59_cm33_core1
+**                          LPC55S69JEV98_cm33_core1
+**
+**     Compilers:           GNU C Compiler
+**                          IAR ANSI C/C++ Compiler for ARM
+**                          Keil ARM C/C++ Compiler
+**                          MCUXpresso Compiler
+**
+**     Reference manual:    LPC55S6x/LPC55S2x/LPC552x User manual(UM11126) Rev.1.3  16 May 2019
+**     Version:             rev. 2.0, 2024-10-29
+**     Build:               b250529
+**
+**     Abstract:
+**         CMSIS Peripheral Access Layer for LPC55S69_cm33_core1
+**
+**     Copyright 1997-2016 Freescale Semiconductor, Inc.
+**     Copyright 2016-2025 NXP
+**     SPDX-License-Identifier: BSD-3-Clause
+**
+**     http:                 www.nxp.com
+**     mail:                 support@nxp.com
+**
+**     Revisions:
+**     - rev. 1.0 (2018-08-22)
+**         Initial version based on v0.2UM
+**     - rev. 1.1 (2019-05-16)
+**         Initial A1 version based on v1.3UM
+**     - rev. 2.0 (2024-10-29)
+**         Change the device header file from single flat file to multiple files based on peripherals,
+**         each peripheral with dedicated header file located in periphN folder.
+**
+** ###################################################################
+*/
+
+/*!
+ * @file LPC55S69_cm33_core1.h
+ * @version 2.0
+ * @date 2024-10-29
+ * @brief CMSIS Peripheral Access Layer for LPC55S69_cm33_core1
+ *
+ * CMSIS Peripheral Access Layer for LPC55S69_cm33_core1
+ */
+
+#if !defined(LPC55S69_cm33_core1_H_)  /* Check if memory map has not been already included */
+#define LPC55S69_cm33_core1_H_
+
+/* IP Header Files List */
+#include "PERI_ADC.h"
+#include "PERI_AHB_SECURE_CTRL.h"
+#include "PERI_ANACTRL.h"
+#include "PERI_CASPER.h"
+#include "PERI_CRC.h"
+#include "PERI_CTIMER.h"
+#include "PERI_DBGMAILBOX.h"
+#include "PERI_DMA.h"
+#include "PERI_FLASH.h"
+#include "PERI_FLASH_CFPA.h"
+#include "PERI_FLASH_CMPA.h"
+#include "PERI_FLASH_KEY_STORE.h"
+#include "PERI_FLEXCOMM.h"
+#include "PERI_GINT.h"
+#include "PERI_GPIO.h"
+#include "PERI_HASHCRYPT.h"
+#include "PERI_I2C.h"
+#include "PERI_I2S.h"
+#include "PERI_INPUTMUX.h"
+#include "PERI_IOCON.h"
+#include "PERI_MAILBOX.h"
+#include "PERI_MRT.h"
+#include "PERI_OSTIMER.h"
+#include "PERI_PINT.h"
+#include "PERI_PLU.h"
+#include "PERI_PMC.h"
+#include "PERI_POWERQUAD.h"
+#include "PERI_PRINCE.h"
+#include "PERI_PUF.h"
+#include "PERI_RNG.h"
+#include "PERI_RTC.h"
+#include "PERI_SCT.h"
+#include "PERI_SDIF.h"
+#include "PERI_SPI.h"
+#include "PERI_SYSCON.h"
+#include "PERI_SYSCTL.h"
+#include "PERI_USART.h"
+#include "PERI_USB.h"
+#include "PERI_USBFSH.h"
+#include "PERI_USBHSD.h"
+#include "PERI_USBHSH.h"
+#include "PERI_USBPHY.h"
+#include "PERI_UTICK.h"
+#include "PERI_WWDT.h"
+
+#endif  /* #if !defined(LPC55S69_cm33_core1_H_) */
