97b9a2773f5000e0825f34610f416fd11596e171
Keith Packard <keithp@keithp.com>
2025-10-28 15:32:58 -0700

    platform: Update picolibc.c for linker script compatibility
    Always use picolibc_startup to initialize all .data and .bss
    segments. Set the picolibc initialization values for these segments to
    all zeros so that the crt0.o code doesn't do anything.
    
    Provide a stub for _set_tls as tf-m doesn't use thread local variables.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- platform/ext/common/picolibc.c
+++ platform/ext/common/picolibc.c
@@ -9,14 +9,14 @@
 #include <string.h>
 #include "config_impl.h"
 
+#ifdef __PICOLIBC__
+
 /*
  * Picolibc's startup code only initializes a single data and bss
  * segment.  Augment that by initializing the remaining segments using
  * the lists provided by the linker script in a constructor which will
  * be called from _start
  */
-
-#if defined(__PICOLIBC__) && TFM_ISOLATION_LEVEL > 0
 
 static void __attribute__((constructor))
 picolibc_startup(void)
@@ -45,4 +45,44 @@ picolibc_startup(void)
     memset(pTable->dest, 0, pTable->wlen << 2);
   }
 }
+
+/*
+ * Create symbols for picolibc. These are used by the
+ * startup code to initialize memory, but that work
+ * is done by the function above
+ */
+
+__asm__(".globl __data_source\n"
+        ".globl __data_start\n"
+        ".globl __data_size\n"
+        ".globl __bss_size\n"
+        ".globl __bss_start\n"
+        ".equ __data_source, 0\n"
+        ".equ __data_start, 0\n"
+        ".equ __data_size, 0\n"
+        ".equ __bss_size, 0\n"
+        ".equ __bss_start, 0");
+
+#ifdef __THREAD_LOCAL_STORAGE
+
+__asm__(".globl __tls_base\n"
+        ".equ __tls_base, 0");
+
+#include <picotls.h>
+
+/*
+ * Picolibc's startup code wants to initialize the thread local
+ * storage base address, but tf-m doesn't use any TLS variables.
+ *
+ * Stub out this function until TLS is required.
+ */
+
+void
+_set_tls(void *tls_base)
+{
+    (void) tls_base;
+}
+
 #endif
+
+#endif /* __PICOLIBC__ */
