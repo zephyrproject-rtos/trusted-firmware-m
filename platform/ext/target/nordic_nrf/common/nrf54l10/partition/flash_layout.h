b3aacda9e2b0ec77e3321a37ea2e3c0562d15150
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-11-10 20:32:49 +0100

    [zep fromtree] platform: nordic: Remove FLPR reserved memory from Nordic boards
    Remove the FLPR reserved non-volatile and volatile memory for all
    Nordic boards since it is not yet supported with TF-M.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Change-Id: I6bb513d52a55b9571e10392b567b9b77a686d79b
    (cherry picked from commit 9dc8a38c48d8189211de9b12e9d06dafcef7848c)

--- platform/ext/target/nordic_nrf/common/nrf54l10/partition/flash_layout.h
+++ platform/ext/target/nordic_nrf/common/nrf54l10/partition/flash_layout.h
@@ -21,14 +21,14 @@
 #error "BL2 is not supported for this platform"
 #endif
 
-/* Flash layout on NRF54L15 Application MCU without BL2:
+/* Flash layout on NRF54L10 Application MCU without BL2:
  *
  * 0x0000_0000 Secure image primary (384 KB)
  * 0x0006_0000 Protected Storage Area (16 KB)
  * 0x0006_4000 Internal Trusted Storage Area (16 KB)
  * 0x0006_8000 OTP / NV counters area (8 KB)
- * 0x0006_A000 Non-secure image primary (494 KB)
- * 0x000E_5800 Non-secure storage, used when built with NRF_NS_STORAGE=ON,
+ * 0x0006_A000 Non-secure image primary (556 KB)
+ * 0x000F_5000 Non-secure storage, used when built with NRF_NS_STORAGE=ON,
  *             otherwise unused (32 KB)
  */
 
@@ -42,20 +42,16 @@
 /* Use Flash memory to store Code data */
 #define FLASH_BASE_ADDRESS                  (0x0)
 
-/* nRF54L10 has 1012 kB of non volatile memory (RRAM) but the last 62kB are reserved
- * for FLPR MCU in Zephyr. For simplicity and for possible support for running FLPR along
- * with TF-M later FLPR non volatile memory is not used by TF-M. */
-#define FLASH_TOTAL_SIZE                    (0xED800)         /* 950 kB since the last 62kB are reserved for FLPR */
+/* nRF54L10 has 1012 kB of non volatile memory (RRAM) */
+#define FLASH_TOTAL_SIZE                    (0xFD000)
 #define TOTAL_ROM_SIZE                       FLASH_TOTAL_SIZE
 
-/* nRF54L10 has 192 kB of volatile memory (SRAM) but the last 48kB are reserved
- * for FLPR MCU in Zephyr. For simplicity and for possible support for running FLPR along
- * with TF-M later FLPR volatile memory is not used by TF-M. */
+/* nRF54L10 has 192 kB of volatile memory (SRAM) */
 #define SRAM_BASE_ADDRESS                   (0x20000000)
-#define TOTAL_RAM_SIZE                      (0x00024000)       /* 144 kB since the last 48kB are reserved for FLPR */
+#define TOTAL_RAM_SIZE                      (0x00030000)
 
 #define FLASH_S_PARTITION_SIZE                (0x60000)       /* S partition: 384 kB*/
-#define FLASH_NS_PARTITION_SIZE               (0x7B800)       /* NS partition: 494 kB*/
+#define FLASH_NS_PARTITION_SIZE               (0x8B000)       /* NS partition: 556 kB*/
 
 #define S_ROM_ALIAS_BASE   FLASH_BASE_ADDRESS
 #define NS_ROM_ALIAS_BASE  FLASH_BASE_ADDRESS
