60e412eff658cceb0c5857351109de424c021701
Laurie Fay <Laurie.Fay@arm.com>
2026-08-18 09:21:57 +0100

    [zep fromtree] BL1_2: Fix unused variable warnings in validate_image_signature
    Only declare 'status' if TFM_BL1_2_EMBED_ROTPK_IN_IMAGE or
    TFM_MEASURED_BOOT_API is defined. Similarly only declare
    'dummy' if TFM_BL1_2_EMBED_ROTPK_IN_IMAGE is defined.
    
    This fixes unused variable warnings when either/both of
    these constants are not defined.
    
    Change-Id: I003c46c7fae1f50f21aa3bba756a38f8aa7d0a58
    Signed-off-by: Laurie Fay <Laurie.Fay@arm.com>
    (cherry picked from commit 40f8e352bf08bb8d44f10754efc9b7596ee96741)

--- bl1/bl1_2/main.c
+++ bl1/bl1_2/main.c
@@ -120,13 +120,16 @@ static fih_ret validate_image_signature(struct bl1_2_image_t *img,
     uint8_t rotpk[TFM_BL1_2_ROTPK_MAX_SIZE];
     uint8_t *p_rotpk = rotpk;
     size_t rotpk_size;
+    enum tfm_bl1_key_type_t key_type;
 #if defined(TFM_BL1_2_EMBED_ROTPK_IN_IMAGE) || defined(TFM_MEASURED_BOOT_API)
     uint8_t rotpk_hash[TFM_BL1_2_ROTPK_HASH_MAX_SIZE];
     enum tfm_bl1_hash_alg_t key_hash_alg;
-#endif /* TFM_BL1_2_EMBED_ROTPK_IN_IMAGE || TFM_MEASURED_BOOT_API */
-    enum tfm_bl1_key_type_t key_type;
-    size_t dummy;
     psa_status_t status;
+#endif /* TFM_BL1_2_EMBED_ROTPK_IN_IMAGE || TFM_MEASURED_BOOT_API */
+
+#ifdef TFM_BL1_2_EMBED_ROTPK_IN_IMAGE
+    size_t dummy;
+#endif /* TFM_BL1_2_EMBED_ROTPK_IN_IMAGE */
 
     if (sig->sig_len > sizeof(sig->sig)) {
         ERROR("Invalid signature length\n");
